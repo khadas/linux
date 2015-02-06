7fd81e5119a06dc6b1febee1369c8873da450a2a
Yan Wang <yan.wang@amlogic.com>
2015-02-06 20:50:18 +0800

    secmon: meson: add interfaces to get share memory area.
    Change-Id: Id789a2c1c267eae8a3fb4a04522528efffd2a69c

---
+++ drivers/amlogic/secmon/secmon.c
@@ -0,0 +1,112 @@
+#include <linux/errno.h>
+#include <linux/err.h>
+#include <linux/of.h>
+#include <linux/of_fdt.h>
+#include <linux/libfdt_env.h>
+#include <linux/of_reserved_mem.h>
+#include <linux/io.h>
+#include <asm/opcodes-sec.h>
+#include <linux/platform_device.h>
+#undef pr_fmt
+#define pr_fmt(fmt) "secmon: " fmt
+
+static void __iomem *sharemem_in_base;
+static void __iomem *sharemem_out_base;
+static long phy_in_base;
+static long phy_out_base;
+
+ #define IN_SIZE	0x1000
+ #define OUT_SIZE 0x1000
+
+static long get_sharemem_info(unsigned function_id)
+{
+	register long r0 asm("r0") = function_id;
+	asm volatile(
+		__asmeq("%0", "r0")
+		__asmeq("%1", "r0")
+		__SMC(0)
+			: "=r" (r0)
+			: "r"(r0));
+
+	return r0;
+}
+static int secmon_probe(struct platform_device *pdev)
+{
+	struct device_node *np = pdev->dev.of_node;
+	unsigned id;
+	int ret;
+	if (!of_property_read_u32(np, "in_base_func", &id))
+		phy_in_base = get_sharemem_info(id);
+
+	if (!of_property_read_u32(np, "out_base_func", &id))
+		phy_out_base = get_sharemem_info(id);
+
+	ret = of_reserved_mem_device_init(&pdev->dev);
+	if (ret == 0)
+		pr_info("probe done");
+	return ret;
+}
+
+static const struct of_device_id secmon_dt_match[] = {
+	{ .compatible = "aml,secmon" },
+	{ /* sentinel */ },
+};
+
+static  struct platform_driver secmon_platform_driver = {
+	.probe		= secmon_probe,
+	.driver		= {
+		.owner		= THIS_MODULE,
+		.name		= "secmon",
+		.of_match_table	= secmon_dt_match,
+	},
+};
+
+int __init meson_secmon_init(void)
+{
+	return  platform_driver_register(&secmon_platform_driver);
+}
+module_init(meson_secmon_init);
+
+void __iomem *get_secmon_sharemem_input_base(void)
+{
+	if (!sharemem_in_base) {
+		sharemem_in_base = ioremap(phy_in_base, IN_SIZE);
+		if (!sharemem_in_base)
+			pr_info("secmon share mem in buffer remap fail!\n");
+		else
+			pr_info("secmon share mem in base: 0x%lx\n",
+							(long)sharemem_in_base);
+	}
+	return sharemem_in_base;
+}
+void __iomem *get_secmon_sharemem_output_base(void)
+{
+	if (!sharemem_out_base) {
+		sharemem_out_base = ioremap(phy_out_base, OUT_SIZE);
+		if (!sharemem_out_base)
+			pr_info("secmon share mem out buffer remap fail!\n");
+		else
+			pr_info("secmon share mem out base: 0x%lx\n",
+				(long)sharemem_out_base);
+	}
+	return sharemem_out_base;
+}
+
+static int secmon_mem_device_init(struct reserved_mem *rmem, struct device *dev)
+{
+	sharemem_in_base = ioremap(phy_in_base, IN_SIZE);
+	sharemem_out_base = ioremap(phy_out_base, OUT_SIZE);
+	return 0;
+}
+static const struct reserved_mem_ops rmem_secmon_ops = {
+	.device_init = secmon_mem_device_init,
+};
+static int __init secmon_mem_setup(struct reserved_mem *rmem)
+{
+	rmem->ops = &rmem_secmon_ops;
+	pr_info("share mem setup\n");
+
+	return 0;
+}
+
+RESERVEDMEM_OF_DECLARE(secmonmem, "aml_secmon_memory", secmon_mem_setup);
---
+++ include/linux/amlogic/secmon.h
@@ -0,0 +1,7 @@
+#ifndef __SEC_MON_H__
+#define __SEC_MON_H__
+
+void __iomem *get_secmon_sharemem_input_base(void);
+void __iomem *get_secmon_sharemem_output_base(void);
+
+#endif
