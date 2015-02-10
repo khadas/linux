3d0a5e47607427e025464d9ebfac1fd94ce51bf7
Xing Xu <xing.xu@amlogic.com>
2015-02-10 10:42:55 +0800

    GPIO: meson: driver should use gpio irq param
    Change-Id: Ib65fc560a386c1f5ab874cd8ea92d6b3f2cc8ffa

--- include/linux/amlogic/aml_gpio_consumer.h
+++ include/linux/amlogic/aml_gpio_consumer.h
@@ -22,4 +22,34 @@ extern int amlogic_set_pull_up_down(unsigned int pin,
 extern int amlogic_disable_pullup(unsigned int pin, const char *owner);
 #define AML_GPIO_IRQ(irq_bank, filter, type) \
 ((irq_bank&0x7)|(filter&0x7)<<8|(type&0x3)<<16)
+
+enum {
+	GPIO_IRQ0 = 0,
+	GPIO_IRQ1,
+	GPIO_IRQ2,
+	GPIO_IRQ3,
+	GPIO_IRQ4,
+	GPIO_IRQ5,
+	GPIO_IRQ6,
+	GPIO_IRQ7,
+};
+
+enum {
+	GPIO_IRQ_HIGH = 0,
+	GPIO_IRQ_LOW,
+	GPIO_IRQ_RISING,
+	GPIO_IRQ_FALLING,
+};
+
+enum {
+	FILTER_NUM0 = 0,
+	FILTER_NUM1,
+	FILTER_NUM2,
+	FILTER_NUM3,
+	FILTER_NUM4,
+	FILTER_NUM5,
+	FILTER_NUM6,
+	FILTER_NUM7,
+};
+
 #endif
