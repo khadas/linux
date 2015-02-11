344d6789e9c2af29ceda1e7b142b96c063dd9fbb
Xing Xu <xing.xu@amlogic.com>
2015-02-11 10:40:15 +0800

    script: meson32: only compile kernel image and dtb file.
    We can load kernel image, ramdisk and dtb seperately, so we can
    compile kernel and dtb seperately.For example,if you want to load
    from sd card,use these instructions:
    	1 mmcinfo
    	2 fatload mmc 0 12000000 uimage
    	3 fatload mmc 0 13000000 rootfs.cpio.uboot
    	4 fatload mmc 0 14000000 meson8m2.dtb
    	5 bootm 12000000 13000000 14000000
    
    Change-Id: Iff494ce93cedaf0445f031e07559f3fe13bae15b

--- scripts/amlogic/mk_m8m2_n200.sh
+++ scripts/amlogic/mk_m8m2_n200.sh
@@ -1,20 +1,16 @@
 #! /bin/bash
 
-#make UIMAGE_COMPRESSION=none uImage -j
-rm -rf m8boot.img
-make  ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- uImage -j20 UIMAGE_LOADADDR=0x1008000
-#make modules
+make  ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- uImage -j20 \
+	UIMAGE_LOADADDR=0x1008000 || echo "Compile uImage Fail !!"
 
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- meson8m2_n200_2G.dtb
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- meson8m2_n200_2G.dtb \
+	|| echo "Compile dtb Fail !!"
 
-#cd ../root/g18
-#find .| cpio -o -H newc | gzip -9 > ../ramdisk.img
 
 #rootfs.cpio -- original buildroot rootfs, busybox
-#m8rootfs.cpio -- build from buildroot
-ROOTFS="rootfs.cpio"
+#ROOTFS="rootfs.cpio"
 
-#cd ..
-./mkbootimg --kernel ./arch/arm/boot/uImage --ramdisk ./${ROOTFS} --second ./arch/arm/boot/dts/amlogic/meson8m2_n200_2G.dtb --output ./m8boot.img
-ls -l ./m8boot.img
-echo "m8boot.img done"
+#./mkbootimg --kernel ./arch/arm/boot/uImage \
+#	--ramdisk ./${ROOTFS} \
+#	--second ./arch/arm/boot/dts/amlogic/meson8m2_n200_2G.dtb \
+#	--output ./m8boot.img || echo "Compile m8boot.img Fail !!"
