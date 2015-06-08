9ecac1641cd77a24ebbd4ef72ddba549d498125d
Rico Yang <wei.yang@amlogic.com>
2015-06-08 14:57:26 +0800

    video_dev: add amlvideo driver
    Change-Id: I9971878aad4cd847d7351d26316eb2c8806c3393

---
+++ drivers/amlogic/video_dev/amlvideo.h
@@ -0,0 +1,14 @@
+/*
+ * amlvideo.h
+ *
+ *  Created on: 2015年5月4日
+ *      Author: Wei.Yang
+ */
+
+#ifndef AMLVIDEO_H_
+#define AMLVIDEO_H_
+
+extern bool omx_secret_mode;
+extern void get_ppmgr_buf_info(char **start, unsigned int *size);
+
+#endif /* AMLVIDEO_H_ */
