746b9bf50ef867dddb7623123a05d8a750b8b76e
Guosong Zhou <guosong.zhou@amlogic.com>
2015-06-26 19:52:11 +0800

    PD#108438: camera: add camera module
    Change-Id: I7c11acaa942c53a9ffceeec4458a897285a493e4

---
+++ drivers/amlogic/camera/common/register.h
@@ -0,0 +1,28 @@
+/*
+ * drivers/amlogic/camera/common/register.h
+ *
+ * Copyright (C) 2015 Amlogic, Inc. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+*/
+
+#ifndef __MACH_MESON8_REG_ADDR_H_
+#define __MACH_MESON8_REG_ADDR_H_
+#define HHI_XTAL_DIVN_CNTL 0xc883c0bc
+#define HHI_GEN_CLK_CNTL 0x108a
+#define P_HHI_GEN_CLK_CNTL		CBUS_REG_ADDR(HHI_GEN_CLK_CNTL)
+#define HHI_MPLL_CNTL7 0x10a6
+#define P_HHI_MPLL_CNTL7		CBUS_REG_ADDR(HHI_MPLL_CNTL7)
+#define HHI_DPLL_TOP_0 0x10c6
+#define P_HHI_DPLL_TOP_0		CBUS_REG_ADDR(HHI_DPLL_TOP_0)
+
+#endif
