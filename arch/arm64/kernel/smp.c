2e660b9f6df647627ed1565bbd9be63a5c360b6c
Zongdong Jiao <zongdong.jiao@amlogic.com>
2015-07-29 10:18:00 +0800

    Revert "arm64: mask SError exception for all cores"
    This reverts commit 2ceac791984ef2f5fb87de764e91566d4ac46b64.
    
    ref to PD#109772
    
    Change-Id: I48244355914013eb2cd87ffdc5972a77fce8fd29

--- arch/arm64/kernel/smp.c
+++ arch/arm64/kernel/smp.c
@@ -170,7 +170,7 @@ asmlinkage void secondary_start_kernel(void)
 	complete(&cpu_running);
 
 	local_irq_enable();
-	/*local_async_enable();*/
+	local_async_enable();
 
 	/*
 	 * OK, it's off to the idle thread for us
