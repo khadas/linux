8da1bda6c7156889e60a42ef101e7d0ddcdd08c2
Yue Wang <yue.wang@amlogic.com>
2015-09-01 11:38:44 +0800

    PD#111494: usb: dwc_otg2: fix usb doggle compatibility
    Change-Id: Ib6c1279e6b95a3e235e094e17819b9e3d83f8124

--- drivers/amlogic/usb/dwc_otg/310/dwc_otg_hcd_intr.c
+++ drivers/amlogic/usb/dwc_otg/310/dwc_otg_hcd_intr.c
@@ -1325,7 +1325,7 @@ static int32_t handle_hc_nak_intr(dwc_otg_hcd_t *hcd,
 			hcd->ssplit_lock = 0;
 
 		qtd->complete_split = 0;
-		if (qtd->error_count > 50) {
+		if (qtd->error_count > 200) {
 			DWC_ERROR("Can not read device info from hub.We take it error\n");
 			halt_channel(hcd, hc, qtd, DWC_OTG_HC_XFER_XACT_ERR);
 		} else {
