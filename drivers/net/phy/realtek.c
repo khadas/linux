8605ed9d29198f4b0c21d80e677d3ee17ad1b641
Pengguang Zhu <pengguang.zhu@amlogic.com>
2015-10-29 10:34:49 +0800

    PD#114565: net: phy: implement rtl8211f phy driver.
    Change-Id: I8a5e66200746e16faac3d149acb75620b929e574

--- drivers/net/phy/realtek.c
+++ drivers/net/phy/realtek.c
@@ -22,12 +22,56 @@
 #define RTL821x_INER		0x12
 #define RTL821x_INER_INIT	0x6400
 #define RTL821x_INSR		0x13
+#define RTL821x_PHYCR2		0x19
+#define RTL821x_CLKOUT_EN	0x1
+#define RTL821x_EPAGSR		0x1f
 
 #define	RTL8211E_INER_LINK_STATUS	0x400
 
 MODULE_DESCRIPTION("Realtek PHY driver");
 MODULE_AUTHOR("Johnson Leung");
 MODULE_LICENSE("GPL");
+
+static int rtl8211f_config_init(struct phy_device *phydev)
+{
+	int val;
+
+	/* close CLOCK output */
+	val = phy_read(phydev, RTL821x_PHYCR2);
+	if (val < 0)
+		return val;
+	phy_write(phydev, RTL821x_EPAGSR, 0xa43);
+	phy_write(phydev, RTL821x_PHYCR2,
+			(val & (~RTL821x_CLKOUT_EN)));
+	phy_write(phydev, RTL821x_EPAGSR, 0x0);
+	phy_write(phydev, MII_BMCR,
+			BMCR_RESET|BMCR_ANENABLE|BMCR_ANRESTART);
+
+	/* wait for ready */
+	do {
+		bmcr = phy_read(phydev, MII_BMCR);
+		if (bmcr < 0)
+			return bmcr;
+	} while (bmcr & BMCR_RESET);
+
+	/* we want to disable eee */
+	phy_write(phydev, RTL8211F_MMD_CTRL, 0x7);
+	phy_write(phydev, RTL8211F_MMD_DATA, 0x3c);
+	phy_write(phydev, RTL8211F_MMD_CTRL, 0x4007);
+	phy_write(phydev, RTL8211F_MMD_DATA, 0x0);
+
+	/* disable 1000m adv*/
+	val = phy_read(phydev, 0x9);
+	phy_write(phydev, 0x9, val&(~(1<<9)));
+
+	/* rx reg 21 bit 3 tx reg 17 bit 8*/
+	/* phy_write(phydev, 0x1f, 0xd08);
+	 * val =  phy_read(phydev, 0x15);
+	 * phy_write(phydev, 0x15,val| 1<<21);
+	 */
+
+	return 0;
+}
 
 static int rtl821x_ack_interrupt(struct phy_device *phydev)
 {
@@ -106,6 +150,22 @@ static struct phy_driver rtl8211e_driver = {
 	.driver		= { .owner = THIS_MODULE,},
 };
 
+/* RTL8211F */
+static struct phy_driver rtl8211f_driver = {
+	.phy_id		= 0x001cc916,
+	.name		= "RTL8211F Gigabit Ethernet",
+	.phy_id_mask	= 0x001fffff,
+	.features	= PHY_GBIT_FEATURES | SUPPORTED_Pause |
+			  SUPPORTED_Asym_Pause,
+	.flags		= PHY_HAS_INTERRUPT | PHY_HAS_MAGICANEG,
+	.config_init	= rtl8211f_config_init,
+	.config_aneg	= &genphy_config_aneg,
+	.read_status	= &genphy_read_status,
+	.suspend	= genphy_suspend,
+	.resume		= genphy_resume,
+	.driver		= { .owner = THIS_MODULE,},
+};
+
 static int __init realtek_init(void)
 {
 	int ret;
@@ -116,13 +176,17 @@ static int __init realtek_init(void)
 	ret = phy_driver_register(&rtl8211b_driver);
 	if (ret < 0)
 		return -ENODEV;
-	return phy_driver_register(&rtl8211e_driver);
+	ret = phy_driver_register(&rtl8211e_driver);
+	if (ret < 0)
+		return -ENODEV;
+	return phy_driver_register(&rtl8211f_driver);
 }
 
 static void __exit realtek_exit(void)
 {
 	phy_driver_unregister(&rtl8211b_driver);
 	phy_driver_unregister(&rtl8211e_driver);
+	phy_driver_unregister(&rtl8211f_driver);
 }
 
 module_init(realtek_init);
