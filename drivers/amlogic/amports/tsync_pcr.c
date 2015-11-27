5140d2d1783b589c90a61c305e26e2a067acc9aa
Zhenwu Che <zhenwu.che@amlogic.com>
2015-11-27 16:04:06 +0800

    PD#115820: amports: change delay time for DTV channel switch
    Change-Id: I1c139d0f352bae5e22e1ef739165cbd79ce50a67

--- drivers/amlogic/amports/tsync_pcr.c
+++ drivers/amlogic/amports/tsync_pcr.c
@@ -390,8 +390,7 @@ void tsync_pcr_pcrscr_set(void)
 	}
 
 	if (first_apts && !(tsync_pcr_inited_flag & complete_init_flag)
-		&& (first_apts < min_checkinpts
-		&& tsync_pcr_inited_mode >= INIT_PRIORITY_AUDIO)) {
+		&& (first_apts < min_checkinpts)) {
 		tsync_pcr_inited_flag |= TSYNC_PCR_INITCHECK_APTS;
 		ref_pcr = first_apts;
 		timestamp_pcrscr_set(ref_pcr);
@@ -414,8 +413,7 @@ void tsync_pcr_pcrscr_set(void)
 	}
 
 	if (first_vpts && !(tsync_pcr_inited_flag & complete_init_flag)
-		&& (first_vpts < min_checkinpts
-		&& tsync_pcr_inited_mode >= INIT_PRIORITY_VIDEO)) {
+		&& (first_vpts < min_checkinpts)) {
 		tsync_pcr_inited_flag |= TSYNC_PCR_INITCHECK_VPTS;
 		ref_pcr = first_vpts - tsync_pcr_ref_latency * 2;
 		timestamp_pcrscr_set(ref_pcr);
