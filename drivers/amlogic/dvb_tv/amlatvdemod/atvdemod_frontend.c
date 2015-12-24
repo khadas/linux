155fd8dcc52d79904e1066036395cdffde3a2525
Alex Deng <alex.deng@amlogic.com>
2015-12-24 10:34:16 +0800

    PD#115629: dvb: fixed issue atv agc is incorrect when resume back
    Change-Id: Ic79c2e53efc427735d866aae9b8349553077dd99

--- drivers/amlogic/dvb_tv/amlatvdemod/atvdemod_frontend.c
+++ drivers/amlogic/dvb_tv/amlatvdemod/atvdemod_frontend.c
@@ -352,6 +352,13 @@ void aml_atvdemod_set_params(struct dvb_frontend *fe,
 			amlatvdemod_devp->parm.if_freq = p->if_freq;
 			amlatvdemod_devp->parm.if_inv = p->if_inv;
 			amlatvdemod_devp->parm.tuner_id = p->tuner_id;
+			/* open AGC if needed */
+			if (amlatvdemod_devp->pin != NULL)
+				devm_pinctrl_put(amlatvdemod_devp->pin);
+			if (amlatvdemod_devp->pin_name)
+				amlatvdemod_devp->pin =
+				devm_pinctrl_get_select(amlatvdemod_devp->dev,
+						amlatvdemod_devp->pin_name);
 			atv_dmd_set_std();
 			pr_info("[amlatvdemod..]%s set std color %s, audio type %s.\n",
 				__func__,
