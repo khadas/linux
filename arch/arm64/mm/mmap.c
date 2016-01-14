82f8b69394ab294528f73086fe63a8485d8adc07
Daniel Cashman <dcashman@google.com>
2016-01-14 15:20:01 -0800

    PD#128865: arm64: mm: support ARCH_MMAP_RND_BITS
    arm64: arch_mmap_rnd() uses STACK_RND_MASK to generate the random offset
    for the mmap base address.  This value represents a compromise between
    increased ASLR effectiveness and avoiding address-space fragmentation.
    Replace it with a Kconfig option, which is sensibly bounded, so that
    platform developers may choose where to place this compromise.  Keep
    default values as new minimums.
    
    Change-Id: I66fc65609babe6add94184a526bc72cf836a45b6

--- arch/arm64/mm/mmap.c
+++ arch/arm64/mm/mmap.c
@@ -59,10 +59,16 @@ static unsigned long mmap_rnd(void)
 {
 	unsigned long rnd = 0;
 
-	if (current->flags & PF_RANDOMIZE)
-		rnd = (long)get_random_int() & (STACK_RND_MASK >> 1);
+#ifdef CONFIG_COMPAT
+	if (test_thread_flag(TIF_32BIT))
+		rnd = (unsigned long)get_random_int()
+			&((1 << mmap_rnd_compat_bits) - 1);
+	else
+#endif
+		rnd = (unsigned long)get_random_int()
+			& ((1 << mmap_rnd_bits) - 1);
 
-	return rnd << (PAGE_SHIFT + 1);
+	return rnd << PAGE_SHIFT;
 }
 
 static unsigned long mmap_base(void)
