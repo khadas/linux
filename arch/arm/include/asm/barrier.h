91966bfb1f22a2275e752e6c7cac9f387d82d86b
Qi Duan <qi.duan@amlogic.com>
2016-02-25 16:23:33 +0800

    PD#118626: arm: remove smp_mb__after_atomic warning
    Change-Id: I0077a44f5613225b1c4a9ad97c998c9f5fc19fb0

--- arch/arm/include/asm/barrier.h
+++ arch/arm/include/asm/barrier.h
@@ -79,5 +79,7 @@ do {									\
 
 #define set_mb(var, value)	do { var = value; smp_mb(); } while (0)
 
+#define smp_mb__before_atomic() smp_mb() /*mb*/
+#define smp_mb__after_atomic()  smp_mb() /*mb*/
 #endif /* !__ASSEMBLY__ */
 #endif /* __ASM_BARRIER_H */
