8875828d8159ca688864a3137cc736d919c58327
Hui Zhang <hui.zhang@amlogic.com>
2016-07-13 18:35:43 +0800

    PD#125585: vh264mvc: add cabac patch for gxm cpu or later
    Change-Id: Id449f9533ec9a7e64c7e8bfa291442ef1c06f064

--- drivers/amlogic/amports/arch/ucode/h264mvc/vh264mvc_mc.c
+++ drivers/amlogic/amports/arch/ucode/h264mvc/vh264mvc_mc.c
@@ -21,25 +21,49 @@
 #include "h264c_mvc_linux.h"
 
 #undef MicroCode
+#define MicroCode gxm_vh264mvc_mc
+#include "gxm_h264c_mvc_linux.h"
+
+#undef MicroCode
 #define MicroCode vh264mvc_header_mc
 #include "h264header_mvc_linux.h"
+
+#undef MicroCode
+#define MicroCode gxm_vh264mvc_header_mc
+#include "gxm_h264header_mvc_linux.h"
 
 #undef MicroCode
 #define MicroCode vh264mvc_mmco_mc
 #include "h264mmc_mvc_linux.h"
 
 #undef MicroCode
+#define MicroCode gxm_vh264mvc_mmco_mc
+#include "gxm_h264mmc_mvc_linux.h"
+
+#undef MicroCode
 #define MicroCode vh264mvc_slice_mc
 #include "h264slice_mvc_linux.h"
 
+#undef MicroCode
+#define MicroCode gxm_vh264mvc_slice_mc
+#include "gxm_h264slice_mvc_linux.h"
+
 #define FOR_VFORMAT VFORMAT_H264MVC
+
+#define DEF_FIRMEARE_FOR_GXM(n) \
+		REGISTER_FIRMARE_PER_CPU(MESON_CPU_MAJOR_ID_GXM,\
+		FOR_VFORMAT, n)
 
 #define REG_FIRMWARE_ALL()\
 	do {\
 		DEF_FIRMWARE(vh264mvc_mc);\
+		DEF_FIRMEARE_FOR_GXM(gxm_vh264mvc_mc);\
 		DEF_FIRMWARE(vh264mvc_header_mc);\
+		DEF_FIRMEARE_FOR_GXM(gxm_vh264mvc_header_mc);\
 		DEF_FIRMWARE(vh264mvc_mmco_mc);\
+		DEF_FIRMEARE_FOR_GXM(gxm_vh264mvc_mmco_mc);\
 		DEF_FIRMWARE(vh264mvc_slice_mc);\
+		DEF_FIRMEARE_FOR_GXM(gxm_vh264mvc_slice_mc);\
 	} while (0)
 
 INIT_DEF_FIRMWARE();
