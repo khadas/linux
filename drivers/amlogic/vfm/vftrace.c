06d6e3fc529637551c8b6eb0241d84fe15d01894
Zhi Zhou <zhi.zhou@amlogic.com>
2016-07-19 22:42:32 -0400

    PD#127098: vfm: debug: fix 32bit compile error
    Change-Id: I7b7e04c369c8c8ea620b2431129611e8a68f2014

--- drivers/amlogic/vfm/vftrace.c
+++ drivers/amlogic/vfm/vftrace.c
@@ -93,7 +93,7 @@ void vftrace_info_in(void *vhandle, struct vframe_s *vf)
 	info->type = vf->type;
 	info->vf = vf;
 	info->pts = vf->pts;
-	info->in_time_us = timeval_to_ns(&tv)/1000;
+	info->in_time_us = div64_u64(timeval_to_ns(&tv), 1000);
 	info->duration = info->duration;
 	vftrace->w_index++;
 	vftrace->num++;
