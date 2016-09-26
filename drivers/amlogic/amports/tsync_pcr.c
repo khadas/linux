bedd920f0f905978513963e6f19e2639acc98041
Pengfei Liu <pengfei.liu@amlogic.com>
2016-09-26 17:48:44 +0800

    PD#129919: tsync_pcr: adjust pcr_diff to fix dtv play not smooth
    Change-Id: I0e7080aa17cc19b260bd2d31661efd695a88a3f7
    Signed-off-by: Pengfei Liu <pengfei.liu@amlogic.com>

--- drivers/amlogic/amports/tsync_pcr.c
+++ drivers/amlogic/amports/tsync_pcr.c
@@ -123,8 +123,8 @@ static u32 tsync_pcr_discontinue_threshold = (TIME_UNIT90K * 1.5);
 static u32 tsync_pcr_ref_latency = (TIME_UNIT90K * 0.3);
 
 /* use for pcr valid mode */
-static u32 tsync_pcr_max_cache_time = TIME_UNIT90K * 1.5;
-static u32 tsync_pcr_up_cache_time = TIME_UNIT90K * 1.2;
+static u32 tsync_pcr_max_cache_time = TIME_UNIT90K * 2.4;
+static u32 tsync_pcr_up_cache_time = TIME_UNIT90K * 2.2;
 /* modify it by dolby av sync */
 static u32 tsync_pcr_down_cache_time = TIME_UNIT90K * 0.8;   /* 0.6 */
 static u32 tsync_pcr_min_cache_time = TIME_UNIT90K * 0.4;    /* 0.2 */
@@ -195,6 +195,18 @@ static int vbuf_fatal_error;
 static DEFINE_SPINLOCK(tsync_pcr_lock);
 
 #define LTRACE() pr_info("[%s:%d]\n", __func__, __LINE__);
+
+module_param(tsync_pcr_max_cache_time , uint, 0664);
+MODULE_PARM_DESC(tsync_pcr_max_cache_time , "\n tsync pcr max cache time\n");
+
+module_param(tsync_pcr_up_cache_time , uint, 0664);
+MODULE_PARM_DESC(tsync_pcr_up_cache_time , "\n tsync pcr up cache time\n");
+
+module_param(tsync_pcr_down_cache_time , uint, 0664);
+MODULE_PARM_DESC(tsync_pcr_down_cache_time , "\n tsync pcr down cache time\n");
+
+module_param(tsync_pcr_min_cache_time , uint, 0664);
+MODULE_PARM_DESC(tsync_pcr_min_cache_time , "\n tsync pcr min cache time\n");
 
 u32 get_stbuf_rp(int type)
 {
@@ -1031,7 +1043,8 @@ static unsigned long tsync_pcr_check(void)
 		u64 ref_pcr = (u64)  tsdemux_pcr;
 		u64 cur_pcr = (u64) timestamp_pcrscr_get();
 		int64_t cur_delta = (int64_t) ref_pcr - (int64_t) cur_pcr;
-		int64_t diff = (cur_delta - tsync_pcr_stream_delta);
+		/* int64_t diff = (cur_delta - tsync_pcr_stream_delta); */
+		int64_t diff = cur_delta;
 
 		/* if(diff > OPEN_RECOVERY_THRESHOLD && cur_pcr<ref_pcr
 		   && play_mode!=PLAY_MODE_SPEED && need_recovery){ */
