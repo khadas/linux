01f9a4acecf1ceb878cd038cc45f6383ac00b65c
Long Yu <long.yu@amlogic.com>
2016-12-13 11:32:31 +0800

    PD#136502: emmc: add reset for emmc resume code
    Change-Id: I9d0c4549eb02c5c18bed17af8c3c7ef928f71ce8
    Signed-off-by: Long Yu <long.yu@amlogic.com>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -1745,7 +1745,7 @@ static int _mmc_resume(struct mmc_host *host)
 		goto out;
 
 	mmc_power_up(host, host->card->ocr);
-	err = mmc_init_card(host, host->card->ocr, host->card);
+	mmc_hw_reset(host);
 	mmc_card_clr_suspended(host->card);
 
 out:
