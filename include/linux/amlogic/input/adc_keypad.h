14ed3d7039ad07baee9723ec81f1d4c94248b40b
Xingyu Chen <xingyu.chen@amlogic.com>
2017-03-14 11:06:43 +0800

    PD#140401: adc_keypad: add sysfs interface for setting dynamic key
    1) add sysfs interface
    2) store key into list instead of array
    
    Change-Id: I22d08b88b7ca42b10da4cc9bbda4e6a3f859adf3
    Signed-off-by: Xingyu Chen <xingyu.chen@amlogic.com>

--- include/linux/amlogic/input/adc_keypad.h
+++ include/linux/amlogic/input/adc_keypad.h
@@ -1,19 +1,53 @@
 #ifndef __LINUX_ADC_KEYPAD_H
 #define __LINUX_ADC_KEYPAD_H
+#include <linux/list.h>
+#include <linux/input.h>
+#include <linux/kobject.h>
+#include <linux/timer.h>
+#include <linux/workqueue.h>
+#include <linux/spinlock.h>
+#ifdef CONFIG_HAS_EARLYSUSPEND
+#include <linux/earlysuspend.h>
+#endif
 
-struct adc_key {
-	int code;	/* input key code */
-	const char *name;
-	int chan;
-	int value;	/* voltage/3.3v * 1023 */
-	int tolerance;
+#define DRIVE_NAME "adckey"
+#define MAX_NAME_LEN 20
+
+enum TOLERANCE_RANGE {
+	TOL_MIN = 0,
+	TOL_MAX = 255
 };
 
-struct adc_kp_platform_data {
-	struct adc_key *key;
-	int key_num;
-	int repeat_delay;
-	int repeat_period;
+enum SAMPLE_VALUE_RANGE {
+	SAM_MIN = 0,
+	SAM_MAX = 4095 /*12bit adc*/
+};
+
+struct adc_key {
+	char name[MAX_NAME_LEN];
+	unsigned int chan;
+	unsigned int code;  /* input key code */
+	int value; /* voltage/3.3v * 1023 */
+	int tolerance;
+	struct list_head list;
+};
+
+struct kp {
+	unsigned char chan[SARADC_CHAN_NUM];
+	unsigned char chan_num;   /*number of channel exclude duplicate*/
+	unsigned char count;
+	unsigned int report_code;
+	unsigned int code;
+	unsigned int poll_period; /*key scan period*/
+	spinlock_t kp_lock;
+	struct list_head adckey_head;
+	struct kobject *adckey_kobj;
+	struct input_dev *input;
+	struct timer_list timer;
+	struct work_struct work_update;
+#ifdef CONFIG_HAS_EARLYSUSPEND
+	struct early_suspend early_suspend;
+#endif
 };
 
 #endif
