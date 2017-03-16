29521824beff11d6685f0ed93288564abd1313ac
KeLe Bai <kele.bai@amlogic.com>
2017-03-16 16:04:10 +0800

    PD#139181: di: update di for txlx
    1. modify reverse func & if1,if2,deband gate for txlx.
    2. invert chan2 field num for pulldown.
    3. fix progressive nr2 size.
    
    Change-Id: Ic53368de7fed29db5bf3f5e66be77b995b55eb9c
    Signed-off-by: KeLe Bai <kele.bai@amlogic.com>

--- drivers/amlogic/deinterlace/deinterlace_hw.h
+++ drivers/amlogic/deinterlace/deinterlace_hw.h
@@ -1,9 +1,14 @@
 #ifndef _DI_HW_H
 #define _DI_HW_H
+enum gate_mode_e {
+	GATE_AUTO,
+	GATE_ON,
+	GATE_OFF,
+};
 void di_top_gate_control(bool enable);
 void di_pre_gate_control(bool enable);
 void di_post_gate_control(bool enable);
 void enable_di_pre_mif(bool enable);
-void enable_di_post_mif(bool enable);
+void enable_di_post_mif(enum gate_mode_e mode);
 void di_hw_uninit(void);
 #endif
