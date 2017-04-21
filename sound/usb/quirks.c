2087a4cdef33a4bbbe86dbf37fa25ef266c06109
Pengguang Zhu <pengguang.zhu@amlogic.com>
2017-04-21 11:43:25 +0800

    PD#142598: security: fix security issue CVE-2016-2184
    The create_fixed_stream_quirk function in
    sound/usb/quirks.c in the snd-usb-audio driver
    in the Linux kernel before 4.5.1 allows physically
    proximate attackers to cause a denial of service
    (NULL pointer dereference or double free, and
    system crash) via a crafted endpoints value in a
    USB device descriptor.
    
    Change-Id: I49df4eec8e31da0e2413c93f7ff521e3bb885cba
    Signed-off-by: Pengguang Zhu <pengguang.zhu@amlogic.com>

--- sound/usb/quirks.c
+++ sound/usb/quirks.c
@@ -138,8 +138,10 @@ static int create_fixed_stream_quirk(struct snd_usb_audio *chip,
 		snd_printk(KERN_ERR "cannot memdup\n");
 		return -ENOMEM;
 	}
+	INIT_LIST_HEAD(&fp->list);
 	if (fp->nr_rates > MAX_NR_RATES) {
 		kfree(fp);
+		list_del(&fp->list);
 		return -EINVAL;
 	}
 	if (fp->nr_rates > 0) {
@@ -147,6 +149,7 @@ static int create_fixed_stream_quirk(struct snd_usb_audio *chip,
 				     sizeof(int) * fp->nr_rates, GFP_KERNEL);
 		if (!rate_table) {
 			kfree(fp);
+			list_del(&fp->list);
 			return -ENOMEM;
 		}
 		fp->rate_table = rate_table;
@@ -158,12 +161,14 @@ static int create_fixed_stream_quirk(struct snd_usb_audio *chip,
 	if (err < 0) {
 		kfree(fp);
 		kfree(rate_table);
+		list_del(&fp->list);
 		return err;
 	}
 	if (fp->iface != get_iface_desc(&iface->altsetting[0])->bInterfaceNumber ||
 	    fp->altset_idx >= iface->num_altsetting) {
 		kfree(fp);
 		kfree(rate_table);
+		list_del(&fp->list);
 		return -EINVAL;
 	}
 	alts = &iface->altsetting[fp->altset_idx];
@@ -450,6 +455,7 @@ static int create_uaxx_quirk(struct snd_usb_audio *chip,
 	fp->ep_attr = get_endpoint(alts, 0)->bmAttributes;
 	fp->datainterval = 0;
 	fp->maxpacksize = le16_to_cpu(get_endpoint(alts, 0)->wMaxPacketSize);
+	INIT_LIST_HEAD(&fp->list);
 
 	switch (fp->maxpacksize) {
 	case 0x120:
@@ -473,6 +479,7 @@ static int create_uaxx_quirk(struct snd_usb_audio *chip,
 		? SNDRV_PCM_STREAM_CAPTURE : SNDRV_PCM_STREAM_PLAYBACK;
 	err = snd_usb_add_audio_stream(chip, stream, fp);
 	if (err < 0) {
+		list_del(&fp->list); /* unlink for avoiding double-free */
 		kfree(fp);
 		return err;
 	}
