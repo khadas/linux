da29019417a8867f2f99bdc067b3dc0d9c523aa2
Zan Peng <zan.peng@amlogic.com>
2017-05-03 10:50:33 +0800

    PD#143626: irblaster: optimizing irblaster driver
    1. add kernel thread to handle tx
    2. add fifo to save tx buffer
    3. add sysfs file
    
    cd /sys/class/meson-irblaster/irblaster1/debug
    echo enable > debug
    echo 900s450s30s40s50s40s > send
    
    Change-Id: Iafdb549d9ed193023dee4e889dfe0d7040fce259
    Signed-off-by: Zan Peng <zan.peng@amlogic.com>
    
    Conflicts:
    	arch/arm64/boot/dts/amlogic/mesongxl.dtsi
    
    fixup! PD#143626: irblaster: optimizing irblaster driver

--- drivers/amlogic/irblaster/irblaster.h
+++ drivers/amlogic/irblaster/irblaster.h
@@ -18,23 +18,28 @@
 #ifndef AM_IRBLASTER_H
 #define AM_IRBLASTER_H
 #define MAX_PLUSE 1024
-struct aml_blaster {
-	unsigned int consumerir_freqs;
-	unsigned int pattern[MAX_PLUSE];
-	unsigned int pattern_len;
-	unsigned int fisrt_pulse_width;
-};
+
 enum  {	/* Modulation level*/
 	fisrt_low  = 0,
 	fisrt_high = 1
 };
+
+struct blaster_window {
+	unsigned int winNum;
+	unsigned int winArray[MAX_PLUSE];
+	int consumerir_freqs;
+	unsigned int consumerir_dutycycle;
+	struct mutex lock;
+};
+
 #define AO_IR_BLASTER_ADDR0 ((0x00 << 10) | (0x30 << 2))
 #define AO_IR_BLASTER_ADDR1 ((0x00 << 10) | (0x31 << 2))
 #define AO_IR_BLASTER_ADDR2 ((0x00 << 10) | (0x32 << 2))
-#define AO_RTI_PIN_MUX_REG (0x14)
+#define AO_RTI_PIN_MUX_REG		(0x14)
+#define AO_RTI_GEN_CTNL_REG0	(0x40)
 
 #define CONSUMERIR_TRANSMIT     0x5500
 #define GET_CARRIER         0x5501
 #define SET_CARRIER         0x5502
-#define SET_MODLEVEL         0x5503
+#define SET_DUTYCYCLE         0x5503
 #endif
