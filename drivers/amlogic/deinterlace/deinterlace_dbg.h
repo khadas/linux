883005e4206b5b9c451522a1cbd853fa8166d0f2
KeLe Bai <kele.bai@amlogic.com>
2017-05-12 17:21:43 +0800

    PD#143982: di: pd enable for reg configuration in auto gate mode
    Change-Id: I18ff9c6b65dc924d89fb69491636d043460e58b5
    Signed-off-by: KeLe Bai <kele.bai@amlogic.com>

---
+++ drivers/amlogic/deinterlace/deinterlace_dbg.h
@@ -0,0 +1,22 @@
+/* * Amlogic M2
+ * frame buffer driver-----------Deinterlace
+ * Author: Rain Zhang <rain.zhang@amlogic.com>
+ * Copyright (C) 2010 Amlogic, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the named License,
+ * or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __DI_DBG_H
+#define __DI_DBG_H
+#include "register.h"
+void dump_di_reg(void);
+#endif
