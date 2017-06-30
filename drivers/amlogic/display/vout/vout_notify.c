7970afcfe00fe405160ac059ace0a134b39f2dcf
Evoke Zhang <evoke.zhang@amlogic.com>
2017-06-30 18:55:47 +0800

    PD#146807: vout: optimize vout serve framerate_automation API
    framerate automation only set current vout server,
    don't change all of the servers' farmerate in registered list.
    
    Change-Id: Ic629f6b2625c4e21331c9fc478a227f5e51afe87
    Signed-off-by: Evoke Zhang <evoke.zhang@amlogic.com>

--- drivers/amlogic/display/vout/vout_notify.c
+++ drivers/amlogic/display/vout/vout_notify.c
@@ -159,12 +159,12 @@ EXPORT_SYMBOL(get_vsource_fps);
 int set_vframe_rate_hint(int duration)
 {
 	int r = -1;
-	struct vout_server_s  *p_server;
+	struct vout_server_s  *p_server = vout_module.curr_vout_server;
 
 	/* mutex_lock(&vout_mutex); */
-	list_for_each_entry(p_server, &vout_module.vout_server_list, list) {
+	if (p_server) {
 		if ((p_server->op.set_vframe_rate_hint != NULL) &&
-		    (p_server->op.set_vframe_rate_hint(duration) == 0)) {
+			(p_server->op.set_vframe_rate_hint(duration) == 0)) {
 			/* mutex_unlock(&vout_mutex); */
 			return 0;
 		}
@@ -182,12 +182,12 @@ EXPORT_SYMBOL(set_vframe_rate_hint);
 int set_vframe_rate_end_hint(void)
 {
 	int r = -1;
-	struct vout_server_s  *p_server;
+	struct vout_server_s  *p_server = vout_module.curr_vout_server;
 
 	/* mutex_lock(&vout_mutex); */
-	list_for_each_entry(p_server, &vout_module.vout_server_list, list) {
+	if (p_server) {
 		if ((p_server->op.set_vframe_rate_end_hint != NULL) &&
-		    (p_server->op.set_vframe_rate_end_hint() == 0)) {
+			(p_server->op.set_vframe_rate_end_hint() == 0)) {
 			/* mutex_unlock(&vout_mutex); */
 			return 0;
 		}
@@ -205,12 +205,12 @@ EXPORT_SYMBOL(set_vframe_rate_end_hint);
 int set_vframe_rate_policy(int policy)
 {
 	int r = -1;
-	struct vout_server_s  *p_server;
+	struct vout_server_s  *p_server = vout_module.curr_vout_server;
 
 	/* mutex_lock(&vout_mutex); */
-	list_for_each_entry(p_server, &vout_module.vout_server_list, list) {
+	if (p_server) {
 		if ((p_server->op.set_vframe_rate_policy != NULL) &&
-		    (p_server->op.set_vframe_rate_policy(policy) == 0)) {
+			(p_server->op.set_vframe_rate_policy(policy) == 0)) {
 			/* mutex_unlock(&vout_mutex); */
 			return 0;
 		}
@@ -228,10 +228,10 @@ EXPORT_SYMBOL(set_vframe_rate_policy);
 int get_vframe_rate_policy(void)
 {
 	int r = -1;
-	struct vout_server_s  *p_server;
+	struct vout_server_s  *p_server = vout_module.curr_vout_server;
 
 	/* mutex_lock(&vout_mutex); */
-	list_for_each_entry(p_server, &vout_module.vout_server_list, list) {
+	if (p_server) {
 		if (p_server->op.get_vframe_rate_policy != NULL) {
 			r = p_server->op.get_vframe_rate_policy();
 			/* mutex_unlock(&vout_mutex); */
