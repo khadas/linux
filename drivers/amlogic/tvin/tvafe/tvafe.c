e2bb318772e5c3ae8cd38615a00801ba261c5fab
Nian Jing <nian.jing@amlogic.com>
2017-08-14 20:39:29 +0800

    PD#149284: tvafe: only txlx enable avdetect config
    Change-Id: I08d985c3f12907da8243214e74bcf5ead89c5e62
    Signed-off-by: Nian Jing <nian.jing@amlogic.com>

--- drivers/amlogic/tvin/tvafe/tvafe.c
+++ drivers/amlogic/tvin/tvafe/tvafe.c
@@ -48,7 +48,9 @@
 #include "tvafe_cvd.h"
 #include "tvafe_general.h"
 #include "tvafe.h"
+#ifdef CONFIG_TVIN_AVIN_DETECT
 #include "tvafe_avin_detect.h"
+#endif
 #include "../vdin/vdin_sm.h"
 
 
@@ -881,16 +883,21 @@ int tvafe_dec_open(struct tvin_frontend_s *fe, enum tvin_port_e port)
 		mutex_unlock(&devp->afe_mutex);
 		return 1;
 	}
-	/*synctip set to 0 when tvafe working*/
-	if (port == TVIN_PORT_CVBS1) {
-		/*channel1*/
-		tvafe_cha1_SYNCTIP_close_config();
-	} else if (port == TVIN_PORT_CVBS2) {
-		/*channel2*/
-		tvafe_cha2_SYNCTIP_close_config();
-	} else {
-		tvafe_cha1_2_SYNCTIP_close_config();
+#ifdef CONFIG_TVIN_AVIN_DETECT
+	/*only txlx chip enabled*/
+	if (is_meson_txlx_cpu()) {
+		/*synctip set to 0 when tvafe working*/
+		if (port == TVIN_PORT_CVBS1) {
+			/*channel1*/
+			tvafe_cha1_SYNCTIP_close_config();
+		} else if (port == TVIN_PORT_CVBS2) {
+			/*channel2*/
+			tvafe_cha2_SYNCTIP_close_config();
+		} else {
+			tvafe_cha1_2_SYNCTIP_close_config();
+		}
 	}
+#endif
 #ifdef CONFIG_CMA
 	tvafe_cma_alloc(devp);
 #endif
@@ -1097,17 +1104,21 @@ void tvafe_dec_close(struct tvin_frontend_s *fe)
 #ifdef CONFIG_CMA
 	tvafe_cma_release(devp);
 #endif
-	/*avsync tip set 1 to resume av detect*/
-	if (tvafe->parm.port == TVIN_PORT_CVBS1) {
-		/*channel1*/
-		tvafe_cha1_detect_restart_config();
-	} else if (tvafe->parm.port == TVIN_PORT_CVBS2) {
-		/*channel2*/
-		tvafe_cha2_detect_restart_config();
-	} else {
-		tvafe_cha1_detect_restart_config();
-		tvafe_cha2_detect_restart_config();
+#ifdef CONFIG_TVIN_AVIN_DETECT
+	if (is_meson_txlx_cpu()) {
+		/*avsync tip set 1 to resume av detect*/
+		if (tvafe->parm.port == TVIN_PORT_CVBS1) {
+			/*channel1*/
+			tvafe_cha1_detect_restart_config();
+		} else if (tvafe->parm.port == TVIN_PORT_CVBS2) {
+			/*channel2*/
+			tvafe_cha2_detect_restart_config();
+		} else {
+			tvafe_cha1_detect_restart_config();
+			tvafe_cha2_detect_restart_config();
+		}
 	}
+#endif
 	/* init variable */
 	memset(tvafe, 0, sizeof(struct tvafe_info_s));
 
