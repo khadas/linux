6410c5bc329f89daffe8e0f48ece77ad05652465
Wenping Zhang <wenping.zhang@rock-chips.com>
2018-06-20 14:56:37 +0800

    power: bq25700: fix crash issue duaring probe bq25700 driver.
    In previous code, driver will update the power supply status before
    power_supply_register, so it will cause kernel panic. So we put the
    bq25700_power_supply_init before bq25700_init_usb to fix this issue.
    
    Change-Id: Id9d206dbecfdadbd968b5c9d24f85f66ef321b9d
    Signed-off-by: Wenping Zhang <wenping.zhang@rock-chips.com>

--- drivers/power/bq25700_charger.c
+++ drivers/power/bq25700_charger.c
@@ -2003,9 +2003,10 @@ static int bq25700_probe(struct i2c_client *client,
 	}
 
 	bq25700_parse_dt(charger);
-
-	bq25700_init_usb(charger);
 	bq25700_init_sysfs(charger);
+
+	bq25700_power_supply_init(charger);
+	bq25700_init_usb(charger);
 
 	if (client->irq < 0) {
 		dev_err(dev, "No irq resource found.\n");
@@ -2027,7 +2028,6 @@ static int bq25700_probe(struct i2c_client *client,
 		goto irq_fail;
 	enable_irq_wake(client->irq);
 
-	bq25700_power_supply_init(charger);
 	bq25700_charger = charger;
 
 irq_fail:
