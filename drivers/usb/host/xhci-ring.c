7e3824a8c1064727acf42ca76e6577315b632a43
William Wu <william.wu@rock-chips.com>
2019-01-04 11:22:54 +0800

    usb: xhci: set xhci trb ent quirk based on platform data
    If an xhci platform needs to enable the ENT flag in the TRB
    to force the xHC to pre-fetch the next TRB of a TD, then
    add the XHCI_TRB_ENT_QUIRK flag.
    
    Change-Id: Ib7cc095a848f0846ad995529ad703ae4e4ee4d44
    Signed-off-by: William Wu <william.wu@rock-chips.com>

--- drivers/usb/host/xhci-ring.c
+++ drivers/usb/host/xhci-ring.c
@@ -3228,6 +3228,8 @@ static int queue_bulk_sg_tx(struct xhci_hcd *xhci, gfp_t mem_flags,
 		 */
 		if (num_trbs > last_trb_num) {
 			field |= TRB_CHAIN;
+			if (xhci->quirks & XHCI_TRB_ENT_QUIRK)
+				field |= TRB_ENT;
 		} else if (num_trbs == last_trb_num) {
 			td->last_trb = ep_ring->enqueue;
 			field |= TRB_IOC;
@@ -3404,6 +3406,8 @@ int xhci_queue_bulk_tx(struct xhci_hcd *xhci, gfp_t mem_flags,
 		 */
 		if (num_trbs > last_trb_num) {
 			field |= TRB_CHAIN;
+			if (xhci->quirks & XHCI_TRB_ENT_QUIRK)
+				field |= TRB_ENT;
 		} else if (num_trbs == last_trb_num) {
 			td->last_trb = ep_ring->enqueue;
 			field |= TRB_IOC;
