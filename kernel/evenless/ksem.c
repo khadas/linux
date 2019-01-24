5cd0c5e7d877a2429cd1ec2daef060b4c2519513
Philippe Gerum <rpm@xenomai.org>
2019-01-24 19:03:11 +0100

    evl: add ksem API
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

---
+++ kernel/evenless/ksem.c
@@ -0,0 +1,78 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0
+ *
+ * Copyright (C) 2019 Philippe Gerum  <rpm@xenomai.org>
+ */
+
+#include <evenless/timer.h>
+#include <evenless/synch.h>
+#include <evenless/clock.h>
+#include <evenless/sched.h>
+#include <evenless/ksem.h>
+
+void evl_init_sem(struct evl_ksem *sem, unsigned int value)
+{
+	sem->value = value;
+	evl_init_syn(&sem->wait_queue, EVL_SYN_PRIO,
+		     &evl_mono_clock, NULL);
+}
+EXPORT_SYMBOL_GPL(evl_init_sem);
+
+void evl_destroy_sem(struct evl_ksem *sem)
+{
+	evl_destroy_syn(&sem->wait_queue);
+}
+EXPORT_SYMBOL_GPL(evl_destroy_sem);
+
+static int down_sem(struct evl_ksem *sem,
+		    ktime_t timeout, enum evl_tmode tmode)
+{
+	unsigned long flags;
+	int ret = 0;
+
+	xnlock_get_irqsave(&nklock, flags);
+
+	if (sem->value > 0)
+		--sem->value;
+	else if (timeout_nonblock(timeout))
+		ret = -EWOULDBLOCK;
+	else {
+		ret = evl_sleep_on_syn(&sem->wait_queue, timeout, tmode);
+		if (ret & T_TIMEO)
+			ret = -ETIMEDOUT;
+		else if (ret & T_RMID)
+			ret = -EIDRM;
+		else if (ret & T_BREAK)
+			ret = -EINTR;
+	}
+
+	xnlock_put_irqrestore(&nklock, flags);
+
+	return ret;
+}
+
+int evl_down_timeout(struct evl_ksem *sem, ktime_t timeout)
+{
+	return down_sem(sem, timeout, EVL_ABS);
+}
+EXPORT_SYMBOL_GPL(evl_down_timeout);
+
+int evl_down(struct evl_ksem *sem)
+{
+	return down_sem(sem, EVL_INFINITE, EVL_REL);
+}
+EXPORT_SYMBOL_GPL(evl_down);
+
+void evl_up(struct evl_ksem *sem)
+{
+	unsigned long flags;
+
+	if (evl_wake_up_syn(&sem->wait_queue))
+		evl_schedule();
+	else {
+		xnlock_get_irqsave(&nklock, flags);
+		sem->value++;
+		xnlock_put_irqrestore(&nklock, flags);
+	}
+}
+EXPORT_SYMBOL_GPL(evl_up);
