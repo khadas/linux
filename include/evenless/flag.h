25254dfe38e02bdd9490dd877706be1fec363b0b
Philippe Gerum <rpm@xenomai.org>
2019-02-10 12:23:13 +0100

    evl/flag: switch to broadcast semantics
    All waiters are unblocked upon signaling.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- include/evenless/flag.h
+++ include/evenless/flag.h
@@ -12,19 +12,19 @@
 
 struct evl_flag {
 	struct evl_wait_queue wait;
-	bool signaled;
+	bool raised;
 };
 
 #define DEFINE_EVL_FLAG(__name)					\
 	struct evl_flag __name = {				\
 		.wait = EVL_WAIT_INITIALIZER((__name).wait),	\
-		.signaled = false,				\
+		.raised = false,				\
 	}
 
 static inline void evl_init_flag(struct evl_flag *wf)
 {
 	wf->wait = (struct evl_wait_queue)EVL_WAIT_INITIALIZER(wf->wait);
-	wf->signaled = false;
+	wf->raised = false;
 }
 
 static inline void evl_destroy_flag(struct evl_flag *wf)
@@ -34,8 +34,8 @@ static inline void evl_destroy_flag(struct evl_flag *wf)
 
 static inline bool evl_read_flag(struct evl_flag *wf)
 {
-	if (wf->signaled) {
-		wf->signaled = false;
+	if (wf->raised) {
+		wf->raised = false;
 		return true;
 	}
 
@@ -61,18 +61,32 @@ struct evl_thread *evl_wait_flag_head(struct evl_flag *wf)
 	return evl_wait_head(&wf->wait);
 }
 
-static inline bool evl_raise_flag(struct evl_flag *wf)
+static inline void evl_raise_flag_nosched(struct evl_flag *wf)
 {
-	struct evl_thread *waiter;
 	unsigned long flags;
 
 	xnlock_get_irqsave(&nklock, flags);
-	wf->signaled = true;
-	waiter = evl_wake_up_head(&wf->wait);
+	wf->raised = true;
+	evl_flush_wait_locked(&wf->wait, T_BCAST);
 	xnlock_put_irqrestore(&nklock, flags);
-	evl_schedule();
+}
 
-	return waiter != NULL;
+static inline void evl_raise_flag(struct evl_flag *wf)
+{
+	evl_raise_flag_nosched(wf);
+	evl_schedule();
+}
+
+static inline void evl_pulse_flag(struct evl_flag *wf)
+{
+	evl_flush_wait(&wf->wait, T_BCAST);
+	evl_schedule();
+}
+
+static inline void evl_flush_flag(struct evl_flag *wf, int reason)
+{
+	evl_flush_wait(&wf->wait, reason);
+	evl_schedule();
 }
 
 #endif /* _EVENLESS_FLAG_H */
