62b25b930ccc679fea1d4d4765d3fc8ca131094b
Philippe Gerum <rpm@xenomai.org>
2019-03-03 16:40:34 +0100

    evl/ksem: switch to evl_sem
    Now that we don't have the evl_sem element in kernel space anymore,
    convert evl_ksem from the kernel API to evl_sem.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- kernel/evenless/sem.c
+++ kernel/evenless/sem.c
@@ -8,22 +8,22 @@
 #include <evenless/wait.h>
 #include <evenless/clock.h>
 #include <evenless/sched.h>
-#include <evenless/ksem.h>
+#include <evenless/sem.h>
 
-void evl_init_sem(struct evl_ksem *sem, unsigned int value)
+void evl_init_sem(struct evl_sem *sem, unsigned int value)
 {
 	sem->value = value;
 	evl_init_wait(&sem->wait_queue, &evl_mono_clock, EVL_WAIT_PRIO);
 }
 EXPORT_SYMBOL_GPL(evl_init_sem);
 
-void evl_destroy_sem(struct evl_ksem *sem)
+void evl_destroy_sem(struct evl_sem *sem)
 {
 	evl_destroy_wait(&sem->wait_queue);
 }
 EXPORT_SYMBOL_GPL(evl_destroy_sem);
 
-static bool down_sem(struct evl_ksem *sem)
+static bool down_sem(struct evl_sem *sem)
 {
 	if (sem->value > 0) {
 		--sem->value;
@@ -33,21 +33,21 @@ static bool down_sem(struct evl_ksem *sem)
 	return false;
 }
 
-int evl_down_timeout(struct evl_ksem *sem, ktime_t timeout)
+int evl_down_timeout(struct evl_sem *sem, ktime_t timeout)
 {
 	return evl_wait_event_timeout(&sem->wait_queue, timeout,
 				EVL_ABS, down_sem(sem));
 }
 EXPORT_SYMBOL_GPL(evl_down_timeout);
 
-int evl_down(struct evl_ksem *sem)
+int evl_down(struct evl_sem *sem)
 {
 	return evl_wait_event_timeout(&sem->wait_queue, EVL_INFINITE,
 				EVL_REL, down_sem(sem));
 }
 EXPORT_SYMBOL_GPL(evl_down);
 
-int evl_trydown(struct evl_ksem *sem)
+int evl_trydown(struct evl_sem *sem)
 {
 	unsigned long flags;
 	bool ret;
@@ -60,7 +60,7 @@ int evl_trydown(struct evl_ksem *sem)
 }
 EXPORT_SYMBOL_GPL(evl_trydown);
 
-void evl_up(struct evl_ksem *sem)
+void evl_up(struct evl_sem *sem)
 {
 	unsigned long flags;
 
