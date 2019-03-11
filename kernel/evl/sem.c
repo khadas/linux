bb276ce2403be5d7b48edb6dd5867789a155bc33
Philippe Gerum <rpm@xenomai.org>
2019-03-11 16:40:37 +0100

    evl/sem: rename to evl_ksem for consistency with evl_kmutex
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- kernel/evl/sem.c
+++ kernel/evl/sem.c
@@ -10,64 +10,64 @@
 #include <evl/sched.h>
 #include <evl/sem.h>
 
-void evl_init_sem(struct evl_sem *sem, unsigned int value)
+void evl_init_ksem(struct evl_ksem *ksem, unsigned int value)
 {
-	sem->value = value;
-	evl_init_wait(&sem->wait_queue, &evl_mono_clock, EVL_WAIT_PRIO);
+	ksem->value = value;
+	evl_init_wait(&ksem->wait_queue, &evl_mono_clock, EVL_WAIT_PRIO);
 }
-EXPORT_SYMBOL_GPL(evl_init_sem);
+EXPORT_SYMBOL_GPL(evl_init_ksem);
 
-void evl_destroy_sem(struct evl_sem *sem)
+void evl_destroy_ksem(struct evl_ksem *ksem)
 {
-	evl_destroy_wait(&sem->wait_queue);
+	evl_destroy_wait(&ksem->wait_queue);
 }
-EXPORT_SYMBOL_GPL(evl_destroy_sem);
+EXPORT_SYMBOL_GPL(evl_destroy_ksem);
 
-static bool down_sem(struct evl_sem *sem)
+static bool down_ksem(struct evl_ksem *ksem)
 {
-	if (sem->value > 0) {
-		--sem->value;
+	if (ksem->value > 0) {
+		--ksem->value;
 		return true;
 	}
 
 	return false;
 }
 
-int evl_down_timeout(struct evl_sem *sem, ktime_t timeout)
+int evl_down_timeout(struct evl_ksem *ksem, ktime_t timeout)
 {
-	return evl_wait_event_timeout(&sem->wait_queue, timeout,
-				EVL_ABS, down_sem(sem));
+	return evl_wait_event_timeout(&ksem->wait_queue, timeout,
+				EVL_ABS, down_ksem(ksem));
 }
 EXPORT_SYMBOL_GPL(evl_down_timeout);
 
-int evl_down(struct evl_sem *sem)
+int evl_down(struct evl_ksem *ksem)
 {
-	return evl_wait_event_timeout(&sem->wait_queue, EVL_INFINITE,
-				EVL_REL, down_sem(sem));
+	return evl_wait_event_timeout(&ksem->wait_queue, EVL_INFINITE,
+				EVL_REL, down_ksem(ksem));
 }
 EXPORT_SYMBOL_GPL(evl_down);
 
-int evl_trydown(struct evl_sem *sem)
+int evl_trydown(struct evl_ksem *ksem)
 {
 	unsigned long flags;
 	bool ret;
 
 	xnlock_get_irqsave(&nklock, flags);
-	ret = down_sem(sem);
+	ret = down_ksem(ksem);
 	xnlock_put_irqrestore(&nklock, flags);
 
 	return ret ? 0 : -EAGAIN;
 }
 EXPORT_SYMBOL_GPL(evl_trydown);
 
-void evl_up(struct evl_sem *sem)
+void evl_up(struct evl_ksem *ksem)
 {
 	unsigned long flags;
 
 	xnlock_get_irqsave(&nklock, flags);
 
-	if (!evl_wake_up_head(&sem->wait_queue))
-		sem->value++;
+	if (!evl_wake_up_head(&ksem->wait_queue))
+		ksem->value++;
 
 	xnlock_put_irqrestore(&nklock, flags);
 
