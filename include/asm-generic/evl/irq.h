f4ece5cb2a60cef2c9d3fd1f234ae038310c62b1
Philippe Gerum <rpm@xenomai.org>
2019-08-03 16:37:26 +0200

    evl: fix build w/ CONFIG_EVL disabled
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- include/asm-generic/evl/irq.h
+++ include/asm-generic/evl/irq.h
@@ -6,12 +6,16 @@
 
 static inline void irq_enter_pipeline(void)
 {
+#ifdef CONFIG_EVL
 	evl_enter_irq();
+#endif
 }
 
 static inline void irq_exit_pipeline(void)
 {
+#ifdef CONFIG_EVL
 	evl_exit_irq();
+#endif
 }
 
 #endif /* !_ASM_GENERIC_EVL_IRQ_H */
--- include/asm-generic/evl/thread_info.h
+++ include/asm-generic/evl/thread_info.h
@@ -2,6 +2,8 @@
 #ifndef _ASM_GENERIC_EVL_THREAD_INFO_H
 #define _ASM_GENERIC_EVL_THREAD_INFO_H
 
+#ifdef CONFIG_EVL
+
 struct evl_thread;
 
 struct oob_thread_state {
@@ -15,5 +17,16 @@ void evl_init_thread_state(struct oob_thread_state *p)
 	p->thread = NULL;
 	p->preempt_count = 0;
 }
+
+#else
+
+struct oob_thread_state { };
+
+static inline
+void evl_init_thread_state(struct oob_thread_state *p)
+{
+}
+
+#endif	/* !CONFIG_EVL */
 
 #endif /* !_ASM_GENERIC_EVL_THREAD_INFO_H */
