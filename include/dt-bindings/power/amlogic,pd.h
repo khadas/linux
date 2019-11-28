d510ed10fb5fb69e52228622d36d4273d6eaca5d
zhiqiang liang <zhiqiang.liang@amlogic.com>
2019-11-28 15:30:30 +0800

    power: add the power domain control API [1/1]
    PD#SWPL-17563
    
    Problem:
    add power domain control API for TM2 and SM1
    
    Solution:
    add power domain control API for TM2 and SM1
    
    Verify:
    T962E2
    
    Change-Id: I2587b2b554281ee7c81d77e8978a2640e5f73be5
    Signed-off-by: zhiqiang liang <zhiqiang.liang@amlogic.com>
    Signed-off-by: chunlong.cao <chunlong.cao@amlogic.com>

---
+++ include/dt-bindings/power/amlogic,pd.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
+/*
+ * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
+ */
+
+#define	PM_DOS_HCODEC		0
+#define	PM_DOS_VDEC		1
+#define	PM_DOS_HEVC		2
+#define	PM_WAVE420L		3
+#define	PM_CSI			6
+#define	PM_VPU			8
+#define	PM_NN			16
+#define	PM_USB			17
+#define	PM_PCIE0		18
+#define	PM_GE2D			19
+#define	PM_PCIE1		20
+#define	PM_DSPA			21
+#define	PM_DSPB			22
+#define	PM_DEMOD		23
