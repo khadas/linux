55f72a932c9f51048b4c302adf1703135da7fa5c
zhiqiang liang <zhiqiang.liang@amlogic.com>
2019-11-28 15:30:30 +0800

    power: add the power domain control API [1/1]
    PD#SWPL-17563
    
    Problem:
    add power domain control API for TM2 and SM1
    
    Solution:
    add power domain control API for TM2 and SM1
    
    Verify:
    T962E2
    
    Change-Id: I2587b2b554281ee7c81d77e8978a2640e5f73be5
    Signed-off-by: zhiqiang liang <zhiqiang.liang@amlogic.com>
    Signed-off-by: chunlong.cao <chunlong.cao@amlogic.com>

---
+++ include/linux/amlogic/power_domain.h
@@ -0,0 +1,22 @@
+/*
+ * include/linux/amlogic/power_domain.h
+ *
+ * Copyright (C) 2017 Amlogic, Inc. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ */
+
+#define PWR_ON	0
+#define PWR_OFF	1
+
+void power_domain_switch(int pwr_domain, bool pwr_switch);
+
