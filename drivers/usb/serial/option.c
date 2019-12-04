c1f3eb27790e9251ae7b8948b8e8ec4e31b3b7f0
goenjoy <goenjoy@khadas.com>
2019-12-04 17:02:34 +0800

    add Quectel 4G module (em06) support

--- drivers/usb/serial/option.c
+++ drivers/usb/serial/option.c
@@ -2012,6 +2012,9 @@ static int option_probe(struct usb_serial *serial,
 	    dev_desc->idProduct == cpu_to_le16(SAMSUNG_PRODUCT_GT_B3730) &&
 	    iface_desc->bInterfaceClass != USB_CLASS_CDC_DATA)
 		return -ENODEV;
+if (serial->dev->descriptor.idVendor == cpu_to_le16(0x2C7C)
+&& serial->interface->cur_altsetting->desc.bInterfaceNumber >= 4)
+return -ENODEV;
 
 	/* Store the device flags so we can use them during attach. */
 	usb_set_serial_data(serial, (void *)device_flags);
