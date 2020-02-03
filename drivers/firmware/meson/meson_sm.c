a548b953a19258226ccc38266df60e64bc2c2140
Jiamin Ma <jiamin.ma@amlogic.com>
2020-02-03 12:13:39 +0800

    dts: amlogic: some improvements [1/1]
    PD#SWPL-19600
    
    Problem:
    Some dts nodes are not quite suitable
    
    Solution:
    Some improvements
    
    Verify:
    AC214
    
    1. use macros instead of magic numbers for the interrupts
       property
    2. remove autocap_reserved node
    3. move ddr_bandwidth node to g12-common.dtsi
    4. comments hdmitx && cec nodes introduced by baylibre
    5. make secmon && secos nodes compatible with bootloader
    6. add key num property for the compatibality of bootloader
    
    Change-Id: Ib47d72e2cfabf31203378a39d5c7f53696bf7c2e
    Signed-off-by: Jiamin Ma <jiamin.ma@amlogic.com>

--- drivers/firmware/meson/meson_sm.c
+++ drivers/firmware/meson/meson_sm.c
@@ -20,6 +20,15 @@
 #include <linux/sizes.h>
  #include <linux/slab.h>
 
+#ifdef CONFIG_AMLOGIC_MODIFY
+#include <linux/dma-contiguous.h>
+#include <linux/cma.h>
+#include <linux/mm.h>
+#include <linux/of_reserved_mem.h>
+
+#define RESERVE_MEM_SIZE	0x300000
+#endif
+
 #include <linux/firmware/meson/meson_sm.h>
 
 struct meson_sm_cmd {
@@ -76,13 +85,24 @@ static u32 __meson_sm_call(u32 cmd, u32 arg0, u32 arg1, u32 arg2,
 	return res.a0;
 }
 
+#ifdef CONFIG_AMLOGIC_MODIFY
+static void __iomem *meson_sm_map_shmem(u32 cmd_shmem, unsigned int size,
+					int cma)
+#else
 static void __iomem *meson_sm_map_shmem(u32 cmd_shmem, unsigned int size)
+#endif
 {
 	u32 sm_phy_base;
 
 	sm_phy_base = __meson_sm_call(cmd_shmem, 0, 0, 0, 0, 0);
 	if (!sm_phy_base)
 		return 0;
+
+#ifdef CONFIG_AMLOGIC_MODIFY
+	/* mapped by kernel, physical address must in low mem */
+	if (cma)
+		return (void __iomem *)phys_to_virt(sm_phy_base);
+#endif
 
 	return ioremap_cache(sm_phy_base, size);
 }
@@ -266,22 +286,75 @@ static const struct of_device_id meson_sm_ids[] = {
 	{ /* sentinel */ },
 };
 
+#ifdef CONFIG_AMLOGIC_MODIFY
+static int __init secmon_cma_setup(struct platform_device *pdev)
+{
+	struct page *page;
+	int size;
+	struct device_node *np = pdev->dev.of_node;
+
+	if (of_property_read_u32(np, "reserve_mem_size", &size))
+		size = RESERVE_MEM_SIZE;
+
+	pr_info("reserve_mem_size:0x%x\n", size);
+
+	page = dma_alloc_from_contiguous(&pdev->dev, size >> PAGE_SHIFT, 0, 0);
+	if (!page) {
+		pr_err("%s, alloc page failed\n", __func__);
+		return -ENOMEM;
+	}
+	pr_info("%s, get page:%lx\n", __func__, page_to_pfn(page));
+
+	return 0;
+}
+#endif
+
 static int __init meson_sm_probe(struct platform_device *pdev)
 {
 	const struct meson_sm_chip *chip;
+#ifdef CONFIG_AMLOGIC_MODIFY
+	int cma = 0, ret;
+	void *tmp;
+#endif
 
 	chip = of_match_device(meson_sm_ids, &pdev->dev)->data;
 
+#ifdef CONFIG_AMLOGIC_MODIFY
+	ret = of_reserved_mem_device_init(&pdev->dev);
+	if (ret) {
+		pr_info("reserve memory init fail:%d\n", ret);
+		return ret;
+	}
+	pr_info("device cma:%lx\n", (unsigned long)pdev->dev.cma_area);
+	if (pdev->dev.cma_area) {
+		cma = 1;
+		if (secmon_cma_setup(pdev))
+			return -EINVAL;
+	}
+#endif
+
 	if (chip->cmd_shmem_in_base) {
+#ifdef CONFIG_AMLOGIC_MODIFY
+		tmp = meson_sm_map_shmem(chip->cmd_shmem_in_base,
+					 chip->shmem_size, cma);
+		fw.sm_shmem_in_base = tmp;
+#else
 		fw.sm_shmem_in_base = meson_sm_map_shmem(chip->cmd_shmem_in_base,
 							 chip->shmem_size);
+#endif
 		if (WARN_ON(!fw.sm_shmem_in_base))
 			goto out;
 	}
 
 	if (chip->cmd_shmem_out_base) {
+#ifdef CONFIG_AMLOGIC_MODIFY
+		tmp = meson_sm_map_shmem(chip->cmd_shmem_out_base,
+					 chip->shmem_size, cma);
+		fw.sm_shmem_out_base = tmp;
+#else
 		fw.sm_shmem_out_base = meson_sm_map_shmem(chip->cmd_shmem_out_base,
 							  chip->shmem_size);
+#endif
 		if (WARN_ON(!fw.sm_shmem_out_base))
 			goto out_in_base;
 	}
