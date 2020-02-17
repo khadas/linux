236b1ba2b4cfc748cde18daec824e1cae9af2d93
Jianxin Pan <jianxin.pan@amlogic.com>
2020-02-17 14:09:24 +0800

    defconfig: base defconfig for gki
    PD#SWPL-20810
    
    Problem:
    Need a base config for GKI
    
    Solution:
    Add a base config for GKI
    
    Verify:
    SM1
    
    Change-Id: I77700b0bff795759d9f19bb9467371598ca7c32e
    Signed-off-by: Jianxin Pan <jianxin.pan@amlogic.com>

---
+++ scripts/amlogic/mk_gki_meson64.sh
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: (GPL-2.0+ OR MIT)
+#
+# Copyright (c) 2019 Amlogic, Inc. All rights reserved.
+#
+
+export CROSS_COMPILE=/opt/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu/bin/aarch64-linux-gnu-
+ARCH=arm64 ./scripts/kconfig/merge_config.sh arch/arm64/configs/meson64_gki_defconfig arch/arm64/configs/meson64_gki.fragment 
+make ARCH=arm64 modules -j12
+make ARCH=arm64 Image -j12
+make ARCH=arm64 dtbs -j12
