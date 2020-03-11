a9378347c4b85e493d3b07dd3a77d4681e5ad0ba
Cao Jian <jian.cao@amlogic.com>
2020-03-11 14:28:04 +0800

    osd: add afbc flag for getting capbility [1/1]
    PD#SWPL-22030
    
    Problem:
    add afbc flag for getting capbility
    
    Solution:
    add this flag support
    
    Verify:
    on g12b w400
    
    Change-Id: I4f6efec85e3a62c484388821c24bbdd7eee06251
    Signed-off-by: Cao Jian <jian.cao@amlogic.com>

--- drivers/amlogic/media/osd/osd.h
+++ drivers/amlogic/media/osd/osd.h
@@ -151,9 +151,10 @@ enum color_index_e {
 #define MALI_AFBC_SPLIT_ON   1
 #define OSD_HW_CURSOR           (1 << 0)
 #define OSD_UBOOT_LOGO          (1 << 1)
-#define OSD_ZORDER			(1 << 2)
-#define OSD_PRIMARY		(1 << 3)
-#define OSD_FREESCALE		(1 << 4)
+#define OSD_ZORDER              (1 << 2)
+#define OSD_PRIMARY             (1 << 3)
+#define OSD_FREESCALE           (1 << 4)
+#define OSD_AFBC                (1 << 5)
 #define OSD_VIU2                (1 << 29)
 #define OSD_VIU1                (1 << 30)
 #define OSD_LAYER_ENABLE        (1 << 31)
