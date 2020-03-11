671835b5aa361f2b1b4bdc96672f52abaf33967d
Jianxin Pan <jianxin.pan@amlogic.com>
2020-03-11 15:19:50 +0800

    gki: more driver to KO [1/1]
    PD#SWPL-21900
    
    Problem:
    more drivers to ko
    
    Solution:
    MESON_CLK_MEASURE and other configs to tristate
    move some basic configs from meson64_gki_defconfig to meson64_gki.fragment
    ONFIG_HW_RANDOM_MESON=m
    CONFIG_I2C_MESON=m
    CONFIG_SPI_MESON_SPIFC=m
    CONFIG_AMLOGIC_TEE=m
    
    Verify:
    AC213
    
    Change-Id: I38df6d3d800cf809e525f9c36f9edba8d3cd521b
    Signed-off-by: Jianxin Pan <jianxin.pan@amlogic.com>

--- scripts/amlogic/mk_gki_meson64.sh
+++ scripts/amlogic/mk_gki_meson64.sh
@@ -4,7 +4,8 @@
 #
 
 export CROSS_COMPILE=/opt/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu/bin/aarch64-linux-gnu-
-ARCH=arm64 ./scripts/kconfig/merge_config.sh arch/arm64/configs/meson64_gki_defconfig arch/arm64/configs/meson64_gki.fragment 
+#ARCH=arm64 ./scripts/kconfig/merge_config.sh arch/arm64/configs/gki_defconfig arch/arm64/configs/meson64_gki.fragment
+ARCH=arm64 ./scripts/kconfig/merge_config.sh arch/arm64/configs/meson64_gki_defconfig arch/arm64/configs/meson64_gki.fragment
 make ARCH=arm64 Image -j12
 make ARCH=arm64 dtbs -j12
 make ARCH=arm64 modules -j12
