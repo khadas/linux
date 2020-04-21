97c3606e48489e8e50c77e8546a2924b8265fa0e
Philippe Gerum <rpm@xenomai.org>
2020-04-21 19:35:33 +0200

    evl/thread: add unblock, demote and yield operations
    - EVL_THRIOC_UNBLOCK releases the target thread from any ongoing wait
      on a resource (does not apply to forcible suspension).
    
    - EVL_THRIOC_DEMOTE unblocks and demotes the target thread to
      SCHED_WEAK.
    
    - EVL_THRIOC_YIELD performs sched_yield().
    
    In addition, EVL_THRIOC_SET/CLEAR_MODE are no more restricted to the
    current thread over oob, which did not make much sense. Calls from
    either in-band or oob are now allowed, and may target an arbitrary
    thread.
    
    These changes trigger a bump to ABI 22, although we keep on supporting
    21.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- include/uapi/evl/control.h
+++ include/uapi/evl/control.h
@@ -18,7 +18,7 @@
  * latter to the former. CAUTION: a litteral value is required for the
  * current ABI definition (scripts reading this may be naive).
  */
-#define EVL_ABI_LEVEL  21
+#define EVL_ABI_LEVEL  22
 
 #define EVL_CONTROL_DEV  "/dev/evl/control"
 
