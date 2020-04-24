a7fa371575befe0db7d9ddfcf27b87dd4d857b34
Ruixuan Li <ruixuan.li@amlogic.com>
2020-04-24 14:54:28 +0800

    emmc: add scan tx window node [1/1]
    PD#SWPL-25560
    
    Problem:
    tx delay is not appropirate
    
    Solution:
    add scan tx window node
    
    Verify:
    passed on s905c1
    
    Change-Id: I30894540f401d3bf38c21bd86c4efc320213f293
    Signed-off-by: Ruixuan Li <ruixuan.li@amlogic.com>
    Signed-off-by: chunlong.cao <chunlong.cao@amlogic.com>

--- drivers/amlogic/mmc/aml_sd_emmc.c
+++ drivers/amlogic/mmc/aml_sd_emmc.c
@@ -3357,6 +3357,10 @@ static int meson_mmc_probe(struct platform_device *pdev)
 		if (ret)
 			dev_warn(mmc_dev(host->mmc),
 					"Unable to creat sysfs attributes\n");
+		ret = device_create_file(&pdev->dev, &dev_attr_emmc_tx_window);
+		if (ret)
+			dev_warn(mmc_dev(host->mmc),
+					"Unable to creat sysfs attributes\n");
 	}
 	return 0;
 
