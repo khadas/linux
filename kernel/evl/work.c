3c723cca7d74a211826375ced2356e5bd234a0b7
Philippe Gerum <rpm@xenomai.org>
2020-04-25 15:21:24 +0200

    evl/work: add safe async call form
    We need a safe form of async workers to address the following issue:
    
    CPU0: evl_call_inband*(arg=some_element)
    CPU0:     irq_work_queue()
    CPU0:          queue_work()
    CPU1: release(element)
    CPU1:     dispose(element)
    CPU0:             work->handler(arg=some_element)
    CPU0:                 invalid access to stale 'some_element'
    
    evl_init_work_safe() guarantees integrity by holding a reference on
    the given element from when evl_call_inband*() is issued until the
    handler has completed.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- kernel/evl/work.c
+++ kernel/evl/work.c
@@ -4,6 +4,7 @@
  * Copyright (C) 2020 Philippe Gerum  <rpm@xenomai.org>
  */
 
+#include <evl/factory.h>
 #include <evl/work.h>
 
 static void do_wq_work(struct work_struct *wq_work)
@@ -12,6 +13,8 @@ static void do_wq_work(struct work_struct *wq_work)
 
 	work = container_of(wq_work, struct evl_work, wq_work);
 	work->handler(work);
+	if (work->element)
+		evl_put_element(work->element);
 }
 
 static void do_wq_work_sync(struct work_struct *wq_work)
@@ -28,7 +31,9 @@ static void do_irq_work(struct irq_work *irq_work)
 	struct evl_work *work;
 
 	work = container_of(irq_work, struct evl_work, irq_work);
-	queue_work(work->wq, &work->wq_work);
+
+	if (!queue_work(work->wq, &work->wq_work) && work->element)
+		evl_put_element(work->element);
 }
 
 void evl_init_work(struct evl_work *work,
@@ -37,8 +42,18 @@ void evl_init_work(struct evl_work *work,
 	init_irq_work(&work->irq_work, do_irq_work);
 	INIT_WORK(&work->wq_work, do_wq_work);
 	work->handler = (typeof(work->handler))handler;
+	work->element = NULL;
 }
 EXPORT_SYMBOL_GPL(evl_init_work);
+
+void evl_init_work_safe(struct evl_work *work,
+			void (*handler)(struct evl_work *work),
+			struct evl_element *element)
+{
+	evl_init_work(work, handler);
+	work->element = element;
+}
+EXPORT_SYMBOL_GPL(evl_init_work_safe);
 
 void evl_init_sync_work(struct evl_sync_work *sync_work,
 			int (*handler)(struct evl_sync_work *sync_work))
@@ -48,6 +63,11 @@ void evl_init_sync_work(struct evl_sync_work *sync_work,
 	init_irq_work(&work->irq_work, do_irq_work);
 	INIT_WORK(&work->wq_work, do_wq_work_sync);
 	work->handler = (typeof(work->handler))handler;
+	/*
+	* No point in holding a safety reference since the caller
+	* waits for the handler to have returned.
+	*/
+	work->element = NULL;
 	evl_init_flag(&sync_work->done);
 }
 EXPORT_SYMBOL_GPL(evl_init_sync_work);
@@ -56,7 +76,17 @@ void evl_call_inband_from(struct evl_work *work,
 			struct workqueue_struct *wq)
 {
 	work->wq = wq;
-	irq_work_queue(&work->irq_work);
+
+	/*
+	 * Async call: get a reference on the protected element if
+	 * specified so that the handler may access such element
+	 * safely after our caller has unwound.
+	 */
+	if (work->element)
+		evl_get_element(work->element);
+
+	if (!irq_work_queue(&work->irq_work) && work->element)
+		evl_put_element(work->element);
 }
 EXPORT_SYMBOL_GPL(evl_call_inband_from);
 
