7e190ab014725a81861a9c62ed81cb14e888b3ba
Nick Xie <nick@khadas.com>
2020-04-28 16:45:20 +0800

    VIM3/3L: add 4G modem EM06 support
    Signed-off-by: Nick Xie <nick@khadas.com>

--- drivers/usb/serial/option.c
+++ drivers/usb/serial/option.c
@@ -2035,6 +2035,7 @@ static struct usb_serial_driver option_1port_device = {
 #ifdef CONFIG_PM
 	.suspend           = usb_wwan_suspend,
 	.resume            = usb_wwan_resume,
+	.reset_resume = usb_wwan_resume,
 #endif
 };
 
@@ -2071,6 +2072,19 @@ static int option_probe(struct usb_serial *serial,
 	    dev_desc->idProduct == cpu_to_le16(SAMSUNG_PRODUCT_GT_B3730) &&
 	    iface_desc->bInterfaceClass != USB_CLASS_CDC_DATA)
 		return -ENODEV;
+
+	if (serial->dev->descriptor.idVendor == cpu_to_le16(0x2C7C)
+		&& serial->interface->cur_altsetting->desc.bInterfaceNumber >= 4)
+		return -ENODEV;
+
+	if (serial->dev->descriptor.idVendor == cpu_to_le16(0x2C7C)) {
+		pm_runtime_set_autosuspend_delay(&serial->dev->dev, 3000);
+		usb_enable_autosuspend(serial->dev);
+	}
+
+	if (serial->dev->descriptor.idVendor == cpu_to_le16(0x2C7C)) {
+		device_init_wakeup(&serial->dev->dev, 1); //usb remote wakeup
+	}
 
 	/* Store the device flags so we can use them during attach. */
 	usb_set_serial_data(serial, (void *)device_flags);
