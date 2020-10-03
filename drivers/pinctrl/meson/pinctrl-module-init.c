e34a78c97583e9210a08da12ba83e1073b03fd89
Qianggui Song <qianggui.song@amlogic.com>
2020-10-03 10:55:06 +0800

    pinctrl: t7: add t7 pinctrl driver [1/1]
    PD#SWPL-36379
    
    Problem:
    Need support t7 pinctrl driver
    
    Solution:
    Add t7 pinctrl driver
    
    Verify:
    t7_pxp
    
    Change-Id: I027fbd506625587629d306ae0a7d9566348595ad
    Signed-off-by: Qianggui Song <qianggui.song@amlogic.com>

--- drivers/pinctrl/meson/pinctrl-module-init.c
+++ drivers/pinctrl/meson/pinctrl-module-init.c
@@ -26,6 +26,7 @@ static int __init pinctrl_module_init(void)
 	load_pinctrl_module(tm2);
 	load_pinctrl_module(sc2);
 	load_pinctrl_module(t5d);
+	load_pinctrl_module(t7);
 
 	pr_info("### %s() end\n", __func__);
 
--- drivers/pinctrl/meson/pinctrl-module-init.h
+++ drivers/pinctrl/meson/pinctrl-module-init.h
@@ -44,6 +44,15 @@ static inline int meson_t5d_pinctrl_init(void)
 }
 #endif
 
+#ifdef CONFIG_PINCTRL_MESON_T7
+int meson_t7_pinctrl_init(void);
+#else
+static inline int meson_t7_pinctrl_init(void)
+{
+	return 0;
+}
+#endif
+
 #endif /* end of ifdef MODULE */
 #endif /* end of _PINCTRL_MODULE_H__ */
 
