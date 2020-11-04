df69915551c40c5c1fa4a5024bb6ad3e60618427
Hanjie Lin <hanjie.lin@amlogic.com>
2020-11-04 17:06:25 +0800

    bringup: improve gki compile [1/1]
    PD#SWPL-31137
    
    Problem:
    gki compile issues
    
    Solution:
     - improve scripts/amlogic/mk[_gki]_meson64.sh
     - introduce scripts/amlogic/mk_gki_meson64_extmodule.sh
    
    Verify:
    sc2
    
    Change-Id: Ie78771252337356f2dad4ae02d7cb581f968dff9
    Signed-off-by: Hanjie Lin <hanjie.lin@amlogic.com>

--- scripts/amlogic/mk_gki_meson64.sh
+++ scripts/amlogic/mk_gki_meson64.sh
@@ -10,8 +10,7 @@ export PATH=/opt/clang-r377782b/bin/:$PATH
 clang_flags="ARCH=arm64 CC=clang HOSTCC=clang LD=ld.lld NM=llvm-nm OBJCOPY=llvm-objcopy"
 export INSTALL_MOD_PATH=./modules_install
 
-which clang
-
+###################### USB and CLOCK configs keeping y for bringup ######################
 cp arch/arm64/configs/meson64_gki.fragment arch/arm64/configs/meson64_gki.fragment.m
 sed -i 's/CONFIG_MESON_CLKC_MODULE=m/CONFIG_MESON_CLKC_MODULE=y/' arch/arm64/configs/meson64_gki.fragment.m
 sed -i 's/CONFIG_USB_DWC3=m/CONFIG_USB_DWC3=y/' arch/arm64/configs/meson64_gki.fragment.m
@@ -21,19 +20,26 @@ sed -i 's/CONFIG_AMLOGIC_USB_HOST_ELECT_TEST=m/CONFIG_AMLOGIC_USB_HOST_ELECT_TES
 sed -i 's/CONFIG_AMLOGIC_USBPHY=m/CONFIG_AMLOGIC_USBPHY=y/' arch/arm64/configs/meson64_gki.fragment.m
 sed -i 's/CONFIG_AMLOGIC_USB2PHY=m/CONFIG_AMLOGIC_USB2PHY=y/' arch/arm64/configs/meson64_gki.fragment.m
 sed -i 's/CONFIG_AMLOGIC_USB3PHY=m/CONFIG_AMLOGIC_USB3PHY=y/' arch/arm64/configs/meson64_gki.fragment.m
-cat ./scripts/kconfig/merge_config.sh arch/arm64/configs/gki_defconfig arch/arm64/configs/meson64_gki.fragment.m > arch/arm64/configs/meson64_tmp_defconfig
+cat arch/arm64/configs/gki_defconfig arch/arm64/configs/meson64_gki.fragment.m > arch/arm64/configs/meson64_tmp_defconfig
 rm arch/arm64/configs/meson64_gki.fragment.m
 
-make ${clang_flags} mrproper
-make ${clang_flags} clean
+#make ${clang_flags} mrproper
+#make ${clang_flags} clean
 make ${clang_flags} meson64_tmp_defconfig
+
 rm arch/arm64/configs/meson64_tmp_defconfig
+find -type f | grep "\.ko$" | xargs rm
 
-make ARCH=arm64 Image -j12
-make ARCH=arm64 dtbs -j12
-make ARCH=arm64 modules -j12
+set -ex
+which clang
+make ${clang_flags} Image -j12
+make ${clang_flags} dtbs -j12
+make ${clang_flags} modules -j12
+mkimage -A arm64 -O linux -T kernel -C none -a 0x1080000 -e 0x1080000 -n linux-next -d arch/arm64/boot/Image uImage
+set +ex
 
 
+######################## build meson64_gki_module_config modules #########################
 GKI_EXT_MODULE_CFG=arch/arm64/configs/meson64_gki_module_config
 
 function read_ext_module_config() {
@@ -46,10 +52,6 @@ function read_ext_module_config() {
 	done < $1
 	echo $ALL_LINE
 }
-
-GKI_EXT_MODULE_CONFIG=$(read_ext_module_config $GKI_EXT_MODULE_CFG)
-
-export GKI_EXT_MODULE_CONFIG
 
 function read_ext_module_predefine() {
 	PRE_DEFINE=""
@@ -67,9 +69,10 @@ function read_ext_module_predefine() {
 	echo $PRE_DEFINE
 }
 
-GKI_EXT_MODULE_PREDEFINE=$(read_ext_module_predefine $GKI_EXT_MODULE_CFG)
-echo "GKI_EXT_MODULE_PREDEFINE="$GKI_EXT_MODULE_PREDEFINE
+GKI_EXT_MODULE_CONFIG=$(read_ext_module_config $GKI_EXT_MODULE_CFG)
+export GKI_EXT_MODULE_CONFIG
 
+GKI_EXT_MODULE_PREDEFINE=$(read_ext_module_predefine $GKI_EXT_MODULE_CFG)
 export GKI_EXT_MODULE_PREDEFINE
 
 # ----------- basic common modules build as extern --------------
@@ -78,21 +81,28 @@ sound/soc/amlogic
 sound/soc/codecs/amlogic
 "
 
+KERNEL_SRC=${PWD}
+KERNEL_SRC=`readlink -f $KERNEL_SRC`
+echo KERNEL_SRC=$KERNEL_SRC
+
 if [[ -z "${SKIP_EXT_MODULES}" ]] && [[ -n "${EXT_MODULES}" ]]; then
   echo "========================================================"
   echo " Building external modules"
 
   for EXT_MOD in ${EXT_MODULES}; do
-    set -x
-    make -C . M=${EXT_MOD} ARCH=arm64 ${GKI_EXT_MODULE_CONFIG} -j
-#make -C . M=${EXT_MOD} ARCH=arm64 ${GKI_EXT_MODULE_CONFIG} modules_install -j
-    set +x
+    EXT_MOD=`readlink -f $EXT_MOD`
+    M=`echo $KERNEL_SRC | sed 's#/[^/]*#../#g'`$EXT_MOD
+    set -ex
+    make ${clang_flags} -C ${EXT_MOD} M=${M} KERNEL_SRC=${KERNEL_SRC} -j12
+    set +ex
   done
 
 fi
 
-echo "pwd = "$PWD
-rm -rfv ${INSTALL_MOD_PATH}
+############################ install builtin/extern modules #########################
+echo "=================================================="
+echo "install modules...."
+rm -rf ${INSTALL_MOD_PATH}
 mkdir -p ${INSTALL_MOD_PATH}/target/
 mkdir -p ${INSTALL_MOD_PATH}/lib/modules/0.0/lib/modules/
 for loop in `find -type f | grep ko$`;
@@ -101,11 +111,12 @@ do
 	cp $loop ${INSTALL_MOD_PATH}/lib/modules/0.0/lib/modules/
 done
 
-echo depmod -b ${INSTALL_MOD_PATH}/ 0.0
+set -x
 depmod -b ${INSTALL_MOD_PATH}/ 0.0
 
 cp ${INSTALL_MOD_PATH}/lib/modules/0.0/lib/modules/*.ko  ${INSTALL_MOD_PATH}/target/
 cp ${INSTALL_MOD_PATH}/lib/modules/0.0/modules.dep ${INSTALL_MOD_PATH}/target/
+set +x
 
 ############### generate modules_install.sh ################
 > ${INSTALL_MOD_PATH}/target/modules_install.sh
@@ -122,10 +133,8 @@ function mod_probe()
         done
 }
 
-
 for loop in `cat ${INSTALL_MOD_PATH}/target/modules.dep | sed 's/:.*//'`;
 do
-#       echo "############################# "$loop
         mod_probe $loop
         echo insmod $loop >> ${INSTALL_MOD_PATH}/target/modules_install.sh
 done
@@ -144,5 +153,4 @@ sed -i '1s/^/#!\/bin\/sh\n&/' ${INSTALL_MOD_PATH}/target/modules_install.sh
 
 ###### done #####
 echo ""
-echo "Done: ${INSTALL_MOD_PATH}/target/"
-
+echo "Done! ${INSTALL_MOD_PATH}/target/: "`wc -l ${INSTALL_MOD_PATH}/target/modules.dep | awk '{print $1}'`" modules."
--- scripts/amlogic/mk_meson64.sh
+++ scripts/amlogic/mk_meson64.sh
@@ -9,19 +9,21 @@ export CLANG_TRIPLE=aarch64-linux-gnu-
 export PATH=/opt/clang-r377782b/bin/:$PATH
 clang_flags="ARCH=arm64 CC=clang HOSTCC=clang LD=ld.lld NM=llvm-nm OBJCOPY=llvm-objcopy"
 
-which clang
 
 cat arch/arm64/configs/meson64_gki_module_config arch/arm64/configs/meson64_gki.fragment > arch/arm64/configs/meson64_gki.fragment.y
 sed -i 's/=m/=y/' arch/arm64/configs/meson64_gki.fragment.y
-cat ./scripts/kconfig/merge_config.sh arch/arm64/configs/gki_defconfig arch/arm64/configs/meson64_gki.fragment.y > arch/arm64/configs/meson64_tmp_defconfig
+cat arch/arm64/configs/gki_defconfig arch/arm64/configs/meson64_gki.fragment.y > arch/arm64/configs/meson64_tmp_defconfig
 rm -fr arch/arm64/configs/meson64_gki.fragment.y
 
-make ${clang_flags} mrproper -j12
-make ${clang_flags} clean -j12
+#make ${clang_flags} mrproper -j12
+#make ${clang_flags} clean -j12
 make ${clang_flags} meson64_tmp_defconfig -j12
 rm arch/arm64/configs/meson64_tmp_defconfig
 
+set -x
+which clang
 make ${clang_flags} Image -j12
 make ${clang_flags} dtbs -j12
 
 mkimage -A arm64 -O linux -T kernel -C none -a 0x1080000 -e 0x1080000 -n linux-next -d arch/arm64/boot/Image uImage
+set +x
