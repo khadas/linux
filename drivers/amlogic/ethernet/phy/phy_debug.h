8550cc8358b0ac298c6dc423f6fd4e21f7be1ca7
Zhuo Wang <zhuo.wang@amlogic.com>
2020-12-31 16:44:22 +0800

    ethernet: t7 bringup [1/1]
    PD#SWPL-38791
    
    Problem:
    bringup t7 ethernet
    
    Solution:
    bringup t7 ethernet
    
    Verify:
    AN400_A311D2#46
    
    Change-Id: Ibaa1ab5caa97935d94cc3f7667794ec4ba8999a8
    Signed-off-by: Zhuo Wang <zhuo.wang@amlogic.com>

---
+++ drivers/amlogic/ethernet/phy/phy_debug.h
@@ -0,0 +1,34 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
+/*
+ * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
+ */
+
+#ifndef _STMMAC_MESON_PHY_DEBUG_H
+#define _STMMAC_MESON_PHY_DEBUG_H
+
+#include <linux/kernel.h>
+#include <linux/interrupt.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+#include <linux/skbuff.h>
+#include <linux/ethtool.h>
+#include <linux/if_ether.h>
+#include <linux/crc32.h>
+#include <linux/mii.h>
+#include <linux/if.h>
+#include <linux/if_vlan.h>
+#include <linux/dma-mapping.h>
+#include <linux/slab.h>
+#include <linux/prefetch.h>
+#include <linux/pinctrl/consumer.h>
+#include <linux/net_tstamp.h>
+
+//#include "eth_reg.h"
+/*add this to stop checking wol,which will reset phy*/
+//extern unsigned int enable_wol_check;
+extern unsigned int tx_amp_bl2;
+extern unsigned int enet_type;
+//extern void __iomem *ioaddr_dbg;
+int gmac_create_sysfs(struct phy_device *phydev, void __iomem *ioaddr);
+int gmac_remove_sysfs(struct phy_device *phydev);
+#endif
--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -30,6 +30,9 @@
 #include <linux/slab.h>
 #include <linux/prefetch.h>
 #include <linux/pinctrl/consumer.h>
+#ifdef CONFIG_AMLOGIC_ETH_PRIVE
+#include "../../../../amlogic/ethernet/phy/phy_debug.h"
+#endif
 #ifdef CONFIG_DEBUG_FS
 #include <linux/debugfs.h>
 #include <linux/seq_file.h>
@@ -48,6 +51,43 @@
 #define	STMMAC_ALIGN(x)		ALIGN(ALIGN(x, SMP_CACHE_BYTES), 16)
 #define	TSO_MAX_BUFF_SIZE	(SZ_16K - 1)
 
+#ifdef CONFIG_AMLOGIC_ETH_PRIVE
+struct phylink {
+       /* private: */
+	struct net_device *netdev;
+	const struct phylink_mac_ops *ops;
+	struct phylink_config *config;
+	struct device *dev;
+	unsigned int old_link_state:1;
+	unsigned long phylink_disable_state; /* bitmask of disables */
+	struct phy_device *phydev;
+	phy_interface_t link_interface; /* PHY_INTERFACE_xxx */
+	u8 link_an_mode;                /* MLO_AN_xxx */
+	u8 link_port;                   /* The current non-phy ethtool port */
+	__ETHTOOL_DECLARE_LINK_MODE_MASK(supported);
+
+	/* The link configuration settings */
+	struct phylink_link_state link_config;
+
+	/* The current settings */
+	phy_interface_t cur_interface;
+
+	struct gpio_desc *link_gpio;
+	unsigned int link_irq;
+	struct timer_list link_poll;
+	void (*get_fixed_state)(struct net_device *dev,
+				struct phylink_link_state *s);
+	/*code review need comment*/
+	struct mutex state_mutex;
+	struct phylink_link_state phy_state;
+	struct work_struct resolve;
+
+	bool mac_link_dropped;
+
+	struct sfp_bus *sfp_bus;
+};
+
+#endif
 /* Module parameters */
 #define TX_TIMEO	5000
 static int watchdog = TX_TIMEO;
@@ -2738,7 +2778,9 @@ static int stmmac_open(struct net_device *dev)
 
 	stmmac_enable_all_queues(priv);
 	stmmac_start_all_queues(priv);
-
+#ifdef CONFIG_AMLOGIC_ETH_PRIVE
+	ret = gmac_create_sysfs(priv->phylink->phydev, priv->ioaddr);
+#endif
 	return 0;
 
 lpiirq_error:
@@ -4682,7 +4724,6 @@ int stmmac_dvr_probe(struct device *device,
 			__func__, ret);
 		goto error_netdev_register;
 	}
-
 #ifdef CONFIG_DEBUG_FS
 	stmmac_init_fs(ndev);
 #endif
