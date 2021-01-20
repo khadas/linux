3d693e55fb334c21844e6fcc55b2638b020960a7
Hanjie Lin <hanjie.lin@amlogic.com>
2021-01-20 13:26:40 +0800

    gki: optimize make script and config [1/1]
    PD#SWPL-41118
    
    Problem:
    optimize mk_gki_meson64.sh
    optimize defconfig
    
    Solution:
    modify
    
    Verify:
    sc2
    
    Change-Id: I6f4765b6a9cdf12377c23d883381ad1370aaff71
    Signed-off-by: Hanjie Lin <hanjie.lin@amlogic.com>

---
+++ scripts/amlogic/mk_p_builtin.sh
@@ -0,0 +1,36 @@
+# SPDX-License-Identifier: (GPL-2.0+ OR MIT)
+#
+# Copyright (c) 2019 Amlogic, Inc. All rights reserved.
+#
+
+target_defconfig=meson64_a64_P_defconfig
+echo "defconfig="$target_defconfig
+echo ""
+
+###########################################################################################
+######################## step 1:  env setup ###############################################
+###########################################################################################
+echo "######################## step 1:  env setup ###############################################"
+export PATH=/opt/clang-r377782b/bin/:/opt/gcc-linaro-6.3.1-2017.02-x86_64_aarch64-linux-gnu/bin/:$PATH
+MAKE_CLANG='make ARCH=arm64 CC=clang HOSTCC=clang LD=ld.lld NM=llvm-nm OBJCOPY=llvm-objcopy CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-gnu- -j8'
+export INSTALL_MOD_PATH=./modules_install
+
+find -type f | grep "\.ko$" | xargs rm -fr
+rm -fr $INSTALL_MOD_PATH
+
+###########################################################################################
+######################## step 2:  build kernel & builtin modules ##########################
+###########################################################################################
+echo "######################## step 2:  build kernel & builtin modules ##########################"
+#make ${clang_flags} mrproper
+#make ${clang_flags} clean
+$MAKE_CLANG $target_defconfig
+
+set -ex
+$MAKE_CLANG Image
+$MAKE_CLANG dtbs
+$MAKE_CLANG modules
+./scripts/amlogic/mkimage -A arm64 -O linux -T kernel -C none -a 0x1080000 -e 0x1080000 -n linux-next -d arch/arm64/boot/Image uImage
+set +ex
+
+echo "Build success!"
