a29ed057a309d8714a87df54825f2b2a96de2886
tuan zhang <tuan.zhang@amlogic.com>
2021-01-26 22:58:09 +0800

    vpu: fixed build error [1/1]
    PD#SWPL-42052
    
    Problem:
    fixed build error for s4 bringup
    
    Solution:
    fixed build error for s4 bringup
    
    Verify:
    S4 PTM
    
    Change-Id: I7f50a31e2205578c475d68c538b234d49570ac5d
    Signed-off-by: tuan zhang <tuan.zhang@amlogic.com>

--- drivers/amlogic/media/common/vpu/vpu.c
+++ drivers/amlogic/media/common/vpu/vpu.c
@@ -2116,7 +2116,7 @@ static struct vpu_data_s vpu_data_s4 = {
 	.mem_pd_reg[4] = PWRCTRL_MEM_PD9_SC2,
 	.mem_pd_reg_flag = 1,
 
-	.pwrctrl_id = 5,
+	.pwrctrl_id_table = vpu_pwrctrl_id_table,
 
 	.power_table = NULL,
 	.iso_table = NULL,
