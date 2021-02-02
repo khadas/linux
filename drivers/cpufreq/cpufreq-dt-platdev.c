b9808771336bc4be436733d86f2e336a1e173a5a
Xingxing Wang <xingxing.wang@amlogic.com>
2021-02-02 15:55:32 +0800

    cpufreq: enable dvfs on s4 [1/1]
    PD#SWPL-42664
    
    Problem:
    s4 dvfs bringup
    
    Solution:
    dvfs bringup
    
    Verify:
    S4 AP222
    
    Change-Id: I792558f96e0204c5873b80821637e1bdd570987b
    Signed-off-by: Xingxing Wang <xingxing.wang@amlogic.com>

--- drivers/cpufreq/cpufreq-dt-platdev.c
+++ drivers/cpufreq/cpufreq-dt-platdev.c
@@ -141,6 +141,7 @@ static const struct of_device_id blacklist[] __initconst = {
 	{ .compatible = "amlogic,tm2", },
 	{ .compatible = "amlogic,t5d", },
 	{ .compatible = "amlogic, t7", },
+	{ .compatible = "amlogic, s4", },
 	{ }
 };
 
