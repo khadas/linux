6ec69ca7ff6e4bdb825a35898384807d526db4d4
Jianxin Pan <jianxin.pan@amlogic.com>
2021-02-04 16:37:06 +0800

    debug: dump memory layout [1/1]
    PD#SWPL-42930
    
    Problem:
    Need memory layout dump
    
    Solution:
    add memory layout dump
    Virtual kernel memory layout:
        modules : 0xffffffc008000000 - 0xffffffc010000000   (   128 MB)
        vmalloc : 0xffffffc010000000 - 0xfffffffebfff0000   (   250 GB)
          .text : 0xffffffc010080000 - 0xffffffc010fa0000   ( 15488 KB)
        .rodata : 0xffffffc010fa0000 - 0xffffffc0119e0000   ( 10496 KB)
          .init : 0xffffffc0119e0000 - 0xffffffc011b10000   (  1216 KB)
          .data : 0xffffffc011b10000 - 0xffffffc011d2f808   (  2175 KB)
           .bss : 0xffffffc011d2f808 - 0xffffffc011e0b810   (   881 KB)
        fixed   : 0xfffffffefe5fb000 - 0xfffffffefea00000   (  4116 KB)
        PCI I/O : 0xfffffffefec00000 - 0xfffffffeffc00000   (    16 MB)
        vmemmap : 0xfffffffeffe00000 - 0xffffffffffe00000   (     4 GB maximum)
                  0xfffffffeffe00000 - 0xffffffff01e00000   (    32 MB actual)
        memory  : 0xffffff8000000000 - 0xffffff8080000000   (  2048 MB)
    
    Verify:
    SC2 5.4
    
    Change-Id: I9477de29fdfe50f447baec9e7671218735aebb79
    Signed-off-by: Jianxin Pan <jianxin.pan@amlogic.com>

---
+++ drivers/amlogic/memory_ext/mem_debug.c
@@ -0,0 +1,114 @@
+// SPDX-License-Identifier: (GPL-2.0+ OR MIT)
+/*
+ * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
+ */
+#include <linux/kernel.h>
+#include <linux/export.h>
+#include <linux/errno.h>
+#include <asm/sections.h>
+#include <linux/sizes.h>
+#include <asm/memory.h>
+#include <linux/init.h>
+#include <linux/initrd.h>
+#include <linux/gfp.h>
+#include <linux/memblock.h>
+#include <linux/dma-mapping.h>
+#include <linux/vmalloc.h>
+#include <linux/mm.h>
+#include <linux/crash_dump.h>
+#include <asm/boot.h>
+#include <asm/fixmap.h>
+#include <linux/kasan.h>
+#include <linux/seq_file.h>
+
+static void dump_mem_layout(char *buf)
+{
+#define MLK(b, t) b, t, ((t) - (b)) >> 10
+#define MLM(b, t) b, t, ((t) - (b)) >> 20
+#define MLG(b, t) b, t, ((t) - (b)) >> 30
+#define MLK_ROUNDUP(b, t) b, t, DIV_ROUND_UP(((t) - (b)), SZ_1K)
+	int pos = 0;
+
+#ifdef CONFIG_ARM64
+	pos += sprintf(buf + pos, "Virtual kernel memory layout:\n");
+#ifdef CONFIG_KASAN
+	pos += sprintf(buf + pos, "    kasan   : 0x%16lx - 0x%16lx   (%6ld GB)\n",
+		MLG(KASAN_SHADOW_START, KASAN_SHADOW_END));
+#endif
+	pos += sprintf(buf + pos, "    modules : 0x%16lx - 0x%16lx   (%6ld MB)\n",
+		MLM(MODULES_VADDR, MODULES_END));
+	pos += sprintf(buf + pos, "    vmalloc : 0x%16lx - 0x%16lx   (%6ld GB)\n",
+		MLG(VMALLOC_START, VMALLOC_END));
+	pos += sprintf(buf + pos, "      .text : 0x%px" " - 0x%px" "   (%6ld KB)\n",
+		MLK_ROUNDUP(_text, _etext));
+	pos += sprintf(buf + pos, "    .rodata : 0x%px" " - 0x%px" "   (%6ld KB)\n",
+		MLK_ROUNDUP(__start_rodata, __init_begin));
+	pos += sprintf(buf + pos, "      .init : 0x%px" " - 0x%px" "   (%6ld KB)\n",
+		MLK_ROUNDUP(__init_begin, __init_end));
+	pos += sprintf(buf + pos, "      .data : 0x%px" " - 0x%px" "   (%6ld KB)\n",
+		MLK_ROUNDUP(_sdata, _edata));
+	pos += sprintf(buf + pos, "       .bss : 0x%px" " - 0x%px" "   (%6ld KB)\n",
+		MLK_ROUNDUP(__bss_start, __bss_stop));
+	pos += sprintf(buf + pos, "    fixed   : 0x%16lx - 0x%16lx   (%6ld KB)\n",
+		MLK(FIXADDR_START, FIXADDR_TOP));
+	pos += sprintf(buf + pos, "    PCI I/O : 0x%16lx - 0x%16lx   (%6ld MB)\n",
+		MLM(PCI_IO_START, PCI_IO_END));
+#ifdef CONFIG_SPARSEMEM_VMEMMAP
+	pos += sprintf(buf + pos, "    vmemmap : 0x%16lx - 0x%16lx   (%6ld GB maximum)\n",
+		MLG(VMEMMAP_START, VMEMMAP_START + VMEMMAP_SIZE));
+	pos += sprintf(buf + pos, "              0x%16lx - 0x%16lx   (%6ld MB actual)\n",
+		MLM((unsigned long)phys_to_page(memblock_start_of_DRAM()),
+		    (unsigned long)virt_to_page(high_memory)));
+#endif
+	pos += sprintf(buf + pos, "    memory  : 0x%16lx - 0x%16lx   (%6ld MB)\n",
+		MLM(__phys_to_virt(memblock_start_of_DRAM()),
+		    (unsigned long)high_memory));
+#else
+
+#endif
+}
+
+static int mdebug_show(struct seq_file *m, void *arg)
+{
+	char *buf = kmalloc(4096, GFP_KERNEL);
+
+	if (!buf) {
+		pr_err("%s failed\n", __func__);
+		return -1;
+	}
+
+	/* update only once */
+	dump_mem_layout(buf);
+	seq_printf(m, "%s\n", buf);
+
+	kfree(buf);
+
+	return 0;
+}
+
+static int mdebug_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, mdebug_show, NULL);
+}
+
+static const struct file_operations mdebug_ops = {
+	.open		= mdebug_open,
+	.read		= seq_read,
+	.llseek		= seq_lseek,
+	.release		= single_release,
+};
+
+static int __init memory_debug_init(void)
+{
+	struct proc_dir_entry *d_mdebug;
+
+	d_mdebug = proc_create("mem_debug", 0444, NULL, &mdebug_ops);
+	if (IS_ERR_OR_NULL(d_mdebug)) {
+		pr_err("%s, create proc failed\n", __func__);
+		return -1;
+	}
+
+	return 0;
+}
+rootfs_initcall(memory_debug_init);
+
