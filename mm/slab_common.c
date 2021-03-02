61775313849ecb567eb55f66e6da57cfa44946cb
Jianxin Pan <jianxin.pan@amlogic.com>
2021-03-03 00:32:07 +0800

    mm: report more in slabinfo [1/1]
    PD#SWPL-41092
    
    Problem:
    report more in slabinfo
    
    Solution:
    report <total bytes> <reclaim> in slabinfo
    
    Verify:
    SC2
    
    Change-Id: I415d5a95fb1caed7a284edec30ccc778f686dac5
    Signed-off-by: Jianxin Pan <jianxin.pan@amlogic.com>

--- mm/slab_common.c
+++ mm/slab_common.c
@@ -1423,7 +1423,14 @@ static void print_slabinfo_header(struct seq_file *m)
 #else
 	seq_puts(m, "slabinfo - version: 2.1\n");
 #endif
+
+#ifdef CONFIG_AMLOGIC_MEMORY_EXTEND
+	/* add total bytes for each slab */
+	seq_puts(m, "# name                        <active_objs> <num_objs> ");
+	seq_puts(m, "<objsize> <objperslab> <pagesperslab> <total bytes> <reclaim>");
+#else
 	seq_puts(m, "# name            <active_objs> <num_objs> <objsize> <objperslab> <pagesperslab>");
+#endif /* CONFIG_AMLOGIC_MEMORY_EXTEND */
 	seq_puts(m, " : tunables <limit> <batchcount> <sharedfactor>");
 	seq_puts(m, " : slabdata <active_slabs> <num_slabs> <sharedavail>");
 #ifdef CONFIG_DEBUG_SLAB
@@ -1473,16 +1480,28 @@ memcg_accumulate_slabinfo(struct kmem_cache *s, struct slabinfo *info)
 static void cache_show(struct kmem_cache *s, struct seq_file *m)
 {
 	struct slabinfo sinfo;
+#ifdef CONFIG_AMLOGIC_MEMORY_EXTEND
+	char name[32];
+	long total;
+#endif
 
 	memset(&sinfo, 0, sizeof(sinfo));
 	get_slabinfo(s, &sinfo);
 
 	memcg_accumulate_slabinfo(s, &sinfo);
 
+#ifdef CONFIG_AMLOGIC_MEMORY_EXTEND
+	strncpy(name, cache_name(s), 31);
+	total = sinfo.num_objs * s->size;
+	seq_printf(m, "%-31s %6lu %6lu %6u %4u %4d %8lu, %s",
+		   name, sinfo.active_objs, sinfo.num_objs, s->size,
+		   sinfo.objects_per_slab, (1 << sinfo.cache_order),
+		   total, (s->flags & SLAB_RECLAIM_ACCOUNT) ? "S_R" : "S_U");
+#else
 	seq_printf(m, "%-17s %6lu %6lu %6u %4u %4d",
 		   cache_name(s), sinfo.active_objs, sinfo.num_objs, s->size,
 		   sinfo.objects_per_slab, (1 << sinfo.cache_order));
-
+#endif /* CONFIG_AMLOGIC_MEMORY_EXTEND */
 	seq_printf(m, " : tunables %4u %4u %4u",
 		   sinfo.limit, sinfo.batchcount, sinfo.shared);
 	seq_printf(m, " : slabdata %6lu %6lu %6lu",
