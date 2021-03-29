5ee980671b4797f88889c0dd09c504d16a2850b2
binqi zhang <binqi.zhang@amlogic.com>
2021-03-29 16:42:16 +0800

    uvm: add new ioctl to set dmabuf from userspace [1/1]
    PD#SWPL-45059
    
    Problem:
    add new ioctl attch to set dmabuffer from userspace
    
    Solution:
    add new ioctl attch to set dmabuffer from userspace
    
    Verify:
    t7
    
    Change-Id: I2af2546e65c3b7c53fdeba9eeea7d446e88c7aa8
    Signed-off-by: binqi zhang <binqi.zhang@amlogic.com>

---
+++ drivers/amlogic/media/common/uvm/meson_uvm_nn_processor.c
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: (GPL-2.0+ OR MIT)
+/*
+ * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
+ */
+
+#include "meson_uvm_nn_processor.h"
+
+struct uvm_hook_mod_info *get_nn_hook_mod_info(void)
+{
+	/* TODO */
+	return NULL;
+}
+
+struct uvm_hook_mod_info *nn_mod_setinfo(void)
+{
+	/* TODO */
+	return NULL;
+}
+
+struct uvm_hook_mod_info *nn_mod_getinfo(void)
+{
+	/* TODO */
+	return NULL;
+}
