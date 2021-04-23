224c24a540a629eed2f1b03ee3dbdf7d1a944e02
Chuangcheng Peng <chuangcheng.peng@amlogic.com>
2021-04-23 09:36:50 +0800

    smartcard: support nds T0 protocol [1/1]
    PD#SWPL-48277
    
    Problem:
     sc io communication protocol not work
    
    Solution:
     correct the freq & etu
    
    Verify:
     verified at sc2_skt
    
    Signed-off-by: Chuangcheng Peng <chuangcheng.peng@amlogic.com>
    Change-Id: Ie3bf672b72c9f52c73ac6c36706eb3d6a9f3e2c9

--- drivers/amlogic/smartcard_sc2/smartcard.c
+++ drivers/amlogic/smartcard_sc2/smartcard.c
@@ -847,10 +847,12 @@ static int smc_hw_set_param(struct smc_dev *smc)
 //      unsigned long freq_cpu = clk_get_rate(aml_smartcard_clk)/1000*DIV_SMC;
 
 	pr_error("hw set param\n");
+	clk_set_rate(aml_smartcard_clk, smc->param.freq * 1000);
 
 	v = SMC_READ_REG(REG0);
 	reg0 = (struct smccard_hw_reg0 *)&v;
-	reg0->etu_divider = smc->param.f / smc->param.d - 1;
+	reg0->etu_divider = ETU_DIVIDER_CLOCK_HZ * smc->param.f /
+	    (smc->param.d * smc->param.freq) - 1;
 	SMC_WRITE_REG(REG0, v);
 	pr_error("REG0: 0x%08lx\n", v);
 	pr_error("f	  :%d\n", smc->param.f);
@@ -966,7 +968,8 @@ static int smc_hw_setup(struct smc_dev *smc)
 	reg0->rst_level = RESET_ENABLE;
 //      reg0->io_level = 0;
 	reg0->recv_fifo_threshold = FIFO_THRESHOLD_DEFAULT;
-	reg0->etu_divider = smc->param.f / smc->param.d - 1;
+	reg0->etu_divider = ETU_DIVIDER_CLOCK_HZ * smc->param.f
+	    / (smc->param.d * smc->param.freq) - 1;
 	reg0->first_etu_offset = 5;
 	SMC_WRITE_REG(REG0, v);
 	smc_clk_enable(reg0->clk_en);
@@ -1425,8 +1428,8 @@ static int smc_hw_reset(struct smc_dev *smc)
 
 			sc_reg0_reg->rst_level = RESET_ENABLE;
 			sc_reg0_reg->clk_en = 1;
-			sc_reg0_reg->etu_divider =
-			    smc->param.f / smc->param.d - 1;
+			sc_reg0_reg->etu_divider = ETU_DIVIDER_CLOCK_HZ *
+			    smc->param.f / (smc->param.d * smc->param.freq) - 1;
 			SMC_WRITE_REG(REG0, sc_reg0);
 			smc_clk_enable(sc_reg0_reg->clk_en);
 
@@ -1484,8 +1487,8 @@ static int smc_hw_reset(struct smc_dev *smc)
 //                      sc_reg0_reg->start_atr_en = 1;
 			sc_reg0_reg->start_atr = 1;
 			sc_reg0_reg->enable = 1;
-			sc_reg0_reg->etu_divider =
-			    smc->param.f / smc->param.d - 1;
+			sc_reg0_reg->etu_divider = ETU_DIVIDER_CLOCK_HZ *
+			    smc->param.f / (smc->param.d * smc->param.freq) - 1;
 			SMC_WRITE_REG(REG0, sc_reg0);
 #ifdef RST_FROM_PIO
 
