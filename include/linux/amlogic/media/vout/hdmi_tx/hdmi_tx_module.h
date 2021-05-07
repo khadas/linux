061fc20807e544ac2aa01806f470f26c8d95bbf3
hang cheng <hang.cheng@amlogic.com>
2021-05-07 16:54:07 +0800

    hdmitx: correct hdmipwr uevent init state [1/1]
    PD#SWPL-49008
    
    Problem:
    hdmi early suspend uevent is not received by sysctl
    
    Solution:
    correct hdmipwr uevent init state
    
    Verify:
    AC214
    
    Change-Id: I42ad027ddd05c0be63e9bc6ed5767dc96670424f
    Signed-off-by: hang cheng <hang.cheng@amlogic.com>

--- include/linux/amlogic/media/vout/hdmi_tx/hdmi_tx_module.h
+++ include/linux/amlogic/media/vout/hdmi_tx/hdmi_tx_module.h
@@ -792,6 +792,9 @@ int hdmitx_set_audio(struct hdmitx_dev *hdmitx_device,
 #define HDMITX_UNPLUG			2
 #define HDMITX_PHY_ADDR_VALID		3
 
+#define HDMI_SUSPEND	0
+#define HDMI_WAKEUP	1
+
 enum hdmitx_event {
 	HDMITX_NONE_EVENT = 0,
 	HDMITX_HPD_EVENT,
