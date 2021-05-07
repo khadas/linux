7b4965ae4efdc20846fff23d5080319a9c8c87f8
Ao Xu <ao.xu@amlogic.com>
2021-05-07 16:08:08 +0800

    drm: handle the logo init parameter with osd [1/1]
    PD#SWPL-48677
    
    Problem:
    1.gki module init parameter need module name prefix
    2.same module parameter will lead kernel bootup fail
    
    Solution:
    move the logo function into osd_logo.h
    
    Verify:
    sc2-ah212
    
    Change-Id: Ia40eda7a4e200a864a3cc501395b23fe1f79d884
    Signed-off-by: Ao Xu <ao.xu@amlogic.com>

---
+++ include/linux/amlogic/media/osd/osd_logo.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
+/*
+ * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
+ */
+
+#ifndef __OSD_LOGO_H__
+#define __OSD_LOGO_H__
+
+u32 get_logo_fb_width(void);
+u32 get_logo_fb_height(void);
+u32 get_logo_display_bpp(void);
+void get_logo_osd_reverse(u32 *index, u32 *reverse_type);
+
+#endif
