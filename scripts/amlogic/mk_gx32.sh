b52c1d1f732a6894131a4ee4aeca5441e20df0af
Jianxin Pan <jianxin.pan@amlogic.com>
2021-07-22 14:57:49 +0800

    defconfig: add clang make for aarch32 [1/1]
    PD#SWPL-55981
    
    Problem:
    enable clang for aarch32
    
    Solution:
    enable clang for aarch32
    
    Verify:
    T5D
    
    Change-Id: I18d3c6041acbd8f85d765f1f583eeab4b01b82ac
    Signed-off-by: Jianxin Pan <jianxin.pan@amlogic.com>

---
+++ scripts/amlogic/mk_gx32.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+export PATH=/opt/clang-r377782b/bin/:/opt/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf/bin/arm-linux-gnueabihf-:$PATH
+MAKE_CLANG='make ARCH=arm CC=clang HOSTCC=clang LD=ld.lld NM=llvm-nm OBJCOPY=llvm-objcopy CLANG_TRIPLE=arm-linux-gnueabihf- CROSS_COMPILE=arm-linux-gnueabihf- -j8'
+export INSTALL_MOD_PATH=./modules_install
+
+$MAKE_CLANG meson64_a32_defconfig
+
+set -ex
+$MAKE_CLANG uImage
+$MAKE_CLANG dtbs
+$MAKE_CLANG modules
+
