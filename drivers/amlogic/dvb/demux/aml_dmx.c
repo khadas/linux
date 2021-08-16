05931c99843cf9ccc7af35d681fe9775df33b5ba
Chuangcheng Peng <chuangcheng.peng@amlogic.com>
2021-08-16 19:05:52 +0800

    demux: don't update filter when source equal [1/1]
    PD#SWPL-57244
    
    Problem:
     es len disnormal
    
    Solution:
     don't update filter when source equal
    
    Verify:
     verified at AH219
    
    Change-Id: I8a5d8702bfb22261104f2acde8a350a709ab813d
    Signed-off-by: Chuangcheng Peng <chuangcheng.peng@amlogic.com>

--- drivers/amlogic/dvb/demux/aml_dmx.c
+++ drivers/amlogic/dvb/demux/aml_dmx.c
@@ -1491,7 +1491,8 @@ static int _dmx_set_hw_source(struct dmx_demux *dmx, int hw_source)
 		dsc_set_sid(demux->id, INPUT_LOCAL, demux->local_sid);
 	} else if (hw_source >= FRONTEND_TS0 && hw_source <= FRONTEND_TS7) {
 		demux->ts_index = hw_source - FRONTEND_TS0;
-		if (advb->ts[demux->ts_index].ts_sid != -1) {
+		if (advb->ts[demux->ts_index].ts_sid != -1 &&
+			advb->ts[demux->ts_index].ts_sid != demux->demod_sid) {
 			demux->demod_sid = advb->ts[demux->ts_index].ts_sid;
 			ts_output_update_filter(demux->id, demux->demod_sid);
 			dsc_set_sid(demux->id, INPUT_DEMOD, demux->demod_sid);
