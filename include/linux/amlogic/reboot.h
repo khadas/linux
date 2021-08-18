c343c9d3f4ab2b870b793be835fa1ae14d02d5e4
Yao Jie <jie.yao@amlogic.com>
2021-08-18 11:29:15 +0800

    reboot: T5W bringup reboot [1/1]
    PD#SWPL-57387
    
    Problem:
    t5w reboot bringup
    
    Solution:
    t5w reboot driver porting
    
    Verify:
    t5w_pxp
    
    Change-Id: I7f2caab3781748f3cf668cc67bd164828667f8c6
    Signed-off-by: Yao Jie <jie.yao@amlogic.com>
    Signed-off-by: Jian Hu <jian.hu@amlogic.com>

--- include/linux/amlogic/reboot.h
+++ include/linux/amlogic/reboot.h
@@ -6,15 +6,15 @@
 /*******************************************************************/
 #define	MESON_COLD_REBOOT					0
 #define	MESON_NORMAL_BOOT					1
-#define	MESON_FACTORY_RESET_REBOOT			2
+#define	MESON_FACTORY_RESET_REBOOT				2
 #define	MESON_UPDATE_REBOOT					3
-#define	MESON_FASTBOOT_REBOOT				4
+#define	MESON_FASTBOOT_REBOOT					4
 #define	MESON_UBOOT_SUSPEND					5
 #define	MESON_HIBERNATE						6
-#define	MESON_BOOTLOADER_REBOOT				7
+#define	MESON_BOOTLOADER_REBOOT					7
 #define	MESON_RPMBP_REBOOT					9
 #define MESON_QUIESCENT_REBOOT					10
-#define	MESON_RESCUEPARTY_REBOOT			11
+#define	MESON_RESCUEPARTY_REBOOT				11
 #define	MESON_KERNEL_PANIC					12
 #define MESON_RECOVERY_QUIESCENT_REBOOT				14
 #define MESON_FFV_REBOOT					15
