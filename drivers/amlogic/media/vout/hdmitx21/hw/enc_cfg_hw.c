7b8e4625c2f2be8e76d7c0aba343902376ee71e7
Zongdong Jiao <zongdong.jiao@amlogic.com>
2021-08-20 20:36:18 +0800

    hdmitx21: add 1080i modes [1/1]
    PD#SWPL-43435
    
    Problem:
    Lack 1080i modes
    
    Solution:
    Add 1080i modes
    
    Verify:
    t7/an400
    
    Change-Id: I13637bf0d8d74413918b73848f1cb51f17cea6fd
    Signed-off-by: Zongdong Jiao <zongdong.jiao@amlogic.com>

--- drivers/amlogic/media/vout/hdmitx21/hw/enc_cfg_hw.c
+++ drivers/amlogic/media/vout/hdmitx21/hw/enc_cfg_hw.c
@@ -114,6 +114,266 @@ static void config_tv_enc_calc(enum hdmi_vic vic)
 	// }
 }
 
+static unsigned long modulo(unsigned long a, unsigned long b)
+{
+	if (a >= b)
+		return a - b;
+	else
+		return a;
+}
+
+static signed int to_signed(unsigned int a)
+{
+	if (a <= 7)
+		return a;
+	else
+		return a - 16;
+}
+
+#define MREG_END_MARKER 0xffff
+
+static const struct reg_s tvregs_1080i60[] = {
+	{ENCP_VIDEO_EN, 0},
+	{ENCI_VIDEO_EN, 0},
+	{VENC_DVI_SETTING, 0x2029},
+	{ENCP_VIDEO_MAX_PXCNT, 2199},
+	{ENCP_VIDEO_MAX_LNCNT, 1124},
+	{ENCP_VIDEO_HSPULS_BEGIN, 88},
+	{ENCP_VIDEO_HSPULS_END, 264},
+	{ENCP_VIDEO_HSPULS_SWITCH, 88},
+	{ENCP_VIDEO_HAVON_BEGIN, 192},
+	{ENCP_VIDEO_HAVON_END, 2111},
+	{ENCP_VIDEO_HSO_BEGIN, 0},
+	{ENCP_VIDEO_HSO_END, 44},
+	{ENCP_VIDEO_EQPULS_BEGIN, 2288},
+	{ENCP_VIDEO_EQPULS_END, 2464},
+	{ENCP_VIDEO_VSPULS_BEGIN, 440},
+	{ENCP_VIDEO_VSPULS_END, 2200},
+	{ENCP_VIDEO_VSPULS_BLINE, 0},
+	{ENCP_VIDEO_VSPULS_ELINE, 4},
+	{ENCP_VIDEO_EQPULS_BLINE, 0},
+	{ENCP_VIDEO_EQPULS_ELINE, 4},
+	{ENCP_VIDEO_VAVON_BLINE, 20},
+	{ENCP_VIDEO_VAVON_ELINE, 559},
+	{ENCP_VIDEO_VSO_BEGIN, 30},
+	{ENCP_VIDEO_VSO_END, 50},
+	{ENCP_VIDEO_VSO_BLINE, 0},
+	{ENCP_VIDEO_VSO_ELINE, 5},
+	{ENCP_VIDEO_YFP1_HTIME, 516},
+	{ENCP_VIDEO_YFP2_HTIME, 4355},
+	{VENC_VIDEO_PROG_MODE, 0x100},
+	{ENCP_VIDEO_OFLD_VOAV_OFST, 0x11},
+	{ENCP_VIDEO_MODE, 0x5ffc},
+	{ENCP_VIDEO_MODE_ADV, 0x0018},
+	{ENCP_VIDEO_SYNC_MODE, 0x207},
+	{ENCI_VIDEO_EN, 0},
+	{MREG_END_MARKER, 0},
+};
+
+static const struct reg_s tvregs_1080i50[] = {
+	{ENCP_VIDEO_EN, 0},
+	{ENCI_VIDEO_EN, 0},
+	{VENC_DVI_SETTING, 0x202d},
+	{ENCP_VIDEO_MAX_PXCNT, 2639},
+	{ENCP_VIDEO_MAX_LNCNT, 1124},
+	{ENCP_VIDEO_HSPULS_BEGIN, 88},
+	{ENCP_VIDEO_HSPULS_END, 264},
+	{ENCP_VIDEO_HSPULS_SWITCH, 88},
+	{ENCP_VIDEO_HAVON_BEGIN, 192},
+	{ENCP_VIDEO_HAVON_END, 2111},
+	{ENCP_VIDEO_HSO_BEGIN, 0},
+	{ENCP_VIDEO_HSO_END, 44},
+	{ENCP_VIDEO_VSPULS_BEGIN, 440},
+	{ENCP_VIDEO_VSPULS_END, 2200},
+	{ENCP_VIDEO_VSPULS_BLINE, 0},
+	{ENCP_VIDEO_VSPULS_ELINE, 4},
+	{ENCP_VIDEO_VAVON_BLINE, 20},
+	{ENCP_VIDEO_VAVON_ELINE, 559},
+	{ENCP_VIDEO_VSO_BEGIN, 30},
+	{ENCP_VIDEO_VSO_END, 50},
+	{ENCP_VIDEO_VSO_BLINE, 0},
+	{ENCP_VIDEO_VSO_ELINE, 5},
+	{ENCP_VIDEO_YFP1_HTIME, 526},
+	{ENCP_VIDEO_YFP2_HTIME, 4365},
+	{VENC_VIDEO_PROG_MODE, 0x100},
+	{ENCP_VIDEO_OFLD_VOAV_OFST, 0x11},
+	{ENCP_VIDEO_MODE, 0x5ffc},
+	{ENCP_VIDEO_MODE_ADV, 0x0018},
+	{ENCP_VIDEO_SYNC_MODE, 0x7},
+	{ENCI_VIDEO_EN, 0},
+	{MREG_END_MARKER, 0},
+};
+
+static void set_vmode_enc_hw(const struct reg_s *s)
+{
+	if (!s)
+		return;
+
+	while (s->reg != MREG_END_MARKER) {
+		hd21_write_reg(s->reg, s->val);
+		s++;
+	}
+}
+
+static void hdmi_tvenc1080i_set(enum hdmi_vic vic)
+{
+	unsigned long VFIFO2VD_TO_HDMI_LATENCY = 2;
+	unsigned long TOTAL_PIXELS = 0, PIXEL_REPEAT_HDMI = 0,
+		PIXEL_REPEAT_VENC = 0, ACTIVE_PIXELS = 0;
+	unsigned int FRONT_PORCH = 88, HSYNC_PIXELS = 0, ACTIVE_LINES = 0,
+		INTERLACE_MODE = 0, TOTAL_LINES = 0, SOF_LINES = 0,
+		VSYNC_LINES = 0;
+	unsigned int LINES_F0 = 0, LINES_F1 = 563, BACK_PORCH = 0,
+		EOF_LINES = 2, TOTAL_FRAMES = 0;
+
+	unsigned long total_pixels_venc = 0;
+	unsigned long active_pixels_venc = 0;
+	unsigned long front_porch_venc = 0;
+	unsigned long hsync_pixels_venc = 0;
+
+	unsigned long de_h_begin = 0, de_h_end = 0;
+	unsigned long de_v_begin_even = 0, de_v_end_even = 0,
+		de_v_begin_odd = 0, de_v_end_odd = 0;
+	unsigned long hs_begin = 0, hs_end = 0;
+	unsigned long vs_adjust = 0;
+	unsigned long vs_bline_evn = 0, vs_eline_evn = 0,
+		vs_bline_odd = 0, vs_eline_odd = 0;
+	unsigned long vso_begin_evn = 0, vso_begin_odd = 0;
+	const struct hdmi_timing *tp = NULL;
+
+	tp = hdmitx21_gettiming_from_vic(vic);
+	if (!tp) {
+		pr_info("not find hdmitx vic %d timing\n", vic);
+		return;
+	}
+	pr_info("%s[%d] vic = %d\n", __func__, __LINE__, tp->vic);
+
+	switch (vic) {
+	case HDMI_5_1920x1080i60_16x9:
+	case HDMI_46_1920x1080i120_16x9:
+		INTERLACE_MODE = 1;
+		PIXEL_REPEAT_VENC = 0;
+		PIXEL_REPEAT_HDMI = 0;
+		ACTIVE_PIXELS = (1920 * (1 + PIXEL_REPEAT_HDMI));
+		ACTIVE_LINES = (1080 / (1 + INTERLACE_MODE));
+		LINES_F0 = 562;
+		LINES_F1 = 563;
+		FRONT_PORCH = 88;
+		HSYNC_PIXELS = 44;
+		BACK_PORCH = 148;
+		EOF_LINES = 2;
+		VSYNC_LINES = 5;
+		SOF_LINES = 15;
+		TOTAL_FRAMES = 4;
+		set_vmode_enc_hw(tvregs_1080i60);
+		break;
+	case HDMI_20_1920x1080i50_16x9:
+	case HDMI_40_1920x1080i100_16x9:
+		INTERLACE_MODE = 1;
+		PIXEL_REPEAT_VENC = 0;
+		PIXEL_REPEAT_HDMI = 0;
+		ACTIVE_PIXELS = (1920 * (1 + PIXEL_REPEAT_HDMI));
+		ACTIVE_LINES = (1080 / (1 + INTERLACE_MODE));
+		LINES_F0 = 562;
+		LINES_F1 = 563;
+		FRONT_PORCH = 528;
+		HSYNC_PIXELS = 44;
+		BACK_PORCH = 148;
+		EOF_LINES = 2;
+		VSYNC_LINES = 5;
+		SOF_LINES = 15;
+		TOTAL_FRAMES = 4;
+		set_vmode_enc_hw(tvregs_1080i50);
+		break;
+	default:
+		break;
+	}
+	TOTAL_PIXELS = FRONT_PORCH + HSYNC_PIXELS + BACK_PORCH + ACTIVE_PIXELS;
+	TOTAL_LINES = LINES_F0 + LINES_F1 * INTERLACE_MODE;
+
+	total_pixels_venc = (TOTAL_PIXELS / (1 + PIXEL_REPEAT_HDMI)) *
+		(1 + PIXEL_REPEAT_VENC);
+	active_pixels_venc = (ACTIVE_PIXELS / (1 + PIXEL_REPEAT_HDMI)) *
+		(1 + PIXEL_REPEAT_VENC);
+	front_porch_venc = (FRONT_PORCH / (1 + PIXEL_REPEAT_HDMI)) *
+		(1 + PIXEL_REPEAT_VENC);
+	hsync_pixels_venc =
+	(HSYNC_PIXELS / (1 + PIXEL_REPEAT_HDMI)) * (1 + PIXEL_REPEAT_VENC);
+
+	hd21_write_reg(ENCP_VIDEO_MODE, hd21_read_reg(ENCP_VIDEO_MODE)
+		| (1 << 14));
+
+	/* Program DE timing */
+	de_h_begin = modulo(hd21_read_reg(ENCP_VIDEO_HAVON_BEGIN) +
+		VFIFO2VD_TO_HDMI_LATENCY, total_pixels_venc);
+	de_h_end = modulo(de_h_begin + active_pixels_venc, total_pixels_venc);
+	hd21_write_reg(ENCP_DE_H_BEGIN, de_h_begin);
+	hd21_write_reg(ENCP_DE_H_END, de_h_end);
+	/* Program DE timing for even field */
+	de_v_begin_even = hd21_read_reg(ENCP_VIDEO_VAVON_BLINE);
+	de_v_end_even = de_v_begin_even + ACTIVE_LINES;
+	hd21_write_reg(ENCP_DE_V_BEGIN_EVEN, de_v_begin_even);
+	hd21_write_reg(ENCP_DE_V_END_EVEN, de_v_end_even);
+	/* Program DE timing for odd field if needed */
+	if (INTERLACE_MODE) {
+		de_v_begin_odd =
+		to_signed((hd21_read_reg(ENCP_VIDEO_OFLD_VOAV_OFST) & 0xf0)
+			  >> 4) + de_v_begin_even + (TOTAL_LINES - 1) / 2;
+		de_v_end_odd = de_v_begin_odd + ACTIVE_LINES;
+		hd21_write_reg(ENCP_DE_V_BEGIN_ODD, de_v_begin_odd);/* 583 */
+		hd21_write_reg(ENCP_DE_V_END_ODD, de_v_end_odd);  /* 1123 */
+	}
+
+	/* Program Hsync timing */
+	if (de_h_end + front_porch_venc >= total_pixels_venc) {
+		hs_begin = de_h_end + front_porch_venc - total_pixels_venc;
+		vs_adjust = 1;
+	} else {
+		hs_begin = de_h_end + front_porch_venc;
+		vs_adjust = 0;
+	}
+	hs_end = modulo(hs_begin + hsync_pixels_venc, total_pixels_venc);
+	hd21_write_reg(ENCP_DVI_HSO_BEGIN, hs_begin);
+	hd21_write_reg(ENCP_DVI_HSO_END, hs_end);
+
+	/* Program Vsync timing for even field */
+	if (de_v_begin_even >= SOF_LINES + VSYNC_LINES + (1 - vs_adjust))
+		vs_bline_evn = de_v_begin_even - SOF_LINES - VSYNC_LINES
+			- (1 - vs_adjust);
+	else
+		vs_bline_evn = TOTAL_LINES + de_v_begin_even - SOF_LINES
+			- VSYNC_LINES - (1 - vs_adjust);
+
+	vs_eline_evn = modulo(vs_bline_evn + VSYNC_LINES, TOTAL_LINES);
+	hd21_write_reg(ENCP_DVI_VSO_BLINE_EVN, vs_bline_evn);   /* 0 */
+	hd21_write_reg(ENCP_DVI_VSO_ELINE_EVN, vs_eline_evn);   /* 5 */
+	vso_begin_evn = hs_begin; /* 2 */
+	hd21_write_reg(ENCP_DVI_VSO_BEGIN_EVN, vso_begin_evn);  /* 2 */
+	hd21_write_reg(ENCP_DVI_VSO_END_EVN, vso_begin_evn);  /* 2 */
+	/* Program Vsync timing for odd field if needed */
+	if (INTERLACE_MODE) {
+		vs_bline_odd = de_v_begin_odd - 1 - SOF_LINES - VSYNC_LINES;
+		vs_eline_odd = de_v_begin_odd - 1 - SOF_LINES;
+		vso_begin_odd = modulo(hs_begin + (total_pixels_venc >> 1),
+					total_pixels_venc);
+		hd21_write_reg(ENCP_DVI_VSO_BLINE_ODD, vs_bline_odd);
+		hd21_write_reg(ENCP_DVI_VSO_ELINE_ODD, vs_eline_odd);
+		hd21_write_reg(ENCP_DVI_VSO_BEGIN_ODD, vso_begin_odd);
+		hd21_write_reg(ENCP_DVI_VSO_END_ODD, vso_begin_odd);
+	}
+
+	hd21_write_reg(VPU_HDMI_SETTING, (0 << 0) |
+		(0 << 1) |
+		(tp->h_pol << 2) |
+		(tp->v_pol << 3) |
+		(0 << 4) |
+		(4 << 5) |
+		(0 << 8) |
+		(0 << 12)
+	);
+	hd21_set_reg_bits(VPU_HDMI_SETTING, 1, 1, 1);
+}
+
 void set_tv_encp_new(u32 enc_index, enum hdmi_vic vic, u32 enable)
 {
 	u32 reg_offset;
@@ -122,7 +382,17 @@ void set_tv_encp_new(u32 enc_index, enum hdmi_vic vic, u32 enable)
 	// VENC2A 0x23
 	reg_offset = enc_index == 0 ? 0 : enc_index == 1 ? 0x600 : 0x800;
 
-	config_tv_enc_calc(vic);
+	switch (vic) {
+	case HDMI_5_1920x1080i60_16x9:
+	case HDMI_46_1920x1080i120_16x9:
+	case HDMI_20_1920x1080i50_16x9:
+	case HDMI_40_1920x1080i100_16x9:
+		hdmi_tvenc1080i_set(vic);
+		break;
+	default:
+		config_tv_enc_calc(vic);
+		break;
+	}
 } /* set_tv_encp_new */
 
 static void config_tv_enci(enum hdmi_vic vic)
@@ -200,7 +470,7 @@ void set_tv_enci_new(u32 enc_index, enum hdmi_vic vic, u32 enable)
 	reg_offset = enc_index == 0 ? 0 : enc_index == 1 ? 0x600 : 0x800;
 
 	config_tv_enci(vic);
-} /* set_tv_encp_new */
+} /* set_tv_enci_new */
 
 void hdmitx21_venc_en(bool en, bool pi_mode)
 {
