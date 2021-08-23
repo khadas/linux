7ebbf48ebf9b5556154baea1d82a131990d56373
Pengcheng Chen <pengcheng.chen@amlogic.com>
2021-08-23 16:08:51 +0800

    codec_io: add codec_io/canvas/rdma support for t5w [1/1]
    PD#SWPL-57407
    
    Problem:
    add codec_io/canvas/rdma support for t5w
    
    Solution:
    add codec_io/canvas/rdma support for t5w
    
    Verify:
    t5w ptm
    
    Change-Id: I03cf815be9e8a81f41b76c408082cfa04daabcf4
    Signed-off-by: Pengcheng Chen <pengcheng.chen@amlogic.com>
    Signed-off-by: jian.cao <jian.cao@amlogic.com>

--- include/linux/amlogic/media/registers/cpu_version.h
+++ include/linux/amlogic/media/registers/cpu_version.h
@@ -58,6 +58,7 @@ enum meson_cpuid_type_e {
 	MESON_CPU_MAJOR_ID_S4 = 0x37,
 	MESON_CPU_MAJOR_ID_T3 = 0x38,
 	MESON_CPU_MAJOR_ID_S4D = 0x3a,
+	MESON_CPU_MAJOR_ID_T5W = 0x3b,
 	MESON_CPU_MAJOR_ID_UNKNOWN,
 };
 
@@ -258,6 +259,11 @@ static inline bool is_meson_t3_cpu(void)
 static inline bool is_meson_s4d_cpu(void)
 {
 	return get_cpu_type() == MESON_CPU_MAJOR_ID_S4D;
+}
+
+static inline bool is_meson_t5w_cpu(void)
+{
+	return get_cpu_type() == MESON_CPU_MAJOR_ID_T5W;
 }
 
 static inline bool cpu_after_eq(unsigned int id)
