beb9b336d977ff9ae7c89297d6c0b6a1709fb8cb
yao liu <yao.liu@amlogic.com>
2021-08-31 20:01:30 +0800

    amvecm: display error in pc monitor mode [1/1]
    PD#SWPL-59001
    
    Problem:
    rgb2yuv is skipped in pc monitor mode when dv enable,
    but actually dv not work(dv bypass)
    
    Solution:
    bypass rgb2yuv only when dv on
    
    Verify:
    t3
    
    Signed-off-by: yao liu <yao.liu@amlogic.com>
    Change-Id: Id5be3ef7446bc1fb8af1801f4d7eb11dd570ce17

--- drivers/amlogic/media/enhancement/amvecm/amcsc_pip.c
+++ drivers/amlogic/media/enhancement/amvecm/amcsc_pip.c
@@ -127,10 +127,10 @@ void hdr_proc(struct vframe_s *vf,
 	int i, index;
 
 	/* RGB / YUV vdin input handling  prepare extra op code or info */
-	if (vf->type & VIDTYPE_RGB_444 && !is_dolby_vision_enable())
+	if (vf->type & VIDTYPE_RGB_444 && !is_dolby_vision_on())
 		hdr_process_select |= RGB_VDIN;
 
-	if (limit_full && !is_dolby_vision_enable())
+	if (limit_full && !is_dolby_vision_on())
 		hdr_process_select |= FULL_VDIN;
 	/* RGB / YUV input handling */
 
@@ -149,9 +149,10 @@ void hdr_proc(struct vframe_s *vf,
 		}
 	}
 
-	pr_csc(8, "am_vecm: hdr module=%s, process=%s\n",
+	pr_csc(8, "am_vecm: hdr module=%s, process=%s, dv %d\n",
 	       module_str[module_sel],
-	       process_str[index]);
+	       process_str[index],
+	       is_dolby_vision_on());
 
 	if (module_sel == 1)
 		process_id[0] = index;
