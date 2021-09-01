ce8a06b0780d5a583d33e4945b164cb450bd1f37
qinglin.li <qinglin.li@amlogic.com>
2021-09-01 16:13:39 +0800

    bandwidth: modify the freq cal parameters [1/1]
    PD#SWPL-58236
    
    Problem:
    P1 the freq cal parameters different from T7
    
    Solution:
    modify the freq cal parameters
    
    Verify:
    AZP1 Ninox
    
    Change-Id: Ia779276ee340e095538d47f5517aac87f8776628
    Signed-off-by: qinglin.li <qinglin.li@amlogic.com>

--- drivers/amlogic/ddr_tool/ddr_band_op_t7.c
+++ drivers/amlogic/ddr_tool/ddr_band_op_t7.c
@@ -143,37 +143,72 @@ static unsigned long t7_get_dmc_freq_quick(struct ddr_bandwidth *db)
 
 	val = readl(db->pll_reg);
 	val = val & 0xfffff;
-	switch ((val >> 16) & 7) {
-	case 0:
-		od_div = 2;
-		break;
+	if (db->cpu_type == DMC_TYPE_P1) {
+		switch ((val >> 16) & 7) {
+		case 0:
+			od_div = 1;
+			break;
 
-	case 1:
-		od_div = 3;
-		break;
+		case 1:
+			od_div = 2;
+			break;
 
-	case 2:
-		od_div = 4;
-		break;
+		case 2:
+			od_div = 3;
+			break;
 
-	case 3:
-		od_div = 6;
-		break;
+		case 3:
+			od_div = 4;
+			break;
 
-	case 4:
-		od_div = 8;
-		break;
+		case 4:
+			od_div = 6;
+			break;
 
-	default:
-		break;
+		case 5:
+			od_div = 8;
+			break;
+
+		default:
+			break;
+		}
+	} else {
+		switch ((val >> 16) & 7) {
+		case 0:
+			od_div = 2;
+			break;
+
+		case 1:
+			od_div = 3;
+			break;
+
+		case 2:
+			od_div = 4;
+			break;
+
+		case 3:
+			od_div = 6;
+			break;
+
+		case 4:
+			od_div = 8;
+			break;
+
+		default:
+			break;
+		}
 	}
-
 	m = val & 0x1ff;
 	n = ((val >> 10) & 0x1f);
 	od1 = (((val >> 19) & 0x1)) == 1 ? 2 : 1;
 	freq = DEFAULT_XTAL_FREQ / 1000;	/* avoid overflow */
-	if (n)
-		freq = ((((freq * m) / n) >> od1) / od_div) * 1000;
+	if (n) {
+		if (db->cpu_type == DMC_TYPE_P1)
+			freq = ((((freq * m) / n) >> od1) / od_div) * 1000 / 2;
+		else
+
+			freq = ((((freq * m) / n) >> od1) / od_div) * 1000;
+	}
 
 	return freq;
 }
