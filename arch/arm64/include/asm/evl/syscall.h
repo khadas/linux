ea54d6dc07aecaccbea02eefaa81208e4ee85c45
Philippe Gerum <rpm@xenomai.org>
2021-09-05 17:24:04 +0200

    evl/syscall: handle prctl() call form
    Handle both the legacy and prctl-based call forms available from
    Dovetail, i.e.:
    
    syscall(sys_evl_<op> | __OOB_SYSCALL_BIT, ...) /* old form */
    syscall(__NR_prctl, sys_evl_<op> | __OOB_SYSCALL, ...) /* new form */
    
    With this change, we gain out-of-the-box support for Valgrind since
    applications can now emit EVL system calls wrapped in prctl()
    requests, which are readily recognized by the instrumentation
    framework.
    
    As a result, the ABI number is bumped to #27.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- arch/arm64/include/asm/evl/syscall.h
+++ arch/arm64/include/asm/evl/syscall.h
@@ -10,41 +10,12 @@
 #define raw_put_user(src, dst)  __put_user(src, dst)
 #define raw_get_user(dst, src)  __get_user(dst, src)
 
-#define oob_retval(__regs)	((__regs)->regs[0])
-#define oob_arg1(__regs)	((__regs)->regs[0])
-#define oob_arg2(__regs)	((__regs)->regs[1])
-#define oob_arg3(__regs)	((__regs)->regs[2])
-#define oob_arg4(__regs)	((__regs)->regs[3])
-#define oob_arg5(__regs)	((__regs)->regs[4])
-
 #define __ARM_NR_BASE_compat		0xf0000
 
-static inline bool is_oob_syscall(const struct pt_regs *regs)
-{
-	return !!(regs->syscallno & __OOB_SYSCALL_BIT);
-}
-
-static inline unsigned int oob_syscall_nr(const struct pt_regs *regs)
-{
-	return regs->syscallno & ~__OOB_SYSCALL_BIT;
-}
-
 static inline bool
-inband_syscall_nr(struct pt_regs *regs, unsigned int *nr)
+is_valid_inband_syscall(unsigned int nr)
 {
-	*nr = oob_syscall_nr(regs);
-
-	return !is_oob_syscall(regs);
-}
-
-static inline void set_oob_error(struct pt_regs *regs, int err)
-{
-	oob_retval(regs) = err;
-}
-
-static inline void set_oob_retval(struct pt_regs *regs, long ret)
-{
-	oob_retval(regs) = ret;
+	return nr < NR_syscalls;
 }
 
 #ifdef CONFIG_COMPAT
