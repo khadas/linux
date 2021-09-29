31d032243bb9b38a67143a4a8577d3bcf7c5d9c9
jian zhou <jian.zhou@amlogic.com>
2021-09-29 16:41:33 +0800

    audio: support more tdmin sources config [1/1]
    PD#SWPL-60766
    
    Problem:
    only can set acodec_adc source for tdmin.
    
    Solution:
    get tdmin source name from dts, and set source id by
    the souece name in tdm driver
    
    Verify:
    t3-ar301
    
    Change-Id: I2db68609adb0648275767abffc0dc72246d4e5cf
    Signed-off-by: jian zhou <jian.zhou@amlogic.com>

--- sound/soc/amlogic/auge/tdm.c
+++ sound/soc/amlogic/auge/tdm.c
@@ -89,7 +89,7 @@ struct aml_tdm {
 	int lane_ss;
 	/* virtual link for i2s to hdmitx */
 	int i2s2hdmitx;
-	int acodec_adc;
+	const char *tdmin_src_name;
 	uint last_mpll_freq;
 	uint last_mclk_freq;
 	uint last_fmt;
@@ -373,8 +373,10 @@ static int aml_tdm_set_fmt(struct aml_tdm *p_tdm, unsigned int fmt, bool capture
 capture:
 	/* update skew for ACODEC_ADC */
 	if (capture_active &&
-		p_tdm->chipinfo->adc_fn	&&
-		p_tdm->acodec_adc) {
+	    p_tdm->chipinfo->adc_fn &&
+	    p_tdm->tdmin_src_name &&
+	    strncmp(p_tdm->tdmin_src_name, SRC_ACODEC,
+		    strlen(p_tdm->tdmin_src_name)) == 0) {
 		aml_update_tdmin_skew(p_tdm->actrl, p_tdm->id, 4);
 		aml_update_tdmin_rev_ws(p_tdm->actrl, p_tdm->id, 0);
 	}
@@ -453,7 +455,7 @@ void aml_tdm_hw_setting_free(struct aml_tdm *p_tdm, int stream)
 	}
 }
 
-unsigned int get_tdmin_src(struct src_table *table, char *src)
+unsigned int get_tdmin_src(struct src_table *table, const char *src)
 {
 	for (; table->name[0]; table++) {
 		if (strncmp(table->name, src, strlen(src)) == 0)
@@ -466,35 +468,37 @@ unsigned int get_tdmin_src(struct src_table *table, char *src)
 void aml_tdmin_set_src(struct aml_tdm *p_tdm)
 {
 	int src_val;
-	char *tdm_src;
-	int id;
+
 	if (!p_tdm)
 		return;
-	id = p_tdm->id;
-	switch (id) {
-	case TDM_A:
-		tdm_src = SRC_TDMIN_A;
-		break;
-	case TDM_B:
-		tdm_src = SRC_TDMIN_B;
-		break;
-	case TDM_C:
-		tdm_src = SRC_TDMIN_C;
-		break;
-	case TDM_D:
-		tdm_src = SRC_TDMIN_D;
-		break;
-	default:
-		tdm_src = SRC_TDMIN_A;
-		break;
-	}
-	src_val = get_tdmin_src(p_tdm->chipinfo->tdmin_srcs, tdm_src);
-	aml_update_tdmin_src(p_tdm->actrl, p_tdm->id, src_val);
+
 	/* if tdm* is using internal ADC, reset tdmin src to ACODEC */
-	if (p_tdm->chipinfo->adc_fn && p_tdm->acodec_adc) {
-		src_val = get_tdmin_src(p_tdm->chipinfo->tdmin_srcs, SRC_ACODEC);
-		aml_update_tdmin_src(p_tdm->actrl, p_tdm->id, src_val);
+	if (p_tdm->chipinfo->adc_fn && p_tdm->tdmin_src_name) {
+		src_val = get_tdmin_src(p_tdm->chipinfo->tdmin_srcs,
+			p_tdm->tdmin_src_name);
+	} else {
+		const char *tdm_src;
+
+		switch (p_tdm->id) {
+		case TDM_A:
+			tdm_src = SRC_TDMIN_A;
+			break;
+		case TDM_B:
+			tdm_src = SRC_TDMIN_B;
+			break;
+		case TDM_C:
+			tdm_src = SRC_TDMIN_C;
+			break;
+		case TDM_D:
+			tdm_src = SRC_TDMIN_D;
+			break;
+		default:
+			tdm_src = SRC_TDMIN_A;
+			break;
+		}
+		src_val = get_tdmin_src(p_tdm->chipinfo->tdmin_srcs, tdm_src);
 	}
+	aml_update_tdmin_src(p_tdm->actrl, p_tdm->id, src_val);
 }
 
 void tdm_mute_capture(struct aml_tdm *p_tdm, bool mute)
@@ -1700,12 +1704,11 @@ static int aml_tdm_platform_probe(struct platform_device *pdev)
 	}
 	/* default no acodec_adc */
 	if (p_tdm->chipinfo->adc_fn) {
-		ret = of_property_read_u32(node, "acodec_adc",
-				&p_tdm->acodec_adc);
-		if (ret < 0)
-			p_tdm->acodec_adc = 0;
-		else
-			pr_info("TDM id %d supports ACODEC_ADC\n", p_tdm->id);
+		ret = of_property_read_string(node, "tdmin-src-name",
+				&p_tdm->tdmin_src_name);
+		if (!ret)
+			pr_info("TDM id %d supports %s\n",
+				p_tdm->id, p_tdm->tdmin_src_name);
 	}
 
 	ret = of_property_read_u32(node, "i2s2hdmi", &p_tdm->i2s2hdmitx);
