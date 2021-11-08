7bd8329bd13ed834b32611263c2d550a0d0c1202
Hanjie Lin <hanjie.lin@amlogic.com>
2021-11-08 16:33:03 +0800

    vmap: add ftrace -pg option in lib/ [1/1]
    PD#SWPL-63807
    
    Problem:
    CONFIG_AMLOGIC_VMAP reused x29(assume it's FP 16bytes alignment)
    leaf function may use x29 as common register(not assure 16bytes alignment)
    vmap exception will cause x29 corrupted, although we have added
    -mno-omit-leaf-frame-pointer option to avoid leaf function use x29 as common
    register, but it only works for gcc and clang11, not works for clang10.
    Now lib/ may offen use x29 as common register.
    
    Solution:
    for clang10, add ftrace -pg option to force not compile to leaf function
    
    Verify:
    sc2
    
    Change-Id: I54e747e32f198175ff84c044269d4861a7dbef44
    Signed-off-by: Hanjie Lin <hanjie.lin@amlogic.com>

--- Makefile
+++ Makefile
@@ -486,19 +486,13 @@ ifndef CONFIG_KASAN
 KBUILD_CFLAGS	+= -Werror
 endif
 
+ifdef CONFIG_AMLOGIC_MODIFY
 ifndef CONFIG_DYNAMIC_DEBUG
-KBUILD_CFLAGS   := -Wall -Wundef -Werror=strict-prototypes -Wno-trigraphs \
-		   -fno-strict-aliasing -fno-common -fshort-wchar -fno-PIE \
-		   -Werror=implicit-function-declaration -Werror=implicit-int \
-		   -Werror=return-type -Wno-format-security \
-		   -Wno-builtin-macro-redefined \
-		   -std=gnu89
-
 # re-define of __func__, __file__ to save rodata size
 AMLOGIC_KFLAGS 	:= -U__FILE__ -D__FILE__='"$(subst $(srctree)/,,$(<))"'
 export AMLOGIC_KFLAGS
 endif
-
+endif
 
 KBUILD_CPPFLAGS := -D__KERNEL__
 KBUILD_AFLAGS_KERNEL :=
--- scripts/amlogic/mk_p_builtin.sh
+++ scripts/amlogic/mk_p_builtin.sh
@@ -11,7 +11,7 @@ echo ""
 ######################## step 1:  env setup ###############################################
 ###########################################################################################
 echo "######################## step 1:  env setup ###############################################"
-export PATH=/opt/clang-r377782b/bin/:/opt/gcc-linaro-6.3.1-2017.02-x86_64_aarch64-linux-gnu/bin/:$PATH
+export PATH=/opt/clang-r383902/bin/:/opt/gcc-linaro-6.3.1-2017.02-x86_64_aarch64-linux-gnu/bin/:$PATH
 MAKE_CLANG='make ARCH=arm64 CC=clang HOSTCC=clang LD=ld.lld NM=llvm-nm OBJCOPY=llvm-objcopy CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-gnu- -j8'
 export INSTALL_MOD_PATH=./modules_install
 
