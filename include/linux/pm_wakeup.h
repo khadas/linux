3d77a37f66bd936e063dfc7e1532a3a44e08ba4a
goenjoy <goenjoy@khadas.com>
2021-11-08 18:13:24 +0800

    RTC: add khadas rtc[VIM4]
    Signed-off-by: tenk.wang <tenk.wang@wesion.com>
    Change-Id: I8341c87239c12ce19b758af7d4eb464e5c297ad5

--- include/linux/pm_wakeup.h
+++ include/linux/pm_wakeup.h
@@ -85,7 +85,9 @@ static inline void device_set_wakeup_path(struct device *dev)
 }
 
 /* drivers/base/power/wakeup.c */
+extern void wakeup_source_prepare(struct wakeup_source *ws, const char *name);
 extern struct wakeup_source *wakeup_source_create(const char *name);
+extern void wakeup_source_drop(struct wakeup_source *ws);
 extern void wakeup_source_destroy(struct wakeup_source *ws);
 extern void wakeup_source_add(struct wakeup_source *ws);
 extern void wakeup_source_remove(struct wakeup_source *ws);
@@ -196,6 +198,19 @@ static inline void pm_wakeup_event(struct device *dev, unsigned int msec)
 static inline void pm_wakeup_hard_event(struct device *dev)
 {
 	return pm_wakeup_dev_event(dev, 0, true);
+}
+
+static inline void k_wakeup_source_init(struct wakeup_source *ws,
+				      const char *name)
+{
+	wakeup_source_prepare(ws, name);
+	wakeup_source_add(ws);
+}
+
+static inline void k_wakeup_source_trash(struct wakeup_source *ws)
+{
+	wakeup_source_remove(ws);
+	wakeup_source_drop(ws);
 }
 
 #endif /* _LINUX_PM_WAKEUP_H */
