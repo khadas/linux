8e1374e5bbb50828aca228c3d5f7e8452de3105f
Bin.Wang2 <bin.wang2@amlogic.com>
2021-11-18 20:10:02 +0800

    amvecm: aipq pq policy [1/1]
    PD#SWPL-58553
    
    Problem:
    osd affect aipq effect
    
    Solution:
    add new policy
    
    Verify:
    T3
    
    Change-Id: I2d707800601814a4599102db4543bf9950e7f9c1
    Signed-off-by: Bin.Wang2 <bin.wang2@amlogic.com>

--- drivers/amlogic/media/video_sink/vpp_pq_process.c
+++ drivers/amlogic/media/video_sink/vpp_pq_process.c
@@ -358,8 +358,8 @@ void aipq_scs_proc(struct vframe_s *vf,
 	} else if ((diff_skin_pct + diff_green_pct + diff_blue_pct < color_th) &&
 			    (pre_top_one >= 0)) {
 		memcpy(out, cfg[pre_top_one], sizeof(int) * SCENES_VALUE);
-	} else if ((diff_skin_pct + diff_green_pct +
-			diff_blue_pct > color_th) && (top_one_prob <= 500)) {
+	} else if ((top_one == 1) && (pre_top_one == 3) && (pre_blue_pct > 500) &&
+		(pre_blue_pct < cur_blue_pct)) {
 		memcpy(out, cfg[pre_top_one], sizeof(int) * SCENES_VALUE);
 	} else {
 		if (pq_debug[2] == 0x8) {
