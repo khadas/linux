a00d23f643792b790921acf515f6a36947d4b9e3
Wenjie Qiao <wenjie.qiao@amlogic.com>
2022-01-04 14:31:28 +0800

    hdmitx: Resolution switching failed when switching to 4K [1/1]
    PD#SWPL-69102
    
    Problem:
    Resolution switching failed when switching to 4K
    
    Solution:
    judge attr and same mode error, modify code
    
    Verify:
    T7
    
    Change-Id: I4d5e22e470bf3bebb2669d8f20c85e174c91ec14
    Signed-off-by: Wenjie Qiao <wenjie.qiao@amlogic.com>

--- drivers/amlogic/media/vout/vout_serve/vout2_serve.c
+++ drivers/amlogic/media/vout/vout_serve/vout2_serve.c
@@ -230,8 +230,8 @@ static int set_vout2_mode(char *name)
 	vout_trim_string(name);
 	VOUTPR("vout2: vmode set to %s\n", name);
 
-	if ((vout2_check_same_vmodeattr(name) == 0) &&
-		(strcmp(name, vout2_mode))) {
+	if (vout2_check_same_vmodeattr(name) &&
+		(strcmp(name, vout2_mode) == 0)) {
 		VOUTPR("vout2: don't set the same mode as current\n");
 		return -1;
 	}
