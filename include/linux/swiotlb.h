30f334d1a0891ad5f91a9a08390f198576cd6cd1
Jianxiong Pan <jianxiong.pan@amlogic.com>
2022-01-04 10:17:47 +0800

    swiotlb: expand the size of swiotlb slots. [1/1]
    PD#SWPL-87401
    
    Problem:
    swiotlb buffer is full (sz: 6123520 bytes), total 32768 (slots),
    used 4208 (slots) overflow 0x000000015c48c000+6123520 of DMA
    mask ffffffff bus mask 0.
    
    Solution:
    expand the swiotlb slot size, from 256k to 4M.
    
    Verify:
    t7_an400.
    
    Change-Id: I9a7154f941de787645383e6e9df9a2082410f8c6
    Signed-off-by: Jianxiong Pan <jianxiong.pan@amlogic.com>

--- include/linux/swiotlb.h
+++ include/linux/swiotlb.h
@@ -22,7 +22,11 @@ enum swiotlb_force {
  * must be a power of 2.  What is the appropriate value ?
  * The complexity of {map,unmap}_single is linearly dependent on this value.
  */
+#ifdef CONFIG_AMLOGIC_MODIFY
+#define IO_TLB_SEGSIZE	2048
+#else
 #define IO_TLB_SEGSIZE	128
+#endif
 
 /*
  * log of the size of each IO TLB slab.  The number of slabs is command line
