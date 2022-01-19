117f3a4704c77e50022380803d1a0d6b8a315eaf
Ao Xu <ao.xu@amlogic.com>
2022-01-19 21:04:03 +0800

    drm: fix gem alloc dw buf for afbc [1/1]
    PD#SWPL-69672
    
    Problem:
    gem alloc dw buf for afbc flag
    
    Solution:
    gem alloc 4k size for afbc flag
    
    Verify:
    am301
    
    Change-Id: Ie9468e28b2a05740de5180793a2022d6cce24736
    Signed-off-by: Ao Xu <ao.xu@amlogic.com>

--- drivers/amlogic/drm/meson_gem.c
+++ drivers/amlogic/drm/meson_gem.c
@@ -58,10 +58,7 @@ static int am_meson_gem_alloc_ion_buff(struct am_meson_gem_object *
 	} else if (flags & MESON_USE_VIDEO_AFBC) {
 		meson_gem_obj->is_uvm = true;
 		meson_gem_obj->is_afbc = true;
-		id = meson_ion_codecmm_heap_id_get();
-		if (id)
-			dmabuf = ion_alloc(meson_gem_obj->base.size, (1 << id),
-						   ION_FLAG_EXTEND_MESON_HEAP);
+		dmabuf = ion_alloc(UVM_FAKE_SIZE, ION_HEAP_SYSTEM, 0);
 	} else {
 		dmabuf = ion_alloc(meson_gem_obj->base.size,
 				   ION_HEAP_SYSTEM, 0);
