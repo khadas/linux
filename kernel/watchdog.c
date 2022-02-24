6ee461f920fc4f32be0aa6a5017401352cdfeb4f
song.han <song.han@amlogic.com>
2022-02-24 15:04:33 +0800

    hardlockup: enable hardlockup func without NMI [1/2]
    PD#SWPL-72675
    
    Problem:
    porting hardlockup without nmi support.
    
    Solution:
    port hardlockup func from 5.4.
    
    Verify:
    C2-AF400
    
    Change-Id: I49538747eab546105d3daac472805009ce0dec53
    Signed-off-by: song.han <song.han@amlogic.com>

--- kernel/watchdog.c
+++ kernel/watchdog.c
@@ -179,8 +179,14 @@ static DEFINE_PER_CPU(unsigned long, watchdog_touch_ts);
 static DEFINE_PER_CPU(unsigned long, watchdog_report_ts);
 static DEFINE_PER_CPU(struct hrtimer, watchdog_hrtimer);
 static DEFINE_PER_CPU(bool, softlockup_touch_sync);
+#ifdef CONFIG_AMLOGIC_DEBUG_LOCKUP
+DEFINE_PER_CPU(unsigned long, hrtimer_interrupts);
+DEFINE_PER_CPU(unsigned long, hrtimer_interrupts_saved);
+extern void watchdog_check_hardlockup_other_cpu(void);
+#else
 static DEFINE_PER_CPU(unsigned long, hrtimer_interrupts);
 static DEFINE_PER_CPU(unsigned long, hrtimer_interrupts_saved);
+#endif
 static unsigned long soft_lockup_nmi_warn;
 
 static int __init nowatchdog_setup(char *str)
@@ -357,13 +363,16 @@ static enum hrtimer_restart watchdog_timer_fn(struct hrtimer *hrtimer)
 	struct pt_regs *regs = get_irq_regs();
 	int duration;
 	int softlockup_all_cpu_backtrace = sysctl_softlockup_all_cpu_backtrace;
-
 	if (!watchdog_enabled)
 		return HRTIMER_NORESTART;
 
 	/* kick the hardlockup detector */
 	watchdog_interrupt_count();
 
+#ifdef CONFIG_AMLOGIC_DEBUG_LOCKUP
+	/* test for hardlockups on the next cpu */
+	watchdog_check_hardlockup_other_cpu();
+#endif
 	/* kick the softlockup detector */
 	if (completion_done(this_cpu_ptr(&softlockup_completion))) {
 		reinit_completion(this_cpu_ptr(&softlockup_completion));
@@ -471,6 +480,9 @@ static void watchdog_enable(unsigned int cpu)
 	/* Enable the perf event */
 	if (watchdog_enabled & NMI_WATCHDOG_ENABLED)
 		watchdog_nmi_enable(cpu);
+#ifdef CONFIG_AMLOGIC_DEBUG_LOCKUP
+	watchdog_nmi_enable(cpu);
+#endif
 }
 
 static void watchdog_disable(unsigned int cpu)
