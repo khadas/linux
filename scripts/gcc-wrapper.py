c15f6f41c6e2cb412be0d3103b8a6537d7bc3db9
Addy Ke <addy.ke@rock-chips.com>
2022-02-25 10:10:52 +0800

    gcc-wrapper.py: ignore dhd_pcie_linux.c:1499
    This fix warning  dhd_pcie_linux.c:1499, build on Debian11 with aarch64-linux-gnu-gcc 10.2.1
    
    Signed-off-by: Addy Ke <addy.ke@rock-chips.com>
    Change-Id: I673b7635639700b7d9e65cfa0d697362140557a9

--- scripts/gcc-wrapper.py
+++ scripts/gcc-wrapper.py
@@ -59,6 +59,7 @@ allowed_warnings = set([
     "sched.h:1211", # include/linux/sched.h:1211:1: warning: type qualifiers ignored on function return type
     "halphyrf_8188e_ce.c:2208", # drivers/net/wireless/rockchip_wlan/rtl8189es/hal/phydm/rtl8188e/halphyrf_8188e_ce.c:2208:1: warning: the frame size of 1056 bytes is larger than 1024 bytes
     "halphyrf_8723b_ce.c:2879", # drivers/net/wireless/rockchip_wlan/rtl8723bu/hal/phydm/rtl8723b/halphyrf_8723b_ce.c:2879:1: warning: the frame size of 1056 bytes is larger than 1024 bytes
+    "dhd_pcie_linux.c:1499", #drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/dhd_pcie_linux.c:1499:12: warning: 'dhdpcie_device_scan' defined but not used [-Wunused-function]
  ])
 
 # Capture the name of the object file, can find it.
