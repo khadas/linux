492947a806dec892f67374bd43a27585264022a6
lei.liu <lei.liu@amlogic.com>
2022-03-23 16:00:17 +0800

    drm: fix the bug of support fb console with drm-fbdev [1/1]
    PD#SWPL-75827
    
    Problem:
    kernel panic and display error
    
    Solution:
    do initial plane before helper initial config
    
    Verify:
    T7
    
    Signed-off-by: lei.liu <lei.liu@amlogic.com>
    Change-Id: I2a1c2fec569bc9ff15b4ae3e2428c637dd478d46

--- drivers/amlogic/drm/meson_fbdev.c
+++ drivers/amlogic/drm/meson_fbdev.c
@@ -611,7 +611,8 @@ static struct device_attribute fbdev_device_attrs[] = {
 	__ATTR(force_free_mem, 0644, show_force_free_mem, store_force_free_mem),
 };
 
-struct meson_drm_fbdev *am_meson_create_drm_fbdev(struct drm_device *dev)
+struct meson_drm_fbdev *am_meson_create_drm_fbdev(struct drm_device *dev,
+					    struct drm_plane *plane)
 {
 	struct meson_drm *drmdev = dev->dev_private;
 	struct meson_drm_fbdev *fbdev;
@@ -626,6 +627,11 @@ struct meson_drm_fbdev *am_meson_create_drm_fbdev(struct drm_device *dev)
 		return NULL;
 
 	helper = &fbdev->base;
+
+	if (plane)
+		fbdev->plane = plane;
+	else
+		return NULL;
 
 	drm_fb_helper_prepare(dev, helper, &meson_drm_fb_helper_funcs);
 
@@ -692,8 +698,7 @@ int am_meson_drm_fbdev_init(struct drm_device *dev)
 	}
 
 	if (drmdev->primary_plane) {
-		fbdev = am_meson_create_drm_fbdev(dev);
-		fbdev->plane = drmdev->primary_plane;
+		fbdev = am_meson_create_drm_fbdev(dev, drmdev->primary_plane);
 		fbdev->zorder = OSD_PLANE_BEGIN_ZORDER;
 		DRM_INFO("create fbdev for primary plane [%p]\n", fbdev);
 	}
@@ -707,8 +712,7 @@ int am_meson_drm_fbdev_init(struct drm_device *dev)
 		if (osd_plane->base.type == DRM_PLANE_TYPE_PRIMARY)
 			continue;
 
-		fbdev = am_meson_create_drm_fbdev(dev);
-		fbdev->plane = &osd_plane->base;
+		fbdev = am_meson_create_drm_fbdev(dev, &osd_plane->base);
 		fbdev->zorder = OSD_PLANE_BEGIN_ZORDER + fbdev_cnt;
 		fbdev_cnt++;
 		DRM_INFO("create fbdev for plane [%d]-[%p]\n", osd_plane->plane_index, fbdev);
