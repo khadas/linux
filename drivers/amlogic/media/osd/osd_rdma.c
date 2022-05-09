53ad4266f63a8da9ff2f363cec2f0a896df0b7e2
Pengcheng Chen <pengcheng.chen@amlogic.com>
2022-05-09 14:26:42 +0800

    rdma: fix build error for a5  [1/1]
    PD#SWPL-80372
    
    Problem:
    a5 build error
    
    Solution:
    fix build error for a5
    
    Verify:
    a5 build
    
    Change-Id: I3a413902caa1a4fce03dba05037e7b38aee2ed20
    Signed-off-by: Pengcheng Chen <pengcheng.chen@amlogic.com>

--- drivers/amlogic/media/osd/osd_rdma.c
+++ drivers/amlogic/media/osd/osd_rdma.c
@@ -1107,6 +1107,11 @@ static unsigned int disable_osd_rdma_reset;
 module_param(disable_osd_rdma_reset, uint, 0664);
 MODULE_PARM_DESC(disable_osd_rdma_reset, "disable_osd_rdma_reset");
 
+int get_rdma_irq_done_line(u32 vpp_index)
+{
+	return rdma_done_line[VIU1];
+}
+
 #ifdef CONFIG_AMLOGIC_MEDIA_RDMA
 static int osd_reset_rdma_handle = -1;
 
@@ -1538,11 +1543,6 @@ static void osd_reset_rdma_func(u32 reset_bit)
 	} else {
 		rdma_clear(osd_reset_rdma_handle);
 	}
-}
-
-int get_rdma_irq_done_line(u32 vpp_index)
-{
-	return rdma_done_line[VIU1];
 }
 
 static void osd_rdma_irq(void *arg)
