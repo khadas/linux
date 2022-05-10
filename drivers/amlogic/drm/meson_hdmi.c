bbaa22c66c10e96a3a1477a25d9e05d367120db0
congyang.huang <congyang.huang@amlogic.com>
2022-05-10 20:31:16 +0800

    drm: get property affect hdmi attr setting [1/1]
    PD#SWPL-82205
    
    Problem:
    get property func affect hdmi attr setting
    
    Solution:
    jump cvbs attr setting
    modify get attr property
    
    Verify:
    verify on AH232 nagra
    
    Change-Id: Ie71b3a76d4b6ebed1acf701fcd1d34f57f98b359
    Signed-off-by: congyang.huang <congyang.huang@amlogic.com>

--- drivers/amlogic/drm/meson_hdmi.c
+++ drivers/amlogic/drm/meson_hdmi.c
@@ -131,6 +131,32 @@ void convert_attrstr(char *attr_str,
 	}
 }
 
+static int convert_attrstr_to_colorspace(char *attr_str)
+{
+	if (strstr(attr_str, "420"))
+		return COLORSPACE_YUV420;
+	else if (strstr(attr_str, "422"))
+		return COLORSPACE_YUV422;
+	else if (strstr(attr_str, "444"))
+		return COLORSPACE_YUV444;
+	else if (strstr(attr_str, "rgb"))
+		return COLORSPACE_RGB444;
+	else
+		return  COLORSPACE_RESERVED;
+}
+
+static int convert_attrstr_to_colordepth(char *attr_str)
+{
+	if (strstr(attr_str, "12bit"))
+		return 12;
+	else if (strstr(attr_str, "10bit"))
+		return 10;
+	else if (strstr(attr_str, "8bit"))
+		return 8;
+	else
+		return COLORDEPTH_RESERVED;
+}
+
 static void build_hdmitx_attr_str(char *attr_str, u32 format, u32 bit_depth)
 {
 	const char *colorspace;
@@ -493,19 +519,18 @@ static int am_hdmitx_connector_atomic_get_property
 	struct am_hdmi_tx *am_hdmi = connector_to_am_hdmi(connector);
 	struct am_hdmitx_connector_state *hdmitx_state =
 		to_am_hdmitx_connector_state(state);
-	struct hdmitx_color_attr *attr = &hdmitx_state->color_attr_para;
 	char attr_force[16];
 
 	am_hdmi_info.hdmitx_dev->get_attr(attr_force);
-	convert_attrstr(attr_force, attr);
+
 	if (property == am_hdmi->update_attr_prop) {
 		*val = 0;
 		return 0;
 	} else if (property == am_hdmi->color_space_prop) {
-		*val = attr->colorformat;
+		*val = convert_attrstr_to_colorspace(attr_force);
 		return 0;
 	} else if (property == am_hdmi->color_depth_prop) {
-		*val = attr->bitdepth;
+		*val = convert_attrstr_to_colordepth(attr_force);
 		return 0;
 	} else if (property == am_hdmi->avmute_prop) {
 		*val = hdmitx_state->avmute;
@@ -1133,6 +1158,8 @@ void meson_hdmitx_encoder_atomic_mode_set(struct drm_encoder *encoder,
 	if (am_hdmi_info.android_path)
 		return;
 
+	DRM_INFO("%s enter:attr[%d-%d]\n", __func__,
+		attr->colorformat, attr->bitdepth);
 	meson_hdmitx_decide_eotf_type(meson_crtc_state, hdmitx_state);
 
 	/*check force attr info: from uboot set or debugfs;
@@ -1141,10 +1168,9 @@ void meson_hdmitx_encoder_atomic_mode_set(struct drm_encoder *encoder,
 	 */
 	if (attr_force_debugfs) {
 		attr_force_debugfs = false;
-		convert_attrstr(attr_debugfs, attr);
 		am_hdmi_info.hdmitx_dev->setup_attr(attr_debugfs);
 		DRM_INFO("debugfs attr\n");
-		goto end;
+		return;
 	}
 
 	if (hdmitx_state->color_force) {
@@ -1169,7 +1195,7 @@ void meson_hdmitx_encoder_atomic_mode_set(struct drm_encoder *encoder,
 				update_attr = false;
 			} else {
 				update_attr = true;
-				DRM_DEBUG_KMS("%s: force attr fail[%d-%d]\n",
+				DRM_INFO("%s: force attr fail[%d-%d]\n",
 					__func__, attr->colorformat, attr->bitdepth);
 			}
 		} else {
@@ -1183,10 +1209,6 @@ void meson_hdmitx_encoder_atomic_mode_set(struct drm_encoder *encoder,
 	}
 
 	meson_hdmitx_setup_color_attr(attr);
-
-end:
-	DRM_INFO("[%s]end\n", __func__);
-
 }
 
 static
