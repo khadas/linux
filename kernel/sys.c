e761da631b11a5422c11529848ff11387de304ff
Hanjie Lin <hanjie.lin@amlogic.com>
2022-06-06 14:06:19 +0800

    bpf: use fake uname for bpfloader and netd [1/1]
    PD#SWPL-81144
    
    Problem:
    32bit kernel user crash
    
    Solution:
    workaround use fake uname(kernel version don't support bpf)
    for bpfloader and netd, and they will not use bpf function.
    
    Verify:
    t5w
    
    Change-Id: I7b356eed5384350a9472e90aeb2b9bcf4d741dac
    Signed-off-by: Hanjie Lin <hanjie.lin@amlogic.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1246,6 +1246,17 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
+#ifdef CONFIG_AMLOGIC_MODIFY
+#ifdef CONFIG_ARM
+	pr_debug("newuname: %s/%d release=%s\n", current->comm, current->pid, tmp.release);
+	if (!strncmp(current->comm, "bpfloader", 9) ||
+	    !strncmp(current->comm, "netd", 4)) {
+		strcpy(tmp.release, "3.14.1"); //fake version don't support bpf
+		pr_debug("fake uname: %s/%d release=%s\n",
+			 current->comm, current->pid, tmp.release);
+	}
+#endif
+#endif
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
