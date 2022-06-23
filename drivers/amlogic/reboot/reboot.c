b6a625b8d196a6f1099ac6e53b5f44877f535e71
Xingxing Wang <xingxing.wang@amlogic.com>
2022-06-23 17:04:34 +0800

    reboot: add debug interface to reboot driver [1/1]
    PD#SWPL-85257
    
    Problem:
    add debug interface to reboot driver
    
    Solution:
    add debug interface to reboot driver. for example,
    echo recovery > /sys/devices/platform/aml_reboot/reset_test to test
    recovery mode reboot.
    
    Verify:
    AXG S420
    
    Change-Id: Ie555adbd13032e1185ff479935fbbd9f8fd67c35
    Signed-off-by: Xingxing Wang <xingxing.wang@amlogic.com>

--- drivers/amlogic/reboot/reboot.c
+++ drivers/amlogic/reboot/reboot.c
@@ -181,6 +181,24 @@ ssize_t reboot_reason_show(struct device *dev,
 
 static DEVICE_ATTR_RO(reboot_reason);
 
+static ssize_t reset_test_store(struct device *dev,
+			   struct device_attribute *attr,  const char *buf, size_t count)
+{
+	char s[64] = {0};
+	u32 reason, i = 0;
+
+	while (*buf != '\n')
+		s[i++] = *buf++;
+	reason = parse_reason(s);
+
+	pr_info("[%s %d]%s reset, reboot reason:%u\n", __func__, __LINE__, s, reason);
+	do_aml_restart(NULL, 0, (void *)s);
+
+	return count;
+}
+
+static DEVICE_ATTR_WO(reset_test);
+
 static void disable_non_bootcpu_shutdown(void)
 {
 	int error = 0, cpu, primary = 0;
@@ -232,6 +250,10 @@ static int aml_restart_probe(struct platform_device *pdev)
 	if (!of_property_read_u32(pdev->dev.of_node, "sys_reset", &id)) {
 		psci_function_id_restart = id;
 		register_restart_handler(&aml_restart_nb);
+		ret = device_create_file(&pdev->dev, &dev_attr_reset_test);
+		if (ret != 0)
+			pr_err("%s, device create file failed, ret = %d!\n",
+			       __func__, ret);
 	}
 
 	if (!of_property_read_u32(pdev->dev.of_node, "sys_poweroff", &id)) {
