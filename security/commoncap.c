786fcc3996c34cd7671e2944fb7afca51b98ad84
Goenjoy Huang <goenjoy@khadas.com>
2022-07-05 14:14:33 +0000

    Add root function support [4/4]
    Change-Id: I60e74e44555cbcf00e34573393667e4b7f67cd48
    Signed-off-by: Goenjoy Huang <goenjoy@khadas.com>

--- security/commoncap.c
+++ security/commoncap.c
@@ -1165,11 +1165,11 @@ static int cap_prctl_drop(unsigned long cap)
 {
 	struct cred *new;
 
-	if (!ns_capable(current_user_ns(), CAP_SETPCAP))
+/*	if (!ns_capable(current_user_ns(), CAP_SETPCAP))
 		return -EPERM;
 	if (!cap_valid(cap))
 		return -EINVAL;
-
+*/
 	new = prepare_creds();
 	if (!new)
 		return -ENOMEM;
