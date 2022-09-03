002eaca86fb0e3259e05b6d29a1d7265909b0f0e
Jianxiong Pan <jianxiong.pan@amlogic.com>
2022-09-03 15:16:26 +0800

    mm: increase task user nice of kswapd. [1/1]
    PD#SWPL-92714
    
    Problem:
    increase task nice of kswapd.
    
    Solution:
    increase task nice of kswapd.
    
    Verify:
    build pass.
    
    Change-Id: Ia3d5464483b95eccf60d8c3b0e45cc899eb9e1ce
    Signed-off-by: Jianxiong Pan <jianxiong.pan@amlogic.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -7030,6 +7030,9 @@ int kswapd(void *p)
 
 	WRITE_ONCE(pgdat->kswapd_order, 0);
 	WRITE_ONCE(pgdat->kswapd_highest_zoneidx, MAX_NR_ZONES);
+#ifdef CONFIG_AMLOGIC_CMA
+	set_user_nice(current, -5);
+#endif /* CONFIG_AMLOGIC_CMA */
 	for ( ; ; ) {
 		bool ret;
 
