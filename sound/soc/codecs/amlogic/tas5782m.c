50d090e1fad113ddefa08d848c3817ba3719d0a4
shu.wang <shu.wang@amlogic.com>
2022-09-29 13:19:58 +0800

    AXG: Add axg v03 dts [1/1]
    PD#SWPL-94544
    
    Problem:
    The AXG DTS in the current kernel 5.4 can't adapt to the latest hardware.
    
    Solution:
    1.Add s400 v03 dts.
    2.Add s400 soundbar v03 dts.
    3.Add s420 v03 dts.
    4.Enable tas5782m codec config in defconfig.
    5.Fix tas5782m driver compile fail.
    
    Verify:
    A113D-S400 A113X-S420
    
    Change-Id: Ib04ef06d1c933cfa12074e0d50ce0ac4c083b411
    Signed-off-by: shu.wang <shu.wang@amlogic.com>

--- sound/soc/codecs/amlogic/tas5782m.c
+++ sound/soc/codecs/amlogic/tas5782m.c
@@ -404,8 +404,10 @@ static int tas5782m_hw_params(struct snd_pcm_substream *substream,
 	case SNDRV_PCM_FORMAT_S24_LE:
 	case SNDRV_PCM_FORMAT_S24_BE:
 		pr_debug("24bit\n");
-	/* go through */
+		break;
 	case SNDRV_PCM_FORMAT_S32_LE:
+		pr_debug("32bit\n");
+		break;
 	case SNDRV_PCM_FORMAT_S20_3LE:
 	case SNDRV_PCM_FORMAT_S20_3BE:
 		pr_debug("20bit\n");
