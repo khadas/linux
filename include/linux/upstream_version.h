2cb2aae05a123094c5a5804610facaa66ab67f6f
Wanwei Jiang <wanwei.jiang@amlogic.com>
2022-10-10 10:23:00 +0800

    Merge branch 'android13-5.15' into amlogic-5.15-dev [1/1]
    PD#SWPL-87897
    
    Problem:
    print upstream commit version
    
    Solution:
    MERGE_DATE             "2022-10-12"
    UPSTREAM_VERSION       "fa951e233fbd"
    AM_PATCH_VERSION       "55b0ae4f9906"
    
    Verify:
    sc2
    
    Change-Id: I705029781d89c078dd76532df95560a5c9e29a2f
    Signed-off-by: Wanwei Jiang <wanwei.jiang@amlogic.com>

---
+++ include/linux/upstream_version.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
+/*
+ * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
+ */
+
+#ifndef _LINUX_UPSTREAM_VERSION_H
+#define _LINUX_UPSTREAM_VERSION_H
+
+#define MERGE_DATE		"2022-10-12"
+#define UPSTREAM_VERSION	"fa951e233fbd"
+#define AM_PATCH_VERSION	"55b0ae4f9906"
+
+#endif
