1b61d15dcf0241725435f9ca4adad40d70b24f99
qiyao.zhou <qiyao.zhou@amlogic.com>
2022-10-10 20:03:44 +0800

    vpp: picture has noise after composer. [1/1]
    PD#SWPL-96856
    
    Problem:
    default color flow in vpp need flow vicp.
    
    Solution:
    modify default color flow in vpp.
    
    Verify:
    S5
    
    Signed-off-by: qiyao.zhou <qiyao.zhou@amlogic.com>
    Change-Id: Ib7a6a58484737d92ad69fdd13cb839a113584cdb

--- drivers/amlogic/media/video_sink/video_hw_s5.c
+++ drivers/amlogic/media/video_sink/video_hw_s5.c
@@ -4331,8 +4331,8 @@ static void vd1_set_dcu_s5(struct video_layer_s *layer,
 
 		r = 0x1700;
 		if (vf &&
-		    (vf->source_type != VFRAME_SOURCE_TYPE_HDMI &&
-		    !IS_DI_POSTWRTIE(vf->type)))
+		  (vf->source_type != VFRAME_SOURCE_TYPE_HDMI &&
+		  (!IS_DI_POSTWRTIE(vf->type) && !(vf->flag & VFRAME_FLAG_COMPOSER_DONE))))
 			r |= (1 << 19); /* dos_uncomp */
 		if (type & VIDTYPE_COMB_MODE)
 			r |= (1 << 20);
@@ -4827,8 +4827,8 @@ static void vd1_set_slice_dcu_s5(struct video_layer_s *layer,
 
 		r = 0x1700;
 		if (vf &&
-		    (vf->source_type != VFRAME_SOURCE_TYPE_HDMI &&
-		    !IS_DI_POSTWRTIE(vf->type)))
+		   (vf->source_type != VFRAME_SOURCE_TYPE_HDMI &&
+		   (!IS_DI_POSTWRTIE(vf->type) && !(vf->flag & VFRAME_FLAG_COMPOSER_DONE))))
 			r |= (1 << 19); /* dos_uncomp */
 		if (type & VIDTYPE_COMB_MODE)
 			r |= (1 << 20);
@@ -5231,8 +5231,8 @@ static void vdx_set_dcu_s5(struct video_layer_s *layer,
 		r = 0x1700;
 		if (cpu_after_eq(MESON_CPU_MAJOR_ID_TL1)) {
 			if (vf &&
-			    (vf->source_type != VFRAME_SOURCE_TYPE_HDMI &&
-			    !IS_DI_POSTWRTIE(vf->type)))
+			  (vf->source_type != VFRAME_SOURCE_TYPE_HDMI &&
+			  (!IS_DI_POSTWRTIE(vf->type) && !(vf->flag & VFRAME_FLAG_COMPOSER_DONE))))
 				r |= (1 << 19); /* dos_uncomp */
 			if (type & VIDTYPE_COMB_MODE)
 				r |= (1 << 20);
