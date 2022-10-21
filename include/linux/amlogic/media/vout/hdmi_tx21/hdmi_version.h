3ee0a00adf050388399d112c2c27aa71e3b104c3
xiang.wu1 <xiang.wu1@amlogic.com>
2022-10-21 16:22:22 +0800

    hdmitx: HDCP failure on resume, EDID read failed [1/1]
    PD#SWPL-93674
    
    Problem:
    there's such case: plugin irq->hdmitx resume + read EDID +
    resume uevent->mode setting + hdcp auth->plugin handler
    read+ EDID,now EDID already read done and hdcp already
    started, not read EDID again.
    
    Solution:
    not read EDID again if EDID already read done
    
    Verify:
    s905y4
    
    Change-Id: Ibdf4646ee8b4a11f14088f4c2bea18ba5a122b72
    Signed-off-by: xiang.wu1 <xiang.wu1@amlogic.com>

--- include/linux/amlogic/media/vout/hdmi_tx21/hdmi_version.h
+++ include/linux/amlogic/media/vout/hdmi_tx21/hdmi_version.h
@@ -37,6 +37,7 @@
 	"V03.00.00.00.03.03.07 [20220901] [NEWF] add dual vsif interface for allm\n" \
 	"V03.00.00.00.03.03.08 [20220903] [BUG] add DDC reset before do EDID transaction\n" \
 	"V03.00.00.00.03.03.09 [20220919] [BUG] y422 mapping and Enable the dither\n" \
-	"V03.00.00.00.03.03.10 [20220919] [BUG] Don't reset variables when parse a new block\n"
+	"V03.00.00.00.03.03.10 [20220919] [BUG] Don't reset variables when parse a new block\n" \
+	"V03.00.00.00.03.03.11 [20221021] [BUG] not read EDID again if EDID already read done\n"
 
 #endif // __HDMI21_VERSION_H__
