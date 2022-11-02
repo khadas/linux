4e8e84e779b9c0cff4324300a1762eab72e19675
Jianxiong Pan <jianxiong.pan@amlogic.com>
2022-11-02 20:24:44 +0800

    mm: backport a vendor hook for arch_setup_dma_ops. [1/1]
    PD#SWPL-100285
    
    Add vendor hook for arch_setup_dma_ops to allow vendor enhancement. This
    hook can be used to setup vendor-specific DMA ops for certain IOMMU
    clients based on their IOMMU configuration.
    
    Bug: 179291683
    
    Change-Id: Ie847a14d6536eb151cbd0a93452f6895ee451930
    Signed-off-by: Patrick Daly <pdaly@codeaurora.org>
    Signed-off-by: Chris Goldsworthy <cgoldswo@codeaurora.org>
    (cherry picked from commit 575ad9a2638d4d1ecd2063c1007a7bb71186ff38)
    Signed-off-by: Isaac J. Manjarres <isaacm@codeaurora.org>
    Signed-off-by: Jianxiong Pan <jianxiong.pan@amlogic.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -24,6 +24,7 @@
 #include <trace/hooks/usb.h>
 #include <trace/hooks/psci.h>
 #include <trace/hooks/ion.h>
+#include <trace/hooks/iommu.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -84,3 +85,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_usb_persist_overwrite);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_psci_tos_resident_on);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_psci_cpu_suspend);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ion_buffer_release);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_iommu_setup_dma_ops);
