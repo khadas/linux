c1b7c495169473a5f1c0a9195b1032f7e01d8f29
yujun.zhang <yujun.zhang@amlogic.com>
2022-11-23 21:07:47 +0800

    drm: update header to MIT license [1/1]
    PD#SWPL-102479
    
    Problem:
    Code pollution for codes with amlogic
    drm header files
    
    Solution:
    Use MIT license header, and keep GPL license
    in implementation files.
    
    Verify:
    sc2
    
    Change-Id: I46c5f181923ac1b6617579dbd1271731af805fa4
    Signed-off-by: yujun.zhang <yujun.zhang@amlogic.com>

--- include/uapi/drm/meson_drm.h
+++ include/uapi/drm/meson_drm.h
@@ -1,6 +1,6 @@
-/* SPDX-License-Identifier: (GPL-2.0+ WITH Linux-syscall-note OR MIT) */
+/* SPDX-License-Identifier: MIT */
 /*
- * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
+ * Copyright © 2019-2022 Amlogic Inc.
  */
 
 #ifndef _MESON_DRM_H
