74139bbd0c5eb26b3fa885e7b4ac154835ca6673
Shuai Li <shuai.li@amlogic.com>
2022-11-30 09:51:21 +0800

    audio: fix TDM HBR audio channel issue [2/3]
    PD#SWPL-100441
    
    Problem:
    When HBR stream goes into TDM device, the channel number is 2.
    Usually only stereo pcm is for HDMITX output even the TDM in config
    to 8 channels.
    But for HBR cases, 8 channels are mandatory.
    
    Solution:
    Only when stereo pcm type is configured, we send 2 channels to HDMITX.
    
    Verify:
    S5: tyson.
    
    Change-Id: I18e04ec61722b0d844ea2e1a24c86cb622adc201
    Signed-off-by: Shuai Li <shuai.li@amlogic.com>

--- sound/soc/amlogic/auge/tdm.c
+++ sound/soc/amlogic/auge/tdm.c
@@ -1435,10 +1435,10 @@ static int aml_dai_tdm_prepare(struct snd_pcm_substream *substream,
 			memset(&aud_param, 0, sizeof(aud_param));
 			aud_param.rate = runtime->rate;
 			aud_param.size = runtime->sample_bits;
-			if (codec_type == AUD_CODEC_TYPE_MULTI_LPCM)
-				aud_param.chs  = runtime->channels;
-			else
+			if (codec_type == AUD_CODEC_TYPE_STEREO_PCM)
 				aud_param.chs  = 2;
+			else
+				aud_param.chs  = runtime->channels;
 			aud_param.aud_src_if = AUD_SRC_IF_I2S;
 
 			memset(&chsts, 0, sizeof(chsts));
