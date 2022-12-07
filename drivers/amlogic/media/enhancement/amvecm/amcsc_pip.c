0bb09fa5d2c28ab988441beff75f0837c74cc93a
yao liu <yao.liu@amlogic.com>
2022-12-07 10:39:48 +0800

    csc: Changed from HDR to SDR after repeating HDMI hotplug [1/1]
    PD#SWPL-104477
    
    Problem:
    cur cur output_format is not update when hotplug, lead to no drm pkt
    
    Solution:
    reset cur output_format when null vout device
    
    Verify:
    sc2
    
    Change-Id: I9b447a1ad5cda24ff0c0c4030f8a7da321006e6e
    Signed-off-by: yao liu <yao.liu@amlogic.com>

--- drivers/amlogic/media/enhancement/amvecm/amcsc_pip.c
+++ drivers/amlogic/media/enhancement/amvecm/amcsc_pip.c
@@ -1861,6 +1861,16 @@ void hdmi_packet_process(int signal_change_flag,
 			vd_signal.signal_type = SIGNAL_SDR;
 			notify_vd_signal_to_amvideo(&vd_signal, vpp_index);
 		}
+		output_format = BT709;
+		pr_csc(4,
+			"am_vecm: vd%d %s %s, vd2 %s %s, update output_format %s => %s\n",
+			vd_path + 1,
+			is_video_layer_on(VD1_PATH) ? "on" : "off",
+			output_str[target_format[VD1_PATH]],
+			is_video_layer_on(VD2_PATH) ? "on" : "off",
+			output_str[target_format[VD2_PATH]],
+			output_str[cur_output_format],
+			output_str[output_format]);
 		return;
 	}
 
