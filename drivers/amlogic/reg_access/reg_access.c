03e9c395599fcff5d01d14f21f2ed29068bae1a9
qiankun.wang <qiankun.wang@amlogic.com>
2023-01-17 14:31:52 +0800

    reg_access: add virtual memory access support [1/1]
    PD#SWPL-103032
    
    Problem:
    The original interface can only view
    physical addresses, not virtual addresses.
    
    Solution:
    add 2 interfaces, vaddr and vdump.
    
    Verify:
    SC2\T7
    
    Change-Id: If6c92bad1280e1f3931258daca415f7703281192
    Signed-off-by: qiankun.wang <qiankun.wang@amlogic.com>

--- drivers/amlogic/reg_access/reg_access.c
+++ drivers/amlogic/reg_access/reg_access.c
@@ -17,16 +17,21 @@
 #include <linux/kasan.h>
 #include <linux/highmem.h>
 
-static struct aml_ddev {
-	unsigned int cached_reg_addr;
-	unsigned int size;
+struct aml_ddev {
+	unsigned long cached_reg_addr;
+	unsigned long size;
 	int (*debugfs_reg_access)(struct aml_ddev *indio_dev,
-				  unsigned int reg, unsigned int writeval,
+				  unsigned long reg, unsigned int writeval,
 				  unsigned int *readval);
-} aml_dev;
+};
+
+static struct aml_ddev paddr_dev;
+static struct aml_ddev pdump_dev;
+static struct aml_ddev vaddr_dev;
+static struct aml_ddev vdump_dev;
 
 int aml_reg_access(struct aml_ddev *indio_dev,
-		   unsigned int reg, unsigned int writeval,
+		   unsigned long reg, unsigned int writeval,
 		   unsigned int *readval)
 {
 	void __iomem *vaddr;
@@ -45,7 +50,7 @@ int aml_reg_access(struct aml_ddev *indio_dev,
 }
 
 int aml_mem_access(struct aml_ddev *indio_dev,
-		   unsigned int addr, unsigned int writeval,
+		   unsigned long addr, unsigned int writeval,
 		   unsigned int *readval)
 {
 	void *vaddr;
@@ -60,6 +65,19 @@ int aml_mem_access(struct aml_ddev *indio_dev,
 	kasan_enable_current();
 	kunmap(page);
 	return 0;
+}
+
+int aml_vaddr_access(struct aml_ddev *indio_dev,
+		   unsigned long vaddr, unsigned int writeval,
+		   unsigned int *readval)
+{
+	int ret;
+
+	if (readval)
+		ret = probe_kernel_read(readval, (void *)vaddr, sizeof(int));
+	else
+		ret = probe_kernel_write((void *)vaddr, (void *)&writeval, sizeof(int));
+	return ret;
 }
 
 static ssize_t paddr_read_file(struct file *file, char __user *userbuf,
@@ -78,7 +96,7 @@ static ssize_t paddr_read_file(struct file *file, char __user *userbuf,
 	if (ret)
 		pr_err("%s: read failed\n", __func__);
 
-	len = snprintf(buf, sizeof(buf), "[0x%x] = 0x%X\n",
+	len = snprintf(buf, sizeof(buf), "[0x%lx] = 0x%X\n",
 		       indio_dev->cached_reg_addr, val);
 
 	return simple_read_from_buffer(userbuf, count, ppos, buf, len);
@@ -88,7 +106,8 @@ static ssize_t paddr_write_file(struct file *file, const char __user *userbuf,
 				size_t count, loff_t *ppos)
 {
 	struct aml_ddev *indio_dev = file->private_data;
-	unsigned int reg, val;
+	unsigned long reg;
+	unsigned int val;
 	char buf[80];
 	int ret;
 
@@ -98,7 +117,7 @@ static ssize_t paddr_write_file(struct file *file, const char __user *userbuf,
 
 	buf[count] = 0;
 
-	ret = sscanf(buf, "%x %x", &reg, &val);
+	ret = sscanf(buf, "%lx %x", &reg, &val);
 
 	if (pfn_valid(reg >> PAGE_SHIFT))
 		indio_dev->debugfs_reg_access = aml_mem_access;
@@ -131,12 +150,13 @@ static const struct file_operations paddr_file_ops = {
 	.write		= paddr_write_file,
 };
 
-static ssize_t dump_write_file(struct file *file, const char __user *userbuf,
+static ssize_t pdump_write_file(struct file *file, const char __user *userbuf,
 			       size_t count, loff_t *ppos)
 {
 	struct seq_file *s = file->private_data;
 	struct aml_ddev *indio_dev = s->private;
-	unsigned int reg, val;
+	unsigned long reg;
+	unsigned int val;
 	char buf[80];
 	int ret;
 
@@ -146,7 +166,7 @@ static ssize_t dump_write_file(struct file *file, const char __user *userbuf,
 
 	buf[count] = 0;
 
-	ret = sscanf(buf, "%x %i", &reg, &val);
+	ret = sscanf(buf, "%lx %i", &reg, &val);
 	if (pfn_valid(reg >> PAGE_SHIFT))
 		indio_dev->debugfs_reg_access = aml_mem_access;
 	else
@@ -165,21 +185,23 @@ static ssize_t dump_write_file(struct file *file, const char __user *userbuf,
 
 static int dump_show(struct seq_file *s, void *what)
 {
-	unsigned int  i = 0, val;
+	unsigned int i = 0, val;
 	int ret;
 	struct aml_ddev *indio_dev = s->private;
 
-	for (i = 0; i < indio_dev->size ; i++) {
+	for (i = 0; i < indio_dev->size; i++) {
 		ret = indio_dev->debugfs_reg_access(indio_dev,
 				  indio_dev->cached_reg_addr,
 				  0, &val);
 		if (ret)
 			pr_err("%s: read failed\n", __func__);
 
-		seq_printf(s, "[0x%x] = 0x%X\n",
+		seq_printf(s, "[0x%lx] = 0x%X\n",
 			   indio_dev->cached_reg_addr, val);
-		indio_dev->cached_reg_addr += 4;
+		indio_dev->cached_reg_addr += sizeof(int);
 	}
+	indio_dev->cached_reg_addr -= sizeof(int) * indio_dev->size;
+
 	return 0;
 }
 
@@ -188,10 +210,116 @@ static int dump_open(struct inode *inode, struct file *file)
 	return single_open(file, dump_show, inode->i_private);
 }
 
-static const struct file_operations dump_file_ops = {
+static const struct file_operations pdump_file_ops = {
 	.open		= dump_open,
 	.read		= seq_read,
-	.write		= dump_write_file,
+	.write		= pdump_write_file,
+	.llseek		= seq_lseek,
+	.release	= single_release,
+};
+
+static ssize_t vaddr_read_file(struct file *file, char __user *userbuf,
+			       size_t count, loff_t *ppos)
+{
+	struct aml_ddev *indio_dev = file->private_data;
+	char buf[80];
+	unsigned int val = 0;
+	ssize_t len;
+	int ret = -1;
+
+	if (indio_dev->debugfs_reg_access)
+		ret = indio_dev->debugfs_reg_access(indio_dev,
+					   indio_dev->cached_reg_addr,
+					   0, &val);
+	if (ret)
+		pr_err("%s: read failed\n", __func__);
+
+	len = snprintf(buf, sizeof(buf), "[0x%lx] = 0x%X\n",
+		       indio_dev->cached_reg_addr, val);
+
+	return simple_read_from_buffer(userbuf, count, ppos, buf, len);
+}
+
+static ssize_t vaddr_write_file(struct file *file, const char __user *userbuf,
+				size_t count, loff_t *ppos)
+{
+	struct aml_ddev *indio_dev = file->private_data;
+	unsigned long reg;
+	unsigned int val;
+	char buf[80];
+	int ret;
+
+	count = min_t(size_t, count, (sizeof(buf) - 1));
+	if (copy_from_user(buf, userbuf, count))
+		return -EFAULT;
+
+	buf[count] = 0;
+
+	ret = sscanf(buf, "%lx %x", &reg, &val);
+
+	indio_dev->debugfs_reg_access = aml_vaddr_access;
+
+	switch (ret) {
+	case 1:
+		indio_dev->cached_reg_addr = reg;
+		break;
+	case 2:
+		indio_dev->cached_reg_addr = reg;
+		ret = indio_dev->debugfs_reg_access(indio_dev, reg,
+					val, NULL);
+		if (ret) {
+			pr_err("%s: write failed\n", __func__);
+			return ret;
+		}
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	return count;
+}
+
+static const struct file_operations vaddr_file_ops = {
+	.open		= simple_open,
+	.read		= vaddr_read_file,
+	.write		= vaddr_write_file,
+};
+
+static ssize_t vdump_write_file(struct file *file, const char __user *userbuf,
+			       size_t count, loff_t *ppos)
+{
+	struct seq_file *s = file->private_data;
+	struct aml_ddev *indio_dev = s->private;
+	unsigned long reg;
+	unsigned int val;
+	char buf[80];
+	int ret;
+
+	count = min_t(size_t, count, (sizeof(buf) - 1));
+	if (copy_from_user(buf, userbuf, count))
+		return -EFAULT;
+
+	buf[count] = 0;
+
+	ret = sscanf(buf, "%lx %i", &reg, &val);
+
+	indio_dev->debugfs_reg_access = aml_vaddr_access;
+	switch (ret) {
+	case 2:
+		indio_dev->cached_reg_addr = reg;
+		indio_dev->size = val;
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	return count;
+}
+
+static const struct file_operations vdump_file_ops = {
+	.open		= dump_open,
+	.read		= seq_read,
+	.write		= vdump_write_file,
 	.llseek		= seq_lseek,
 	.release	= single_release,
 };
@@ -208,9 +336,13 @@ static int __init aml_debug_init(void)
 			return -ENOMEM;
 		}
 		debugfs_create_file("paddr", S_IFREG | 0440,
-				    dir_aml_reg, &aml_dev, &paddr_file_ops);
-		debugfs_create_file("dump", S_IFREG | 0440,
-				    dir_aml_reg, &aml_dev, &dump_file_ops);
+				    dir_aml_reg, &paddr_dev, &paddr_file_ops);
+		debugfs_create_file("pdump", S_IFREG | 0440,
+				    dir_aml_reg, &pdump_dev, &pdump_file_ops);
+		debugfs_create_file("vaddr", S_IFREG | 0440,
+				    dir_aml_reg, &vaddr_dev, &vaddr_file_ops);
+		debugfs_create_file("vdump", S_IFREG | 0440,
+				    dir_aml_reg, &vdump_dev, &vdump_file_ops);
 	}
 
 	return 0;
