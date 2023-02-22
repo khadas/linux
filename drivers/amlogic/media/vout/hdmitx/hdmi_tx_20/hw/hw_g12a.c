946fcac1188fe9e2ae6cbaecf389ef325d847f6c
Xiong Zhang <xiong.zhang@wesion.com>
2023-02-22 17:03:04 +0800

    HDMI: update 2560x1600 resolution timimg parameter [2/2]
    Signed-off-by: Xiong Zhang <xiong.zhang@wesion.com>

--- drivers/amlogic/media/vout/hdmitx/hdmi_tx_20/hw/hw_g12a.c
+++ drivers/amlogic/media/vout/hdmitx/hdmi_tx_20/hw/hw_g12a.c
@@ -563,7 +563,7 @@ void set_g12a_hpll_clk_out(unsigned int frac_rate, unsigned int clk)
 		WAIT_FOR_PLL_LOCKED(P_HHI_HDMI_PLL_CNTL0);
 		printk("HPLL: 0x%x\n", hd_read_reg(P_HHI_HDMI_PLL_CNTL0));
 		break;
-	case 2686200:
+	case 3485000:
 		hd_write_reg(P_HHI_HDMI_PLL_CNTL0, 0x3b0004DF);//2560x1600
 		if (frac_rate)
 			hd_write_reg(P_HHI_HDMI_PLL_CNTL1, 0x0001ffff);
