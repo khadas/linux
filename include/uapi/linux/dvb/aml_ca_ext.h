33659e6da1e25ae9d0cfcea28806d9a233527fda
shufei.zhao <shufei.zhao@amlogic.com>
2023-03-09 11:02:01 +0000

    uapi: add license [1/2]
    PD#SWPL-113370
    
    Problem:
    uapi file is missing a license
    
    Solution:
    Add license
    
    Verify:
    local
    
    Change-Id: I70c73625f62d8eccb2492736478f9c0b170bdcbd
    Signed-off-by: shufei.zhao <shufei.zhao@amlogic.com>

--- include/uapi/linux/dvb/aml_ca_ext.h
+++ include/uapi/linux/dvb/aml_ca_ext.h
@@ -1,5 +1,4 @@
-/* SPDX-License-Identifier: LGPL-2.1+ WITH Linux-syscall-note */
-
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 /*
  * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
  */
