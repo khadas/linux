96eef14269a7d8c01165376fb61ec61f4edcbdbc
Nick Xie <nick@khadas.com>
2023-03-30 11:34:11 +0800

    Edge2: gsensor: add support for kxtj3
    Signed-off-by: Nick Xie <nick@khadas.com>
    Change-Id: I29b35e11fad235af628dbf69fdcdc58ec86e5249

--- drivers/input/sensors/sensor-dev.c
+++ drivers/input/sensors/sensor-dev.c
@@ -1447,7 +1447,7 @@ static int sensor_misc_device_register(struct sensor_private_data *sensor, int t
 			sensor->fops.release = gsensor_dev_release;
 
 			sensor->miscdev.minor = MISC_DYNAMIC_MINOR;
-			sensor->miscdev.name = "mma8452_daemon";
+			sensor->miscdev.name = "accel";
 			sensor->miscdev.fops = &sensor->fops;
 		} else {
 			memcpy(&sensor->miscdev, sensor->ops->misc_dev, sizeof(*sensor->ops->misc_dev));
