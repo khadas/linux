701d726a3bffeb240ecd208b4bd487a85c323e27
shihong.zheng <shihong.zheng@amlogic.com>
2023-04-10 19:55:45 +0800

    codec_mm: add config for scatter alloc from DMA32. [1/1]
    PD#SWPL-118764
    
    Problem:
    DI cannot access address over 8G.
    
    Solution:
    set alloc from dma32 zone when
    config_alloc_flags bit0 is set.
    
    Verify:
    t7c
    
    Change-Id: Ia64d2fb76874e12414a828715076ec57ee9ea8dc
    Signed-off-by: shihong.zheng <shihong.zheng@amlogic.com>

--- include/linux/amlogic/media/codec_mm/codec_mm_scatter.h
+++ include/linux/amlogic/media/codec_mm/codec_mm_scatter.h
@@ -14,6 +14,9 @@
 #define phy_addr_type ulong
 #define page_sid_type u16
 
+/* config_alloc_flags bitmask */
+#define SC_ALLOC_SYS_DMA32  BIT(0)
+
 #define PAGE_INDEX(page) ((page) >> PAGE_SHIFT)
 
 struct codec_mm_scatter {
@@ -98,5 +101,9 @@ int codec_mm_dump_scatter(struct codec_mm_scatter *mms, void *buf, int size);
 int codec_mm_scatter_dec_owner_user(void *sc_mm, int delay_ms);
 
 int codec_mm_alloc_sys_size(void);
+
+int codec_mm_scatter_alloc_flags_config(int is_tvp, int sc_alloc_flags);
+
+int codec_mm_scatter_alloc_flag_get(void);
 
 #endif
