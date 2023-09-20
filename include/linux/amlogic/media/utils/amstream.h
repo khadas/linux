b4e909f4fdb159d7adf4736f01325a3685e15ff0
dehong.chen <dehong.chen@amlogic.com>
2023-09-20 12:37:56 +0000

    decoder: CF2 add use tsync flag [1/9]
    PD#SWPL-130171
    
    Problem:
    add use tsync flag for 5.4
    
    Solution:
    add use tsync flag for 5.4
    
    Verify:
    t5d
    
    Change-Id: I042a39f710014cdbd54edf2baa32f6a5dc3ff2e5
    Signed-off-by: dehong.chen <dehong.chen@amlogic.com>

--- include/linux/amlogic/media/utils/amstream.h
+++ include/linux/amlogic/media/utils/amstream.h
@@ -30,6 +30,7 @@
 		PORT_FLAG_AID | PORT_FLAG_SID | PORT_FLAG_UD)
 #define PORT_FLAG_INITED    0x100
 #define PORT_FLAG_DMABUF    0x200
+#define PORT_FLAG_TSYNC     0x400
 
 #define PORT_TYPE_VIDEO         0x01
 #define PORT_TYPE_AUDIO         0x02
