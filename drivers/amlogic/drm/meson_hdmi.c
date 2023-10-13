8f826a793a331fae896fce57058ca1e6cc67b4c8
congyang.huang <congyang.huang@amlogic.com>
2023-10-13 17:18:26 +0800

    drm: Remove the judgment of max_bpc [1/1]
    PD#SWPL-141895
    
    Problem:
    Plug and unplug the HDMI cable,hdmitx output SDR due to
    the smaller bit of attr than the value of max_bpc
    
    Solution:
    Remove the judgment of max_bpc because hdmitx does not
    rely on max_bpc
    
    Verify:
    sc2
    
    Test:
    DRM-TX-44
    
    Change-Id: I457a874f86b9f80218b5eb1a9365b333d5b61728
    Signed-off-by: congyang.huang <congyang.huang@amlogic.com>

--- drivers/amlogic/drm/meson_hdmi.c
+++ drivers/amlogic/drm/meson_hdmi.c
@@ -231,10 +231,8 @@ static bool meson_hdmitx_test_color_attr(struct am_meson_crtc_state *crtc_state,
 	char *outputmode = crtc_state->base.adjusted_mode.name;
 	struct hdmitx_color_attr *attr_list = NULL;
 	char attr_str[HDMITX_ATTR_LEN_MAX];
-	u8 max_bpc = conn_state->base.max_bpc;
 
-	if (test_attr->colorformat == COLORSPACE_RESERVED ||
-		test_attr->bitdepth > max_bpc)
+	if (test_attr->colorformat == COLORSPACE_RESERVED)
 		return false;
 
 	attr_list = meson_hdmitx_get_candidate_attr_list(crtc_state);
@@ -265,7 +263,7 @@ static bool meson_hdmitx_test_color_attr(struct am_meson_crtc_state *crtc_state,
 
 static int meson_hdmitx_decide_color_attr
 	(struct am_meson_crtc_state *crtc_state,
-	u8 max_bpc, struct hdmitx_color_attr *attr)
+	struct hdmitx_color_attr *attr)
 {
 	char *outputmode = crtc_state->base.adjusted_mode.name;
 	struct hdmitx_color_attr *attr_list = NULL;
@@ -282,19 +280,17 @@ static int meson_hdmitx_decide_color_attr
 		if (attr_list->colorformat == COLORSPACE_RESERVED)
 			break;
 
-		if (attr_list->bitdepth <= max_bpc) {
-			build_hdmitx_attr_str(attr_str,
-				attr_list->colorformat, attr_list->bitdepth);
-			if (am_hdmi_info.hdmitx_dev->test_attr(outputmode,
-					attr_str)) {
-				attr->colorformat = attr_list->colorformat;
-				attr->bitdepth = attr_list->bitdepth;
-				DRM_INFO("%s get fmt attr [%d]+[%d]\n",
-					__func__,
-					attr->colorformat,
-					attr->bitdepth);
-				break;
-			}
+		build_hdmitx_attr_str(attr_str,
+			attr_list->colorformat, attr_list->bitdepth);
+		if (am_hdmi_info.hdmitx_dev->test_attr(outputmode,
+				attr_str)) {
+			attr->colorformat = attr_list->colorformat;
+			attr->bitdepth = attr_list->bitdepth;
+			DRM_INFO("%s get fmt attr [%d]+[%d]\n",
+				__func__,
+				attr->colorformat,
+				attr->bitdepth);
+			break;
 		}
 	} while (attr_list++);
 	if (attr_list->colorformat == COLORSPACE_RESERVED) {
@@ -303,8 +299,8 @@ static int meson_hdmitx_decide_color_attr
 		attr->bitdepth = 8;
 	}
 
-	DRM_DEBUG_KMS("[%s]:[%s,bpc:%d,eotf:%d]=>attr[%d,%d]\n", __func__,
-		outputmode, max_bpc, crtc_state->crtc_eotf_type,
+	DRM_DEBUG_KMS("[%s]:[%s,eotf:%d]=>attr[%d,%d]\n", __func__,
+		outputmode, crtc_state->crtc_eotf_type,
 		attr->colorformat, attr->bitdepth);
 
 	return 0;
@@ -1500,7 +1496,7 @@ static int meson_hdmitx_choose_preset_mode(struct am_hdmi_tx *hdmitx,
 	}
 
 	/*traverse all color format and test.*/
-	meson_hdmitx_decide_color_attr(meson_crtc_state, HDMITX_MAX_BPC, &matched_attr);
+	meson_hdmitx_decide_color_attr(meson_crtc_state, &matched_attr);
 	build_hdmitx_attr_str(attr_str, matched_attr.colorformat, matched_attr.bitdepth);
 	if (!hdmitx->hdmitx_dev->test_attr(modename, attr_str)) {
 		DRM_ERROR("mode %s NOT supporteds\n", modename);
@@ -1585,7 +1581,7 @@ void meson_hdmitx_encoder_atomic_mode_set(struct drm_encoder *encoder,
 		}
 		if (update_attr) {
 			meson_hdmitx_decide_color_attr(meson_crtc_state,
-				hdmitx_state->base.max_bpc, attr);
+				attr);
 			hdmitx_state->update = true;
 		}
 	}
