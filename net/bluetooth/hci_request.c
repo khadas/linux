86e8d6eb9d829a9ba759726f52e98280e0471e04
hong.wang <hong.wang@amlogic.com>
2023-10-25 14:49:58 +0800

    BT: The t5m broadcast is abnormal. [1/1]
    PD#SWPL-142947
    
    Problem:
    The t5m broadcast is abnormal.
    
    Solution:
    data handle is 0.
    
    Verify:
    t5w
    
    Change-Id: I19d1eb7a78b1a1245daa561edf27178025b4bcec
    Signed-off-by: hong.wang <hong.wang@amlogic.com>

--- net/bluetooth/hci_request.c
+++ net/bluetooth/hci_request.c
@@ -1231,7 +1231,7 @@ void __hci_req_update_scan_rsp_data(struct hci_request *req, u8 instance)
 		memcpy(hdev->scan_rsp_data, cp.data, sizeof(cp.data));
 		hdev->scan_rsp_data_len = len;
 
-		cp.handle = 0;
+		cp.handle = instance;
 		cp.length = len;
 		cp.operation = LE_SET_ADV_DATA_OP_COMPLETE;
 		cp.frag_pref = LE_SET_ADV_DATA_NO_FRAG;
@@ -1375,7 +1375,7 @@ void __hci_req_update_adv_data(struct hci_request *req, u8 instance)
 		hdev->adv_data_len = len;
 
 		cp.length = len;
-		cp.handle = 0;
+		cp.handle = instance;
 		cp.operation = LE_SET_ADV_DATA_OP_COMPLETE;
 		cp.frag_pref = LE_SET_ADV_DATA_NO_FRAG;
 
@@ -1660,7 +1660,7 @@ int __hci_req_setup_ext_adv_instance(struct hci_request *req, u8 instance)
 
 		memset(&cp, 0, sizeof(cp));
 
-		cp.handle = 0;
+		cp.handle = instance;
 		bacpy(&cp.bdaddr, &random_addr);
 
 		hci_req_add(req,
