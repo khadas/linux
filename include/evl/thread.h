6f3cc1e04f29389f8aba7b73b1f1483a55a9db2a
Philippe Gerum <philippe.gerum@exail.com>
2023-10-29 10:44:38 +0100

    evl/thread: remove useless dynamic lock key
    All lock instances serializing accesses to the shared data in thread
    descriptors belong to the same class lockdep-wise. Calling
    raw_spin_lock_init() to initialize such lock is enough and
    non-ambiguous.
    
    Signed-off-by: Philippe Gerum <philippe.gerum@exail.com>

--- include/evl/thread.h
+++ include/evl/thread.h
@@ -63,7 +63,6 @@ struct evl_init_thread_attr {
 
 struct evl_thread {
 	hard_spinlock_t lock;
-	struct lock_class_key lock_key;	/* lockdep disambiguation */
 
 	/*
 	 * Shared thread-specific data, covered by ->lock.
