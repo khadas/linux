0d159ddd34a7e0051eed9dcd3938ad0ea5ba25c7
Pengcheng Chen <pengcheng.chen@amlogic.com>
2023-10-31 14:47:16 +0800

    osd: add osd afbc reg to recovery [1/1]
    PD#SWPL-144251
    
    Problem:
    osd afbc reg not recovery
    
    Solution:
    add osd afbc reg to recovery
    
    Verify:
    t5d
    
    Change-Id: I30852245aaa1d78efb8aabb3d8ad733e746d4af2
    Signed-off-by: Pengcheng Chen <pengcheng.chen@amlogic.com>

--- drivers/amlogic/media/osd/osd_hw.c
+++ drivers/amlogic/media/osd/osd_hw.c
@@ -14459,11 +14459,11 @@ void osd_init_hw(u32 logo_loaded, u32 osd_probe,
 #endif
 	} else if (osd_meson->cpu_id >= __MESON_CPU_MAJOR_ID_G12A) {
 		if (osd_dev_hw.display_type != C3_DISPLAY) {
-			if (osd_dev_hw.multi_afbc_core)
-				backup_regs_init(HW_RESET_NONE);
-			else
+			if (osd_meson->cpu_id == __MESON_CPU_MAJOR_ID_G12A)
 				backup_regs_init(HW_RESET_MALI_AFBCD_REGS);
-		}
+			else
+				backup_regs_init(HW_RESET_NONE);
+			}
 	} else {
 		backup_regs_init(HW_RESET_NONE);
 	}
