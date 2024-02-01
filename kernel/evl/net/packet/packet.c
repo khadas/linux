f9c0350578f0694b5a2a246cf7e306704f3464b3
Philippe Gerum <philippe.gerum@exail.com>
2024-02-01 11:17:47 +0100

    evl/net: socket: make bind, connect handlers optional
    Signed-off-by: Philippe Gerum <philippe.gerum@exail.com>

--- kernel/evl/net/packet/packet.c
+++ kernel/evl/net/packet/packet.c
@@ -661,7 +661,6 @@ static struct evl_net_proto ether_packet_proto = {
 	.attach	= attach_packet_socket,
 	.detach = detach_packet_socket,
 	.bind = bind_packet_socket,
-	.connect = evl_proto_no_connect,
 	.oob_send = send_packet,
 	.oob_poll = poll_packet,
 	.oob_receive = receive_packet,
