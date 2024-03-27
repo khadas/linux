06ba7b8d96206daaad5916bc0e05d49b59256da8
Sugar Zhang <sugar.zhang@rock-chips.com>
2024-03-27 13:35:51 +0800

    ASoC: rockchip: i2s-tdm: Optimize pm_runtime_forbid
    Should be placed after pm_runtime_enable to do
    rpm_resume at the moment. otherwise, it will make sense
    at the next pm_runtime_get.
    
    Signed-off-by: Sugar Zhang <sugar.zhang@rock-chips.com>
    Change-Id: I759e5c8c0602b380c7f50d80d8f564316beea632

--- sound/soc/rockchip/rockchip_i2s_tdm.c
+++ sound/soc/rockchip/rockchip_i2s_tdm.c
@@ -2720,8 +2720,6 @@ static int rockchip_i2s_tdm_keep_clk_always_on(struct rk_i2s_tdm_dev *i2s_tdm)
 	else
 		rockchip_i2s_tdm_xfer_start(i2s_tdm, SNDRV_PCM_STREAM_PLAYBACK);
 
-	pm_runtime_forbid(i2s_tdm->dev);
-
 	dev_info(i2s_tdm->dev, "CLK-ALWAYS-ON: mclk: %d, bclk: %d, fsync: %d\n",
 		 mclk_rate, bclk_rate, DEFAULT_FS);
 
@@ -3112,6 +3110,14 @@ static int rockchip_i2s_tdm_probe(struct platform_device *pdev)
 		if (ret)
 			goto err_pm_disable;
 	}
+
+	/*
+	 * Should be placed after pm_runtime_enable to do
+	 * rpm_resume at the moment. otherwise, it will make sense
+	 * at the next pm_runtime_get.
+	 */
+	if (i2s_tdm->quirks & QUIRK_ALWAYS_ON)
+		pm_runtime_forbid(i2s_tdm->dev);
 
 	ret = rockchip_i2s_tdm_register_platform(&pdev->dev);
 	if (ret)
