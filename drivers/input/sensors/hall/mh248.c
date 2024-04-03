5c6d5ffd84ef1ed4dd2d9911719e92496068a60d
Zorro Liu <lyx@rock-chips.com>
2024-04-03 22:43:15 +0800

    input: sensors: hall: mh248: judge wakeup sources for hall sensor
    for hardware reason, we need judge wakeup sources to avoid wakeup
    screen by wrong irq when wakeup from ultra sleep
    
    Change-Id: I5a3ef85eb71a312ba0a9e992b70ef0b14e00fc47
    Signed-off-by: Zorro Liu <lyx@rock-chips.com>

--- drivers/input/sensors/hall/mh248.c
+++ drivers/input/sensors/hall/mh248.c
@@ -22,6 +22,8 @@
 #include <linux/rk_keys.h>
 #include <linux/sensor-dev.h>
 #include <linux/slab.h>
+#include <linux/suspend.h>
+#include <linux/syscore_ops.h>
 #include <linux/uaccess.h>
 #include <linux/workqueue.h>
 
@@ -35,7 +37,20 @@ struct mh248_para {
 	int gpio_pin;
 	int irq;
 	int active_value;
+#ifdef CONFIG_ROCKCHIP_LITE_ULTRA_SUSPEND
+	void __iomem *pmu_gpio_regs;
+	u32 pmu_gpio_regs_base;
+	u32 pmu_gpio_regs_size;
+	u32 pmu_gpio_int_reg;
+	u32 pmu_gpio_rtc_int_mask;
+	u32 pmu_gpio_pmic_int_mask;
+	int is_hall_wakeup;
+#endif
 };
+
+#ifdef CONFIG_ROCKCHIP_LITE_ULTRA_SUSPEND
+static struct mh248_para *g_mh248 = NULL;
+#endif
 
 static int hall_ebc_notifier_callback(struct notifier_block *self,
 				     unsigned long action, void *data)
@@ -87,6 +102,8 @@ static irqreturn_t hall_mh248_interrupt(int irq, void *dev_id)
 	int gpio_value = 0;
 
 	gpio_value = gpio_get_value(mh248->gpio_pin);
+	pr_info("%s: gpio value =  %d\n", __func__, gpio_value);
+
 	if ((gpio_value != mh248->active_value) &&
 	    (mh248->is_suspend == 0)) {
 		input_report_key(mh248->hall_input, KEY_POWER, 1);
@@ -104,6 +121,26 @@ static irqreturn_t hall_mh248_interrupt(int irq, void *dev_id)
 	return IRQ_HANDLED;
 }
 
+#ifdef CONFIG_ROCKCHIP_LITE_ULTRA_SUSPEND
+static void mh248_syscore_resume(void)
+{
+	u32 intr_status;
+
+	g_mh248->is_hall_wakeup = 1;
+	intr_status = readl_relaxed(g_mh248->pmu_gpio_regs + g_mh248->pmu_gpio_int_reg);
+	if (intr_status & g_mh248->pmu_gpio_rtc_int_mask)
+		g_mh248->is_hall_wakeup = 0;
+	else if (intr_status & g_mh248->pmu_gpio_pmic_int_mask)
+		g_mh248->is_hall_wakeup = 0;
+
+	pr_info("%s: GPIO0 INT status =  %x, is_hall_wakeup = %d\n", __func__, intr_status, g_mh248->is_hall_wakeup);
+}
+
+static struct syscore_ops mh248_syscore_ops = {
+	.resume = mh248_syscore_resume,
+};
+#endif
+
 static int hall_mh248_probe(struct platform_device *pdev)
 {
 	struct device_node *np = pdev->dev.of_node;
@@ -117,6 +154,39 @@ static int hall_mh248_probe(struct platform_device *pdev)
 		return -ENOMEM;
 
 	mh248->dev = &pdev->dev;
+
+#ifdef CONFIG_ROCKCHIP_LITE_ULTRA_SUSPEND
+	ret = of_property_read_u32(np, "pmu-gpio-regs-base", &mh248->pmu_gpio_regs_base);
+	if (ret) {
+		dev_err(mh248->dev, "Can not read property pmu-gpio-regs-base\n");
+		return ret;
+	}
+	dev_info(mh248->dev, "pmu_gpio_regs_base = 0x%x\n", mh248->pmu_gpio_regs_base);
+	ret = of_property_read_u32(np, "pmu-gpio-regs-size", &mh248->pmu_gpio_regs_size);
+	if (ret) {
+		dev_err(mh248->dev, "Can not read property pmu-gpio-reg-size\n");
+		return ret;
+	}
+	dev_info(mh248->dev, "pmu_gpio_regs_size = 0x%x\n", mh248->pmu_gpio_regs_size);
+	ret = of_property_read_u32(np, "pmu-gpio-int-reg", &mh248->pmu_gpio_int_reg);
+	if (ret) {
+		dev_err(mh248->dev, "Can not read property pmu-gpio-int-reg\n");
+		return ret;
+	}
+	dev_info(mh248->dev, "pmu_gpio_int_reg = 0x%x\n", mh248->pmu_gpio_int_reg);
+	ret = of_property_read_u32(np, "pmu-gpio-rtc-int-mask", &mh248->pmu_gpio_rtc_int_mask);
+	if (ret) {
+		dev_err(mh248->dev, "Can not read property pmu-gpio-rtc-int-mask\n");
+		return ret;
+	}
+	dev_info(mh248->dev, "pmu_gpio_rtc_int_mask = 0x%x\n", mh248->pmu_gpio_rtc_int_mask);
+	ret = of_property_read_u32(np, "pmu-gpio-pmic-int-mask", &mh248->pmu_gpio_pmic_int_mask);
+	if (ret) {
+		dev_err(mh248->dev, "Can not read property pmu-gpio-pmic-int-mask\n");
+		return ret;
+	}
+	dev_info(mh248->dev, "pmu_gpio_pmic_int_mask = 0x%x\n", mh248->pmu_gpio_pmic_int_mask);
+#endif
 
 	mh248->gpio_pin = of_get_named_gpio_flags(np, "irq-gpio",
 						  0, &irq_flags);
@@ -169,10 +239,63 @@ static int hall_mh248_probe(struct platform_device *pdev)
 
 	mh248->ebc_notif.notifier_call = hall_ebc_notifier_callback;
 	ebc_register_notifier(&mh248->ebc_notif);
+
+#ifdef CONFIG_ROCKCHIP_LITE_ULTRA_SUSPEND
+	mh248->pmu_gpio_regs = devm_ioremap(mh248->dev, mh248->pmu_gpio_regs_base, mh248->pmu_gpio_regs_size);
+	register_syscore_ops(&mh248_syscore_ops);
+	g_mh248 = mh248;
+#endif
+
 	dev_info(mh248->dev, "hall_mh248_probe success.\n");
 
 	return 0;
 }
+
+#ifdef CONFIG_ROCKCHIP_LITE_ULTRA_SUSPEND
+static int hall_mh248_remove(struct platform_device *pdev)
+{
+	unregister_syscore_ops(&mh248_syscore_ops);
+
+	return 0;
+}
+
+static int mh248_suspend(struct device *dev)
+{
+	if (mem_sleep_current == PM_SUSPEND_MEM_ULTRA) {
+		disable_irq_wake(g_mh248->irq);
+		disable_irq(g_mh248->irq);
+	}
+
+	return 0;
+}
+
+static int mh248_resume(struct device *dev)
+{
+	int gpio_value = 0;
+
+	if (mem_sleep_current == PM_SUSPEND_MEM_ULTRA) {
+		if (g_mh248->is_hall_wakeup) {
+			gpio_value = gpio_get_value(g_mh248->gpio_pin);
+			if ((gpio_value == g_mh248->active_value) &&
+				(g_mh248->is_suspend == 1)) {
+				input_report_key(g_mh248->hall_input, KEY_WAKEUP, 1);
+				input_sync(g_mh248->hall_input);
+				input_report_key(g_mh248->hall_input, KEY_WAKEUP, 0);
+				input_sync(g_mh248->hall_input);
+				pr_info("%s: send wakeup key\n", __func__);
+			}
+		}
+		enable_irq(g_mh248->irq);
+		enable_irq_wake(g_mh248->irq);
+	}
+	return 0;
+}
+
+static const struct dev_pm_ops mh248_pm = {
+	.resume = mh248_resume,
+	.suspend = mh248_suspend,
+};
+#endif
 
 static const struct of_device_id hall_mh248_match[] = {
 	{ .compatible = "hall-mh248" },
@@ -181,10 +304,16 @@ static const struct of_device_id hall_mh248_match[] = {
 
 static struct platform_driver hall_mh248_driver = {
 	.probe = hall_mh248_probe,
+#ifdef CONFIG_ROCKCHIP_LITE_ULTRA_SUSPEND
+	.remove = hall_mh248_remove,
+#endif
 	.driver = {
 		.name = "mh248",
 		.owner = THIS_MODULE,
 		.of_match_table	= hall_mh248_match,
+#ifdef CONFIG_ROCKCHIP_LITE_ULTRA_SUSPEND
+		.pm = &mh248_pm,
+#endif
 	},
 };
 
