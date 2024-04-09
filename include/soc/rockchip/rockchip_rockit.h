7154b7a38274d8029d1c95cb925d291b16443fd7
LongChang Ma <chad.ma@rock-chips.com>
2024-04-09 15:07:18 +0800

    include: rockchip: rockchip_rockit: modify ROCKIT_BUF_NUM_MAX to 20
    Change-Id: I924ba4782c2cd39dfee1ee4938bfff3bbc303e22
    Signed-off-by: LongChang Ma <chad.ma@rock-chips.com>

--- include/soc/rockchip/rockchip_rockit.h
+++ include/soc/rockchip/rockchip_rockit.h
@@ -8,7 +8,7 @@
 #include <linux/dma-buf.h>
 #include <linux/rk-isp2-config.h>
 
-#define ROCKIT_BUF_NUM_MAX	5
+#define ROCKIT_BUF_NUM_MAX	20
 #define ROCKIT_ISP_NUM_MAX	3
 #define ROCKIT_STREAM_NUM_MAX	12
 
