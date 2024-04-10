84b52f69bfd7e4e5f0eaf519781e89382d1eccee
Zhang Yubing <yubing.zhang@rock-chips.com>
2024-04-10 17:44:33 +0800

    drm/rockchip: dw-dp: enable hdcp after enable video stream
    In DisplayPort HDCP1.3 CTS case 1A-01 page 10, unencrypted video
    signal should be sent before enable hdcp.
    In DisplayPort HDCP2.2 CTS case 1A-1 page 12, unencrypted video
    signal may be sent before enable hdcp.
    
    Change-Id: I12f5ec4143728683663cba5a0f079c915c500383
    Signed-off-by: Zhang Yubing <yubing.zhang@rock-chips.com>

--- drivers/gpu/drm/rockchip/dw-dp.c
+++ drivers/gpu/drm/rockchip/dw-dp.c
@@ -3131,14 +3131,14 @@ static void dw_dp_bridge_atomic_enable(struct drm_bridge *bridge,
 		return;
 	}
 
-	if (conn_state->content_protection == DRM_MODE_CONTENT_PROTECTION_DESIRED)
-		dw_dp_hdcp_enable(dp, conn_state->hdcp_content_type);
-
 	ret = dw_dp_video_enable(dp);
 	if (ret < 0) {
 		dev_err(dp->dev, "failed to enable video: %d\n", ret);
 		return;
 	}
+
+	if (conn_state->content_protection == DRM_MODE_CONTENT_PROTECTION_DESIRED)
+		dw_dp_hdcp_enable(dp, conn_state->hdcp_content_type);
 
 	if (dp->panel)
 		drm_panel_enable(dp->panel);
