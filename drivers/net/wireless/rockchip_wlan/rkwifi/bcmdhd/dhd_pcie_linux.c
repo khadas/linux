6f5319f06201cfcc033399f42cf757042c07181a
Jon Lin <jon.lin@rock-chips.com>
2024-05-15 11:06:37 +0800

    net: wireless: rockchip_wlan: bcmdhd: Reset PCIe when wifi reg on
    Change-Id: Iec7811ae42a7e8bc35ef59006d0694b78e372368
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/dhd_pcie_linux.c
+++ drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/dhd_pcie_linux.c
@@ -87,6 +87,10 @@
 
 #include <dhd_plat.h>
 
+#ifdef CONFIG_ARCH_ROCKCHIP
+#include <linux/aspm_ext.h>
+#endif
+
 #define PCI_CFG_RETRY		10		/* PR15065: retry count for pci cfg accesses */
 #define OS_HANDLE_MAGIC		0x1234abcd	/* Magic # to recognize osh */
 #define BCM_MEM_FILENAME_LEN	24		/* Mem. filename length */
@@ -2404,6 +2408,10 @@ dhdpcie_start_host_dev(dhd_bus_t *bus)
 	ret = tegra_pcie_pm_resume();
 #endif /* CONFIG_ARCH_TEGRA_210_SOC */
 #endif /* CONFIG_ARCH_TEGRA */
+#ifdef CONFIG_ARCH_ROCKCHIP
+	if (bus->rc_dev)
+		ret = rockchip_dw_pcie_pm_ctrl_for_user(bus->rc_dev, ROCKCHIP_PCIE_PM_CTRL_RESET);
+#endif /* CONFIG_ARCH_ROCKCHIP */
 
 	if (ret) {
 		DHD_ERROR(("%s Failed to bring up PCIe link\n", __FUNCTION__));
