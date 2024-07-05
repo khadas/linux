ce1b7a4f5641dfc83e6c329405a2e158cf91590a
Xiong Zhang <xiong.zhang@wesion.com>
2024-07-05 14:40:51 +0800

    LCD: NEW Compatible with TS050,TS101 and HDMI [2/3] u-boot [1/3] system/core [3/3]
    Signed-off-by: Xiong Zhang <xiong.zhang@wesion.com>

--- drivers/video/of_display_timing.c
+++ drivers/video/of_display_timing.c
@@ -153,7 +153,7 @@ struct display_timings *of_get_display_timings(const struct device_node *np)
 		return NULL;
 
 	printk("of_get_display_timings\n");
-	if(1 == khadas_mipi_id){
+	if(2 == khadas_mipi_id){
 		timings_np = of_get_child_by_name(np, "display-timings1");
 	} else {
 		timings_np = of_get_child_by_name(np, "display-timings");
