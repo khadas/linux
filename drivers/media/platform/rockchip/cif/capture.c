2b5a6bf9ae5569c9b4994a32af0efee6986f5eb7
Zefa Chen <zefa.chen@rock-chips.com>
2024-07-23 15:40:07 +0800

    media: rockchip: vicap wait fe to stop stream when stream suspend
    Signed-off-by: Zefa Chen <zefa.chen@rock-chips.com>
    Change-Id: I001f2052481c7b91b09100d1d6e5bae0d5555327

--- drivers/media/platform/rockchip/cif/capture.c
+++ drivers/media/platform/rockchip/cif/capture.c
@@ -12297,7 +12297,7 @@ int rkcif_stream_suspend(struct rkcif_device *cif_dev, int mode)
 			suspend_cnt++;
 			v4l2_dbg(1, rkcif_debug, &cif_dev->v4l2_dev,
 				 "stream[%d] stopping\n", stream->id);
-			if (stream->dma_en) {
+			if (!cif_dev->is_sensor_off) {
 				stream->stopping = true;
 				ret = wait_event_timeout(stream->wq_stopped,
 							 stream->state != RKCIF_STATE_STREAMING,
