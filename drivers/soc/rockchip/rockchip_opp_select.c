8ceb525d09ef38ead3c367d0c3be0b3d4ee3cfc2
Finley Xiao <finley.xiao@rock-chips.com>
2024-09-18 17:32:20 +0800

    soc: rockchip: opp_select: Fix dev_pm_opp_get_opp_table() return value
    Signed-off-by: Finley Xiao <finley.xiao@rock-chips.com>
    Change-Id: I2f3b2bb6ccf359574a0b8d503f3491d9fa7e2299

--- drivers/soc/rockchip/rockchip_opp_select.c
+++ drivers/soc/rockchip/rockchip_opp_select.c
@@ -785,10 +785,10 @@ static int rockchip_init_pvtpll_info(struct rockchip_opp_info *info)
 		return -ENOMEM;
 
 	opp_table = dev_pm_opp_get_opp_table(info->dev);
-	if (!opp_table) {
+	if (IS_ERR(opp_table)) {
 		kfree(info->opp_table);
 		info->opp_table = NULL;
-		return -ENOMEM;
+		return PTR_ERR(opp_table);
 	}
 
 	mutex_lock(&opp_table->lock);
@@ -863,7 +863,7 @@ static void rockchip_pvtpll_calibrate_opp(struct rockchip_opp_info *info)
 		return;
 
 	opp_table = dev_pm_opp_get_opp_table(info->dev);
-	if (!opp_table)
+	if (IS_ERR(opp_table))
 		return;
 
 	if (info->clocks) {
@@ -1020,7 +1020,7 @@ static void rockchip_pvtpll_add_length(struct rockchip_opp_info *info)
 		goto out;
 
 	opp_table = dev_pm_opp_get_opp_table(info->dev);
-	if (!opp_table)
+	if (IS_ERR(opp_table))
 		goto out;
 	old_rate = clk_get_rate(opp_table->clk);
 	opp_flag = OPP_ADD_LENGTH | ((margin & OPP_LENGTH_MASK) << OPP_LENGTH_SHIFT);
@@ -1783,8 +1783,8 @@ static int rockchip_adjust_opp_by_irdrop(struct device *dev,
 	rockchip_get_sel_table(np, "rockchip,board-irdrop", &irdrop_table);
 
 	opp_table = dev_pm_opp_get_opp_table(dev);
-	if (!opp_table) {
-		ret =  -ENOMEM;
+	if (IS_ERR(opp_table)) {
+		ret = PTR_ERR(opp_table);
 		goto out;
 	}
 
@@ -1855,7 +1855,7 @@ static void rockchip_adjust_opp_by_mbist_vmin(struct device *dev,
 		return;
 
 	opp_table = dev_pm_opp_get_opp_table(dev);
-	if (!opp_table)
+	if (IS_ERR(opp_table))
 		return;
 
 	mutex_lock(&opp_table->lock);
@@ -1887,7 +1887,7 @@ static void rockchip_adjust_opp_by_otp(struct device *dev,
 		 opp_info.min_freq, opp_info.max_freq, opp_info.volt);
 
 	opp_table = dev_pm_opp_get_opp_table(dev);
-	if (!opp_table)
+	if (IS_ERR(opp_table))
 		return;
 
 	mutex_lock(&opp_table->lock);
