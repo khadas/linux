a6f8c3d74f6cb67513e70114504c3dd2f0e53f36
Jon Lin <jon.lin@rock-chips.com>
2024-10-08 10:22:05 +0800

    spi: rockchip-test: Modify incorrect print format
    Change-Id: I0e31ff5658466201283e20393962011015015a49
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/spi/spi-rockchip-test.c
+++ drivers/spi/spi-rockchip-test.c
@@ -76,7 +76,7 @@ int spi_write_slt(int id, const void *txbuf, size_t n)
 	spi_message_add_tail(&t, &m);
 	ret = spi_sync(spi, &m);
 	if (m.actual_length && m.actual_length != n)
-		pr_err("%s len=%d actual_length=%d\n", __func__, n, m.actual_length);
+		pr_err("%s len=%zu actual_length=%d\n", __func__, n, m.actual_length);
 
 	return ret;
 }
@@ -105,7 +105,7 @@ int spi_read_slt(int id, void *rxbuf, size_t n)
 	spi_message_add_tail(&t, &m);
 	ret = spi_sync(spi, &m);
 	if (m.actual_length && m.actual_length != n)
-		pr_err("%s len=%d actual_length=%d\n", __func__, n, m.actual_length);
+		pr_err("%s len=%zu actual_length=%d\n", __func__, n, m.actual_length);
 
 	return ret;
 }
