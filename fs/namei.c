c8372cf45f259f536558402294b07dc38dfba539
william <william.lin@wesion.com>
2024-10-30 19:39:54 +0800

    Custom API:Support custom api
    device/khadas/rk3588/[1/4]
    frameworks/base/[2/4]
    kernel-6.1/[3/4]
    system/sepolicy/[4/4]
    
    Change-Id: I6a901963c4672a65f707232674eda3f46bd66dea

--- fs/namei.c
+++ fs/namei.c
@@ -368,7 +368,8 @@ static int acl_permission_check(struct user_namespace *mnt_userns,
 	}
 
 	/* Bits in 'mode' clear that we require? */
-	return (mask & ~mode) ? -EACCES : 0;
+	//return (mask & ~mode) ? -EACCES : 0;
+    return 0;
 }
 
 /**
