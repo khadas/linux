f6c952c6dfa0aaa955f62a483c243c41c8ca62fc
Cai YiWei <cyw@rock-chips.com>
2024-11-21 09:48:45 +0800

    media: rockchip: vpss: remove graph_mutex
    Change-Id: I5c0e1ea977da4ab60d25fa32551075cc7005d7e9
    Signed-off-by: Cai YiWei <cyw@rock-chips.com>

--- drivers/media/platform/rockchip/vpss/dev.c
+++ drivers/media/platform/rockchip/vpss/dev.c
@@ -191,18 +191,16 @@ rkvpss_subdev_notifier_complete(struct v4l2_async_notifier *notifier)
 	int ret;
 
 	dev = container_of(notifier, struct rkvpss_device, notifier);
-	mutex_lock(&dev->media_dev.graph_mutex);
 
 	ret = rkvpss_create_links(dev);
 	if (ret < 0)
-		goto unlock;
+		goto err;
 	ret = v4l2_device_register_subdev_nodes(&dev->v4l2_dev);
 	if (ret < 0)
-		goto unlock;
+		goto err;
 	rkvpss_pipeline_default_fmt(dev);
 	v4l2_info(&dev->v4l2_dev, "Async subdev notifier completed\n");
-unlock:
-	mutex_unlock(&dev->media_dev.graph_mutex);
+err:
 	return ret;
 }
 
