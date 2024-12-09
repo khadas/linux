9b334bba81dba81fe94d5d9d684b94e0f68e1fa4
Tao Huang <huangtao@rock-chips.com>
2024-12-09 18:21:21 +0800

    drivers: rkflash: Fix typos in Rockchip copyright notices
    There are many cases in which the company name is misspelled.
    The patch fixes these typos.
    
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>
    Change-Id: I1eeceb62c2fd2203649858216ea07c4b55bddba6

--- drivers/rkflash/rk_sftl.h
+++ drivers/rkflash/rk_sftl.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 
-/* Copyright (c) 2018 Rockchip Electronics Co. Ltd. */
+/* Copyright (c) 2018 Rockchip Electronics Co., Ltd. */
 
 #ifndef __RK_SFTL_H
 #define __RK_SFTL_H
--- drivers/rkflash/sfc_nand.h
+++ drivers/rkflash/sfc_nand.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 
-/* Copyright (c) 2018 Rockchip Electronics Co. Ltd. */
+/* Copyright (c) 2018 Rockchip Electronics Co., Ltd. */
 
 #ifndef __SFC_NAND_H
 #define __SFC_NAND_H
