3382c69c26f93574a98918d1258285a2e020031d
Tao Huang <huangtao@rock-chips.com>
2024-12-09 18:49:36 +0800

    soc: rockchip: Fix typos in Rockchip copyright notices
    There are many cases in which the company name is misspelled.
    The patch fixes these typos.
    
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>
    Change-Id: I3c206728140bccf5ea0de76a16960aa8281d65b9

--- include/soc/rockchip/rockchip_rockit.h
+++ include/soc/rockchip/rockchip_rockit.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright (c) 2021 Rockchip Electronics Co., Ltd
+ * Copyright (c) 2021 Rockchip Electronics Co., Ltd.
  */
 #ifndef __SOC_ROCKCHIP_ROCKIT_H
 #define __SOC_ROCKCHIP_ROCKIT_H
