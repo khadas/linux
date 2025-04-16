9cb3e0518006e2ece3f627bebf34bc7fe1cf5648
Xiong Zhang <xiong.zhang@wesion.com>
2025-04-16 14:04:23 +0800

    Add root function support
    frameworks/base [1/4]
    kernel-6.1 [2/4]
    system/core [3/4]
    system/extras [4/4]
    
    Signed-off-by: Xiong Zhang <xiong.zhang@wesion.com>

--- security/commoncap.c
+++ security/commoncap.c
@@ -1243,10 +1243,10 @@ static int cap_prctl_drop(unsigned long cap)
 {
 	struct cred *new;
 
-	if (!ns_capable(current_user_ns(), CAP_SETPCAP))
+	/*if (!ns_capable(current_user_ns(), CAP_SETPCAP))
 		return -EPERM;
 	if (!cap_valid(cap))
-		return -EINVAL;
+		return -EINVAL;*/
 
 	new = prepare_creds();
 	if (!new)
